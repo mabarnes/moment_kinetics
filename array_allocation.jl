a768b3e04d457d24bdeccb29247225c483d0a821
Michael Barnes <michael.barnes@physics.ox.ac.uk>
2020-10-13 06:52:21 +0100

    added allocate_complex function to allocate_arrays.jl i

--- array_allocation.jl
+++ array_allocation.jl
@@ -22,4 +22,14 @@ function allocate_float(n,m,p)
     return array = Array{Float64,3}(undef, n, m, p)
 end
 
+# allocate 1d array with n entries of type Complex{Float64}
+function allocate_complex(n)
+    return array = Array{Complex{Float64},1}(undef, n)
+end
+
+# allocate a 2d array of size n×m with entries of type Complex{Float64}
+function allocate_complex(n,m)
+    return array = Array{Complex{Float64},2}(undef, n, m)
+end
+
 end
