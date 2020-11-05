732c1e3652f85b3f823111f27408cd89ce8f480f
Michael Barnes <michael.barnes@physics.ox.ac.uk>
2020-11-05 13:45:19 +0000

    added precompile.jl, which generates the static sysimage file moment_kinetics.so. if this file is passed as an argument to julia, it will use pre-compiled versions of the packages specified in precompile.jl.

---
+++ precompile.jl
@@ -0,0 +1,17 @@
+using Pkg
+using PackageCompiler
+
+# if "tmpdir" exists, then remove it to make way for clean creation
+isdir("tmpdir") && rm("tmpdir", recursive=true)
+# create a temporary directory called tmpdir, where a new environment will be
+# activated and the new sysimage will be initially created
+mkpath("tmpdir")
+# activate a new environment for the temporary project directory
+Pkg.activate("tmpdir")
+# add the packages we wish to pre-compile
+Pkg.add("NCDatasets")
+# creat the sysimage 'moment_kinetics.so' in the base moment_kinetics source directory
+# with the above pre-compiled packages
+create_sysimage(:NCDatasets; sysimage_path="moment_kinetics.so")
+# remove the temporary directory
+rm("tmpdir", recursive=true)
