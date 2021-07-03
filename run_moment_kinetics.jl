a916f88c9ad2233f7c33f79ffd43895077f763e6
John Omotani <john.omotani@ukaea.uk>
2021-07-03 22:42:31 +0200

    Auto-activate package in top-level scripts
    Saves needing to pass `--project` argument to `julia`.

--- run_moment_kinetics.jl
+++ run_moment_kinetics.jl
@@ -1,5 +1,8 @@
 # provide option of running from command line via 'julia moment_kinetics.jl'
 if abspath(PROGRAM_FILE) == @__FILE__
+    using Pkg
+    Pkg.activate(".")
+
     using TimerOutputs
     using moment_kinetics
     using moment_kinetics.moment_kinetics_input: mk_input
--- run_post_processing.jl
+++ run_post_processing.jl
@@ -1,5 +1,8 @@
-import moment_kinetics as mk
-
 if abspath(PROGRAM_FILE) == @__FILE__
+    using Pkg
+    Pkg.activate(".")
+
+    import moment_kinetics as mk
+
     mk.post_processing.analyze_and_plot_data(ARGS[1])
 end
