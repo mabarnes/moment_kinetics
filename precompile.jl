d78cbc06c3d8f259ed4d5f955dc6ae451ff303a1
John Omotani <john.omotani@ukaea.uk>
2021-07-04 02:49:08 +0200

    Update precompilation
    Now two scripts, precompile_dependencies.jl to precompile just the
    dependencies (which is what the previous precompile.jl did), and a new
    precompile.jl which precompiles the whole moment_kinetics package (but
    prevents the code being edited without re-precompiling).

--- precompile.jl
+++ precompile.jl
@@ -1,28 +1,14 @@
 using Pkg
+
+# Activate the moment_kinetics package
+Pkg.activate(".")
+
 using PackageCompiler
 
-# if "tmpdir" exists, then remove it to make way for clean creation
-isdir("tmpdir") && rm("tmpdir", recursive=true)
-# create a temporary directory called tmpdir, where a new environment will be
-# activated and the new sysimage will be initially created
-mkpath("tmpdir")
-# activate a new environment for the temporary project directory
-Pkg.activate("tmpdir")
-# add the packages we wish to pre-compile
-Pkg.add("PackageCompiler")
-Pkg.add("TimerOutputs")
-Pkg.add("NCDatasets")
-Pkg.add("FFTW")
-Pkg.add("Plots")
-Pkg.add("LsqFit")
-Pkg.add("OrderedCollections");
-Pkg.add("Glob");
-Pkg.add("NaturalSort")
-Pkg.add("SpecialFunctions")
-Pkg.add("Roots")
-packages = [:PackageCompiler, :TimerOutputs, :NCDatasets, :FFTW, :Plots, :LsqFit, :OrderedCollections, :Glob, :NaturalSort]
-# create the sysimage 'moment_kinetics.so' in the base moment_kinetics source directory
-# with the above pre-compiled packages
-create_sysimage(packages; sysimage_path="moment_kinetics.so")
-# remove the temporary directory
-rm("tmpdir", recursive=true)
+packages = [:moment_kinetics, :PackageCompiler, :TimerOutputs, :NCDatasets, :FFTW, :Plots, :LsqFit, :OrderedCollections, :Glob, :NaturalSort, :SpecialFunctions, :Roots]
+
+# Create the sysimage 'moment_kinetics.so' in the base moment_kinetics source directory
+# with both moment_kinetics and the dependencies listed above precompiled.
+# Warning: editing the code will not affect what runs when using this .so, you
+# need to re-precompile if you change anything.
+create_sysimage(packages; sysimage_path="moment_kinetics.so", precompile_execution_file="util/precompile_run.jl")
