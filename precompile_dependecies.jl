114bbd97c66ddade753dbed8dd8a48441da0256b
John Omotani <john.omotani@ukaea.uk>
2021-07-05 19:33:38 +0100

    Remove :Revise from the list of packages for PackageCompiler
    Seems to prevent Revise.jl from working, although I thought I read
    somewhere that pre-compiling Revise.jl was recommended.
    
    Also removes unused util/precompile_dependencies_run.jl - the only
    difference between that and util/precompile_run.jl was `using Revise`,
    which is not needed.

--- precompile_dependecies.jl
+++ precompile_dependecies.jl
@@ -5,8 +5,8 @@ Pkg.activate(".")
 
 using PackageCompiler
 
-packages = [:PackageCompiler, :TimerOutputs, :NCDatasets, :FFTW, :Plots, :LsqFit, :OrderedCollections, :Glob, :NaturalSort, :SpecialFunctions, :Roots, :TOML, :Revise]
+packages = [:PackageCompiler, :TimerOutputs, :NCDatasets, :FFTW, :Plots, :LsqFit, :OrderedCollections, :Glob, :NaturalSort, :SpecialFunctions, :Roots, :TOML]
 
-# create the sysimage 'moment_kinetics.so' in the base moment_kinetics source directory
+# create the sysimage 'dependencies.so' in the base moment_kinetics source directory
 # with the above pre-compiled packages
 create_sysimage(packages; sysimage_path="dependencies.so", precompile_execution_file="util/precompile_run.jl")
