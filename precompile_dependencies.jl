eb1e1cc8ff644a0f2935334c573e6920724fd41f
Michael Barnes <michael.barnes@physics.ox.ac.uk>
2021-07-22 14:40:34 +0100

    renamed precompile_dependecies.jl to precompile_dependencies.jl to fix typo in the name of the file.

---
+++ precompile_dependencies.jl
@@ -0,0 +1,12 @@
+using Pkg
+
+# Activate the moment_kinetics package
+Pkg.activate(".")
+
+using PackageCompiler
+
+packages = [:PackageCompiler, :TimerOutputs, :NCDatasets, :FFTW, :Plots, :LsqFit, :OrderedCollections, :Glob, :NaturalSort, :SpecialFunctions, :Roots, :TOML]
+
+# create the sysimage 'dependencies.so' in the base moment_kinetics source directory
+# with the above pre-compiled packages
+create_sysimage(packages; sysimage_path="dependencies.so", precompile_execution_file="util/precompile_run.jl")
