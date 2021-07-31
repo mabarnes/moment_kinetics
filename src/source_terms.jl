d208cc1d7f18bd1f0a81c9373f0f03535100dbfb
John Omotani <john.omotani@ukaea.uk>
2021-07-31 21:21:43 +0100

    Rearrange loops in source_terms.jl
    Optimization: avoids repeating z-derivatives of density, etc.

--- src/source_terms.jl
+++ src/source_terms.jl
@@ -10,25 +10,19 @@ function source_terms!(pdf_out, fvec_in, moments, vpa, z, dt, spectral, composit
     #n_species = size(pdf_out,3)
     if moments.evolve_ppar
         for is ∈ 1:composition.n_species
-            for ivpa ∈ 1:vpa.n
-                @views source_terms_evolve_ppar!(pdf_out[ivpa,:,is], fvec_in.pdf[ivpa,:,is],
-                                                 fvec_in.density[:,is], fvec_in.upar[:,is], fvec_in.ppar[:,is],
-                                                 moments.vth[:,is], moments.qpar[:,is], z, dt, spectral)
-            end
+            @views source_terms_evolve_ppar!(pdf_out[:,:,is], fvec_in.pdf[:,:,is],
+                                             fvec_in.density[:,is], fvec_in.upar[:,is], fvec_in.ppar[:,is],
+                                             moments.vth[:,is], moments.qpar[:,is], z, dt, spectral)
         end
         if composition.n_neutral_species > 0 && abs(CX_frequency) > 0.0
-            for ivpa ∈ 1:vpa.n
-                @views source_terms_evolve_ppar_CX!(pdf_out[ivpa,:,:], fvec_in.pdf[ivpa,:,:],
-                                                    fvec_in.density, fvec_in.ppar, composition,
-                                                    CX_frequency, dt)
-            end
+            @views source_terms_evolve_ppar_CX!(pdf_out[:,:,:], fvec_in.pdf[:,:,:],
+                                                fvec_in.density, fvec_in.ppar, composition,
+                                                CX_frequency, dt)
         end
     elseif moments.evolve_density
         for is ∈ 1:composition.n_species
-            for ivpa ∈ 1:vpa.n
-                @views source_terms_evolve_density!(pdf_out[ivpa,:,is], fvec_in.pdf[ivpa,:,is],
-                                                    fvec_in.density[:,is], fvec_in.upar[:,is], z, dt, spectral)
-            end
+            @views source_terms_evolve_density!(pdf_out[:,:,is], fvec_in.pdf[:,:,is],
+                                                fvec_in.density[:,is], fvec_in.upar[:,is], z, dt, spectral)
         end
     end
     return nothing
@@ -37,39 +31,50 @@ function source_terms_evolve_density!(pdf_out, pdf_in, dens, upar, z, dt, spectr
     # calculate d(n*upar)/dz
     @. z.scratch = dens*upar
     derivative!(z.scratch, z.scratch, z, spectral)
+    @. z.scratch *= dt/dens
     #derivative!(z.scratch, z.scratch, z, -upar, spectral)
     # update the density
-    @. pdf_out += dt*z.scratch*pdf_in/dens
+    for i in CartesianIndices(pdf_out)
+        pdf_out[i] += pdf_in[i]*z.scratch[i[2]]
+    end
     return nothing
 end
 function source_terms_evolve_ppar!(pdf_out, pdf_in, dens, upar, ppar, vth, qpar, z, dt, spectral)
     # calculate dn/dz
     derivative!(z.scratch, dens, z, spectral)
     # update the pdf to account for the density gradient contribution to the source
-    @. pdf_out += dt*z.scratch*pdf_in*upar/dens
+    @. z.scratch *= dt*upar/dens
     # calculate dvth/dz
-    derivative!(z.scratch, vth, z, spectral)
+    derivative!(z.scratch2, vth, z, spectral)
     # update the pdf to account for the -g*upar/vth * dvth/dz contribution to the source
-    @. pdf_out -= dt*z.scratch*pdf_in*upar/vth
+    @. z.scratch -= dt*z.scratch2*upar/vth
     # calculate dqpar/dz
-    derivative!(z.scratch, qpar, z, spectral)
+    derivative!(z.scratch2, qpar, z, spectral)
     # update the pdf to account for the parallel heat flux contribution to the source
-    @. pdf_out -= 0.5*dt*z.scratch*pdf_in/ppar
+    @. z.scratch -= 0.5*dt*z.scratch2/ppar
+
+    for i ∈ CartesianIndices(pdf_out)
+        pdf_out[i] += pdf_in[i] * z.scratch[i[2]]
+    end
     return nothing
 end
 function source_terms_evolve_ppar_CX!(pdf_out, pdf_in, dens, ppar, composition, CX_frequency, dt)
     for is ∈ 1:composition.n_ion_species
         for isn ∈ 1:composition.n_neutral_species
             isp = composition.n_ion_species + isn
-            @views @. pdf_out[:,is] -= 0.5*dt*pdf_in[:,is]*CX_frequency *
-                (dens[:,isp]*ppar[:,is]-dens[:,is]*ppar[:,isp])/ppar[:,is]
+            for iz in 1:size(ppar)[1]
+                @views @. pdf_out[:,iz,is] -= 0.5*dt*pdf_in[:,iz,is]*CX_frequency *
+                    (dens[iz,isp]*ppar[iz,is]-dens[iz,is]*ppar[iz,isp])/ppar[iz,is]
+            end
         end
     end
     for isn ∈ 1:composition.n_neutral_species
         is = composition.n_ion_species + isn
         for isp ∈ 1:composition.n_ion_species
-            @views @. pdf_out[:,is] -= 0.5*dt*pdf_in[:,is]*CX_frequency *
-                (dens[:,isp]*ppar[:,is]-dens[:,is]*ppar[:,isp])/ppar[:,is]
+            for iz in 1:size(ppar)[1]
+                @views @. pdf_out[:,iz,is] -= 0.5*dt*pdf_in[:,iz,is]*CX_frequency *
+                    (dens[iz,isp]*ppar[iz,is]-dens[iz,is]*ppar[iz,isp])/ppar[iz,is]
+            end
         end
     end
     return nothing
