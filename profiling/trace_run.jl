0a5e229cbf82a460f87f8aad005fddf5568aee74
John Omotani <john.omotani@ukaea.uk>
2021-08-04 20:54:30 +0100

    trace_run.jl script for simple debugging

---
+++ profiling/trace_run.jl
@@ -0,0 +1,30 @@
+using TimerOutputs
+using TOML
+
+using moment_kinetics
+
+function main(input_file)
+    input = TOML.parsefile(input_file)
+
+    test_output_directory = tempname()
+    mkpath(test_output_directory)
+    input["base_directory"] = test_output_directory
+
+    short_input = deepcopy(input)
+    short_input["nstep"] = 2
+
+    # Short run to make sure everything is compiled
+    run_moment_kinetics(TimerOutput(), short_input)
+
+    run_moment_kinetics(TimerOutput(), input)
+
+    return nothing
+end
+
+# Call main() using first argument as input_file name if running as a script
+if abspath(PROGRAM_FILE) == @__FILE__
+    if length(ARGS) < 1
+        error("Must provide input file as first command line argument")
+    end
+    main(ARGS[1])
+end
