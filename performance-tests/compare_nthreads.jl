92fd948c925003d3b735124bfbaa740599a2fc08
John Omotani <john.omotani@ukaea.uk>
2021-08-17 19:19:01 +0100

    Plotting function/script to compare different numbers of threads

--- performance-tests/compare_nthreads.jl
+++ performance-tests/compare_nthreads.jl
@@ -6,9 +6,14 @@ if length(ARGS) > 1
 else
     filename = "results/sound_wave.txt"
 end
-machine = undef
-if length(ARGS) > 1
-    compare_nthreads_performance_history(filename, machine)
+if length(ARGS) > 2
+    start_from = parse(Int, ARGS[3])
 else
-    compare_nthreads_performance_history(filename)
+    start_from = MKPlotPerformance.default_start_from
+end
+if length(ARGS) > 1
+    machine = ARGS[2]
+    compare_nthreads_performance_history(filename, machine, start_from=start_from)
+else
+    compare_nthreads_performance_history(filename, start_from=start_from)
 end
--- performance-tests/plot_performance.jl
+++ performance-tests/plot_performance.jl
@@ -10,6 +10,9 @@ using Dates
 using CSV
 using Glob
 using Plots
+using StatsBase: countmap
+
+default_start_from = -100
 
 """
 Loads performance data from a file formatted by PerformanceTestUtils.upload_result()
@@ -96,6 +99,31 @@ function get_x_ticks(commits, dates)
     xticks = (1:length(commits), ["$(a[1:6]) $(Dates.format(b, "Y-m-d"))"
                                   for (a, b) ∈ zip(commits, dates)])
     blankticks = (1:length(commits), ["" for _ ∈ 1:length(commits)])
+
+    return xticks, blankticks
+end
+
+"""
+Get nicely formatted tick labels for the x-axis
+
+Arguments
+---------
+commits : ElasticArray{String}
+    Commits to put in the tick labels
+
+Returns
+-------
+xticks : Tuple(UnitRange{Int64}, Vector{String})
+    Positions and labels for the x-ticks
+blankticks : Tuple(UnitRange{Int64, Vector{String})
+    Positions and empty labels for x-axes that don't need tick labels
+"""
+function get_x_ticks(commits)
+    # truncate commit hashes to first 6 characters and trim the time from dates
+    xticks = (1:length(commits), ["$(c[1:6])" for c ∈ commits])
+    blankticks = (1:length(commits), ["" for _ ∈ 1:length(commits)])
+
+    return xticks, blankticks
 end
 
 """
@@ -157,7 +185,8 @@ start_from : Int, default -100
     the most recent `abs(start_from)+1` recorded results. If the value is positive,
     start plotting from result number `start_from`.
 """
-function plot_performance_history(filename, machine=nothing; show=false, save=true, start_from=-100)
+function plot_performance_history(filename, machine=nothing; show=false,
+                                  save=true, start_from=default_start_from)
     if machine == nothing
         config = get_config()
         machine = config["machine"]
@@ -177,7 +206,7 @@ function plot_performance_history(filename, machine=nothing; show=false, save=tr
 
     ntests = machine_data.ntests
 
-    plots = plot(;layout=(2, 1), link=:x, yaxis=:log, grid=true, minorgrid=true, legend=:outertopright)
+    plots = plot(;layout=(2, 1), link=:x, yaxis=:log, grid=true, minorgrid=:y, legend=:outertopright)
     memplot = plots[1]
     timeplot = plots[2]
     ylabel!(memplot, "Memory usage (B)")
@@ -210,6 +239,63 @@ function plot_performance_history(filename, machine=nothing; show=false, save=tr
 end
 
 """
+Remove duplicates of any commits that appear more than once in a MachineData struct,
+checking that the memory usage is identical for all entries and taking the minimum
+run-time.
+"""
+function remove_duplicates!(d::MachineData)
+    commits = [r.commit for r ∈ d.list]
+    # First find any duplicated commits
+    n_instances = countmap(commits)
+    duplicates = [commit for (commit,count) ∈ n_instances if count > 1]
+
+    # Remove duplicates
+    for commit ∈ duplicates
+        positions = findall(c -> c == commit, commits)
+
+        for i ∈ 1:d.ntests
+            # All values of memory usage should be the same for the same code version
+            memvalues = [r.data[i].memory for r ∈ d.list[positions]]
+            # Check all entries of memvalues are the same
+            @assert all(isequal(first(memvalues)), memvalues)
+
+            # Take the minimum runtime in case some entries were affected by how busy the
+            # machine was or other external factors
+            runtimes = [x.data[i].min for x ∈ d.list[positions]]
+            d.list[positions[1]].data[i].min = minimum(runtimes)
+        end
+
+        # Remove duplicate entries
+        deleteat!(d.list, positions[2:end])
+
+        commits = [r.commit for r ∈ d.list]
+    end
+end
+
+"""
+Given a MachineData `source`, ensure all commits are present in `destination`, adding
+anything missing in `destination` with `nothing` for the entries in `destination.data`.
+Assumes that neither source nor destination have duplicated commits (use
+remove_duplicates!() on both before calling).
+"""
+function add_missing_commits!(source, destination)
+    last_index = 0
+    dest_commits = [r.commit for r in destination.list]
+    for (commit, date) ∈ collect((r.commit, r.date) for r in source.list)
+        if commit ∈ dest_commits
+            last_index = findfirst(isequal(commit), dest_commits)
+        else
+            new_entry = MachineDataRow(commit, date,
+                                       [RunData([NaN for j ∈ 1:4]...)
+                                        for i ∈ 1:destination.ntests])
+            insert!(destination.list, last_index+1, new_entry)
+            dest_commits = [r.commit for r in destination.list]
+            last_index += 1
+        end
+    end
+end
+
+"""
 Compare performance data for different numbers of threads
 
 * Plots memory usage and run time for saved test results, comparing different
@@ -222,6 +308,13 @@ Compare performance data for different numbers of threads
 * Different subplots show different test cases - for their meaning, refer to the test file
     that generated the results. Different lines show results for different
     numbers of threads used.
+* Unlike plot_performance(), removes duplicate entries for the same commit, taking the
+    minimum run-time.
+
+Bugs
+----
+* Markers do not show in legend if the first entry for that series is NaN. Guess this is
+    a bug in Plots.jl or the backend, but does not seem important enough to chase down.
 
 Arguments
 ---------
@@ -240,7 +333,9 @@ start_from : Int, default -100
     the most recent `abs(start_from)+1` recorded results. If the value is positive,
     start plotting from result number `start_from`.
 """
-function compare_nthreads_performance_history(filename, machine=nothing; show=false, save=true, start_from=-100)
+function compare_nthreads_performance_history(filename, machine=nothing;
+        show=false, save=true, start_from=default_start_from)
+
     if machine == nothing
         config = get_config()
         machine = config["machine"]
@@ -248,53 +343,88 @@ function compare_nthreads_performance_history(filename, machine=nothing; show=fa
 
     # Find filenames for different possible numbers of threads
     base, ext = splitext(filename)
-    filenames_vec = glob("$(base)_*threads$ext")
+    filenames_vec = [filename; glob("$(base)_*threads$ext")]
 
     # Load data for each number of threads present
     machine_data = Dict{Int, MachineData}()
     nthreads_vec = []
     for f ∈ filenames_vec
-        # Get number of threads from the filename...
-        # remove suffix
-        name, _ = splitext(f)
-        # separate out part of name with number of threads, e.g. "2threads"
-        threadsuffix = split(name, "_")[end]
-        # remove "threads" and convert to an Int
-        nthreads = parse(Int, split(threadsuffix, "threads")[begin])
+        if f == filename
+            nthreads = 1
+        else
+            # Get number of threads from the filename...
+            # remove suffix
+            name, _ = splitext(f)
+            # separate out part of name with number of threads, e.g. "2threads"
+            threadsuffix = split(name, "_")[end]
+            # remove "threads" and convert to an Int
+            nthreads = parse(Int, split(threadsuffix, "threads")[begin])
+        end
 
         push!(nthreads_vec, nthreads)
 
-        data = load_run(f)
-        split_data = split_machines(data)
-        machine_data[nthreads] = split_data[machine]
+        all_data = load_run(f)
+        split_data = split_machines(all_data)
+        this_data = split_data[machine]
+        remove_duplicates!(this_data)
+        machine_data[nthreads] = this_data
     end
 
-    start_index = (start_from <= 0 ? max(length(machine_data.commits) + start_from, 1)
-                                   : min(start_from, length(machine_data.commits)))
+    n = length(nthreads_vec)
+    for i ∈ 1:n
+        for j ∈ 1:n
+            if i ≠ j
+                add_missing_commits!(machine_data[nthreads_vec[i]],
+                                     machine_data[nthreads_vec[j]])
+            end
+        end
+    end
 
-    commits = [x[1:6] for x in machine_data.commits[start_index:end]]
+    commits = [r.commit[1:6] for r ∈ machine_data[nthreads_vec[1]].list]
 
-    dates = machine_data.dates[start_index:end]
+    ncommits = length(commits)
+    start_index = (start_from <= 0 ? max(ncommits + start_from, 1)
+                                   : min(start_from, ncommits))
 
-    ntests = machine_data.ntests
+    commits = commits[start_index:end]
+
+    ntests = machine_data[nthreads_vec[1]].ntests
 
     nx, ny = square_grid_sizes(ntests)
-    memplots = plot(;layout=(nx, ny), link=:x, yaxis=:log, grid=true,
-                     minorgrid=true, legend=:outertopright)
-    timeplots = plot(;layout=(nx, ny), link=:x, yaxis=:log, grid=true,
-                      minorgrid=true, legend=:outertopright)
-    ylabel!(memplots, "Memory usage (B)")
-    xlabel!(memplots, "commit")
-    ylabel!(timeplots, "Run time (s)")
-    xlabel!(timeplots, "commit")
+    fontsize = 4
+    markersize = 3
+    #yaxis = :log
+    yaxis = ((0.0, Inf),)
+    memplots = plot(;layout=(nx, ny), yaxis=yaxis, grid=true,
+                     minorgrid=:y, legend=:outertopright,
+                     xtickfontsize=fontsize, ytickfontsize=fontsize,
+                     legendfontsize=fontsize)
+    timeplots = plot(;layout=(nx, ny), yaxis=yaxis, grid=true,
+                      minorgrid=:y, legend=:outertopright,
+                      xtickfontsize=fontsize, ytickfontsize=fontsize,
+                      legendfontsize=fontsize)
+    # Don't know how to make nice labels outside a grid of subplots
+    #ylabel!(memplots, "Memory usage (B)")
+    #xlabel!(memplots, "commit")
+    #ylabel!(timeplots, "Run time (s)")
+    #xlabel!(timeplots, "commit")
 
-    xticks, blankticks = get_x_ticks(commits, dates)
+    # Make extra subplots blank - see
+    # https://discourse.julialang.org/t/blank-subplot-in-plots-jl/16453
+    for i ∈ ntests+1:nx*ny
+        p = memplots[i]
+        plot!(p, legend=false,grid=false,foreground_color_subplot=:white)
+        p = timeplots[i]
+        plot!(p, legend=false,grid=false,foreground_color_subplot=:white)
+    end
+
+    xticks, blankticks = get_x_ticks(commits)
     for i ∈ 1:ntests
         p = memplots[i]
         for nthreads ∈ nthreads_vec
-            perf_data = machine_data[nthreads].data[i]
-            plot!(p, perf_data[1, start_index:end];
-                  marker=:x, label="$nthreads")
+            mem_data = [r.data[i].memory
+                        for r ∈ machine_data[nthreads].list[start_index:end]]
+            plot!(p, mem_data; marker=:x, markersize=markersize, label="$nthreads")
         end
         if is_bottom(i, ntests, nx)
             xticks!(p, xticks, xrotation=45)
@@ -303,12 +433,12 @@ function compare_nthreads_performance_history(filename, machine=nothing; show=fa
         end
         p = timeplots[i]
         for nthreads ∈ nthreads_vec
-            perf_data = machine_data[nthreads].data[i]
-            plot!(p, perf_data[2, start_index:end];
-              marker=:x,
-              yerror=(0.0, perf_data[3, start_index:end]
-                           - perf_data[2, start_index:end]),
-              label="$nthreads")
+            min_data = [r.data[i].min
+                        for r ∈ machine_data[nthreads].list[start_index:end]]
+            med_data = [r.data[i].median
+                        for r ∈ machine_data[nthreads].list[start_index:end]]
+            plot!(p, min_data; marker=:x, markersize=markersize,
+                  yerror=[(0.0, e) for e ∈ (med_data - min_data)], label="$nthreads")
         end
         if is_bottom(i, ntests, nx)
             xticks!(p, xticks, xrotation=45)
@@ -323,10 +453,11 @@ function compare_nthreads_performance_history(filename, machine=nothing; show=fa
 
     if save
         run_type = split(basename(filename), ".")[1]
-        savefig(plots, string(run_type, ".pdf"))
+        savefig(memplots, string(run_type, "_compare_threads_memory.pdf"))
+        savefig(timeplots, string(run_type, "_compare_threads_runtime.pdf"))
     end
 
-    return plots
+    return memplots, timeplots
 end
 
 end # MKPlotPerformance
@@ -335,15 +466,20 @@ using .MKPlotPerformance
 
 if abspath(PROGRAM_FILE) == @__FILE__
     filename = undef
-    if length(ARGS) > 1
+    if length(ARGS) > 0
         filename = ARGS[1]
     else
         filename = "results/sound_wave.txt"
     end
-    machine = undef
-    if length(ARGS) > 1
-        plot_performance_history(filename, machine)
+    if length(ARGS) > 2
+        start_from = parse(Int, ARGS[3])
     else
-        plot_performance_history(filename)
+        start_from = MKPlotPerformance.default_start_from
+    end
+    if length(ARGS) > 1
+        machine = ARGS[2]
+        plot_performance_history(filename, machine, start_from=start_from)
+    else
+        plot_performance_history(filename, start_from=start_from)
     end
 end
