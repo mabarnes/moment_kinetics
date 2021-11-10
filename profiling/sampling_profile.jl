42cc4a2eb961eaefdb552181ec1afae56923a149
John Omotani <john.omotani@ukaea.uk>
2021-11-10 18:57:37 +0100

    When running sampling profile, only profile time_advance!()
    Makes reported statistics easier to read, as it doesn't seem to be
    possible to filter/restrict the profile in the browser after running.

--- profiling/sampling_profile.jl
+++ profiling/sampling_profile.jl
@@ -2,7 +2,7 @@ using Profile
 using StatProfilerHTML
 using TOML
 
-using moment_kinetics: run_moment_kinetics, options
+using moment_kinetics: run_moment_kinetics, setup_moment_kinetics, time_advance!, cleanup_moment_kinetics!, options
 using moment_kinetics.communication: block_rank
 
 function main(input_file)
@@ -18,8 +18,15 @@ function main(input_file)
     # Short run to make sure everything is compiled
     run_moment_kinetics(short_input)
 
+    ## profile including setup/cleanup calls
+    #Profile.clear()
+    #Profile.@profile run_moment_kinetics(input)
+
+    # Don't profile initialization
+    mk_state = setup_moment_kinetics(input)
     Profile.clear()
-    Profile.@profile run_moment_kinetics(input)
+    # Do profile time_advance!()
+    Profile.@profile time_advance!(mk_state...)
 
     # Produce html output
     statprofilehtml(path="statprof/profile$(block_rank[])")
@@ -27,6 +34,9 @@ function main(input_file)
     # Print to stdout
     # Use IOContext to increase width so that lines don't get trucated
     block_rank[] == 0 && Profile.print(IOContext(stdout, :displaysize => (24, 500)))
+
+    # Clean up, without profiling
+    cleanup_moment_kinetics!(mk_state[end-1:end]...)
 
     return nothing
 end
