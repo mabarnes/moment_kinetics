aa1304be5b98105865d67f40fbc4a1b4b2fa5e15
John Omotani <john.omotani@ukaea.uk>
2022-01-02 20:08:59 +0000

    Workaround to make precompilation work with MPI
    See https://github.com/JuliaParallel/MPI.jl/issues/518.

--- precompile.jl
+++ precompile.jl
@@ -11,4 +11,8 @@ packages = [:moment_kinetics, :PackageCompiler, :ArgParse, :Combinatorics, :Deli
 # with both moment_kinetics and the dependencies listed above precompiled.
 # Warning: editing the code will not affect what runs when using this .so, you
 # need to re-precompile if you change anything.
-create_sysimage(packages; sysimage_path="moment_kinetics.so", precompile_execution_file="util/precompile_run.jl")
+create_sysimage(packages;
+                sysimage_path="moment_kinetics.so",
+                precompile_execution_file="util/precompile_run.jl",
+                include_transitive_dependencies=false, # This is needed to make MPI work, see https://github.com/JuliaParallel/MPI.jl/issues/518
+               )
--- precompile_dependencies.jl
+++ precompile_dependencies.jl
@@ -9,4 +9,8 @@ packages = [:PackageCompiler, :ArgParse, :Combinatorics, :DelimitedFiles, :FFTW,
 
 # create the sysimage 'dependencies.so' in the base moment_kinetics source directory
 # with the above pre-compiled packages
-create_sysimage(packages; sysimage_path="dependencies.so", precompile_execution_file="util/precompile_run_short.jl")
+create_sysimage(packages;
+                sysimage_path="dependencies.so",
+                precompile_execution_file="util/precompile_run_short.jl",
+                include_transitive_dependencies=false, # This is needed to make MPI work, see https://github.com/JuliaParallel/MPI.jl/issues/518
+               )
