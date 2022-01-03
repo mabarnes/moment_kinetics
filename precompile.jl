e3e787ec8ce52228b1b5883d6e0e3694c8695eef
John Omotani <john.omotani@ukaea.uk>
2022-01-03 00:58:26 +0000

    Pass optimization arguments when building static system image
    If we are precompiling, presumably want an optimized, production build
    so always pass `-O3 --check-bounds=no` to the build process.

--- precompile.jl
+++ precompile.jl
@@ -15,4 +15,5 @@ create_sysimage(packages;
                 sysimage_path="moment_kinetics.so",
                 precompile_execution_file="util/precompile_run.jl",
                 include_transitive_dependencies=false, # This is needed to make MPI work, see https://github.com/JuliaParallel/MPI.jl/issues/518
+                sysimage_build_args=`-O3 --check-bounds=no`, # Assume if we are precompiling we want an optimized, production build
                )
