5edb279b8e41676f4ab6e1e16c46b78102317cea
Michael Barnes <michael.barnes@physics.ox.ac.uk>
2022-01-14 15:08:59 +0000

    more experimenting with comments in charge_exchange.jl to track down debug-check issues that I cannot reproduce on my laptop.

--- src/charge_exchange.jl
+++ src/charge_exchange.jl
@@ -45,9 +45,9 @@ function charge_exchange_collisions!(f_out, fvec_in, moments, composition, vpa, 
                             # calculate the shift in the wpa grid to the desired (ion) wpa locations
                             wpa_shift = (fvec_in.upar[iz,isi] - fvec_in.upar[iz,isn])/wpa_shift_norm
                             # construct the wpa grid on which to interpolate
-                            for ivpa ∈ 1:vpa.n
-                                vpa.scratch[ivpa] = vpa.grid[ivpa] + wpa_shift
-                            end
+                            # for ivpa ∈ 1:vpa.n
+                            #     vpa.scratch[ivpa] = vpa.grid[ivpa] + wpa_shift
+                            # end
                             # interpolate to the new grid (passed in as vpa.scratch)
                             # and return interpolated values in vpa.scratch
                             println("!!TEMPORARY COMMENTING OF VPA.SCRATCH IN CHARGE_EXCHANGE FOR TESTING!!")
@@ -55,12 +55,14 @@ function charge_exchange_collisions!(f_out, fvec_in, moments, composition, vpa, 
                             # add the charge exchange contribution to df/dt
                             for ivpa ∈ 1:vpa.n
                                 f_out[ivpa,iz,isi] += dt*charge_exchange_frequency*fvec_in.density[iz,isn] *
-                                    (vpa.scratch[ivpa] - fvec_in.pdf[ivpa,iz,isi])
+                                    (fvec_in.pdf[ivpa,iz,isn] - fvec_in.pdf[ivpa,iz,isi])
+                                    #(vpa.scratch[ivpa] - fvec_in.pdf[ivpa,iz,isi])
                             end
                         end
                     end
+                end
                 # repeat the above interpolation process to get the ion pdf on the neutral vpa grid
-                elseif is ∈ composition.ion_species_range
+                if is ∈ composition.ion_species_range
                     # identify the 'is' index as corresponding to an ion species
                     isi = is
                     @s_z_loop_z iz begin
@@ -75,9 +77,9 @@ function charge_exchange_collisions!(f_out, fvec_in, moments, composition, vpa, 
                             # calculate the shift in the wpa grid to the desired (neutral) wpa locations
                             wpa_shift = (fvec_in.upar[iz,isn] - fvec_in.upar[iz,isi])/wpa_shift_norm
                             # construct the wpa grid on which to interpolate
-                            for ivpa ∈ 1:vpa.n
-                                vpa.scratch[ivpa] = vpa.grid[ivpa] + wpa_shift
-                            end
+                            # for ivpa ∈ 1:vpa.n
+                            #     vpa.scratch[ivpa] = vpa.grid[ivpa] + wpa_shift
+                            # end
                             # interpolate to the new grid (passed in as vpa.scratch)
                             # and return interpolated values in vpa.scratch
                             println("!!TEMPORARY COMMENTING OF VPA.SCRATCH IN CHARGE_EXCHANGE FOR TESTING!!")
@@ -85,7 +87,8 @@ function charge_exchange_collisions!(f_out, fvec_in, moments, composition, vpa, 
                             # add the charge exchange contribution to df/dt
                             for ivpa ∈ 1:vpa.n
                                 f_out[ivpa,iz,isn] += dt*charge_exchange_frequency*fvec_in.density[iz,isi] *
-                                    (vpa.scratch[ivpa]- fvec_in.pdf[ivpa,iz,isn])
+                                    (fvec_in.pdf[ivpa,iz,isi] - fvec_in.pdf[ivpa,iz,isn])
+                                    #(vpa.scratch[ivpa]- fvec_in.pdf[ivpa,iz,isn])
                             end
                         end
                     end
