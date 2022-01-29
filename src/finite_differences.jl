318df88b7e9e3077065eeeae88f80bb13592e998
John Omotani <john.omotani@ukaea.uk>
2022-01-29 14:36:12 +0000

    Cubic Hermite spline interpolation for finite difference mode
    In-place implementation of cubic Hermite spline interpolation
    (https://en.wikipedia.org/wiki/Cubic_Hermite_spline). Much faster than
    B-splines from Interpolations.jl because Hermite splines do not match
    the curvature, so do not require solving an equation system.

--- src/finite_differences.jl
+++ src/finite_differences.jl
@@ -5,7 +5,6 @@ export derivative_finite_difference!
 using Interpolations
 
 using ..type_definitions: mk_float
-import ..interpolation: interpolate_to_grid_1d!
 
 function fd_check_option(option, ngrid)
     if option == "second_order_upwind"
@@ -412,58 +411,6 @@ function centered_fourth_order!(df::Array{mk_float,2}, f, del, bc, igrid, ieleme
         for j ∈ 2:ielement[end]
             df[1, j] = df[end, j-1]
         end
-end
-
-"""
-Interpolation from a regular grid to a 1d grid with arbitrary spacing
-
-Arguments
----------
-new_grid : Array{mk_float, 1}
-    Grid of points to interpolate `coord` to
-f : Array{mk_float}
-    Field to be interpolated
-coord : coordinate
-    `coordinate` struct giving the coordinate along which f varies
-not_spectral : Bool
-    A Bool argument here indicates that the coordinate is not spectral-element
-    discretized, i.e. it is on a uniform ('finite difference') grid.
-"""
-function interpolate_to_grid_1d!(result, new_grid, f, coord, not_spectral::Bool)
-    # Convert coord to a range, assuming it is uniformly spaced
-    x = coord.grid
-    x_range = range(x[begin], x[end], length=size(x)[1])
-    unscaled = interpolate(f, BSpline(Cubic(Periodic(OnCell()))))
-    interpolation_function = scale(unscaled, x_range)
-    # check to see if any of the new_grid points are to the left of the first grid point
-    n_new = size(new_grid)[1]
-    start_idx = 1
-    for j ∈ 1:n_new
-        # if the new grid location is outside the bounds of the original grid,
-        # extrapolate f assuming f is constant beyond the domain
-        if new_grid[j] < coord.grid[1]
-            start_idx += 1
-            result[j] = f[1]
-        else
-            break
-        end
-    end
-    # check for new_grid points beyond the final grid location
-    end_idx = n_new
-    for j ∈ n_new:-1:1
-        # if the new grid location is outside the bounds of the original grid,
-        # extrapolate f assuming f is constant beyond the domain
-        if new_grid[j] > coord.grid[end]
-            end_idx -= 1
-            result[j] = f[end]
-        else
-            break
-        end
-    end
-    interp_f = interpolation_function(view(new_grid,start_idx:end_idx))
-    result[start_idx:end_idx] .= interp_f
-
-    return nothing
 end
 
 end
