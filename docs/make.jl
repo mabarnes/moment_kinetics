fedfb82fbfc82a68e2e4924b827b348a2fb6b807
John Omotani <john.omotani@ukaea.uk>
2022-02-04 16:16:26 +0000

    Setup make.jl to deploy docs to web page

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,11 @@ makedocs(
     modules = [moment_kinetics]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+if get(ENV, "CI", nothing) == "true"
+    # Documenter can also automatically deploy documentation to gh-pages.
+    # See "Hosting Documentation" and deploydocs() in the Documenter manual
+    # for more information.
+    deploydocs(
+        repo = "github.com/mabarnes/moment_kinetics"
+    )
+end
