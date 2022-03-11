964363ada1520c9a39872c811416781c55aa91d4
John Omotani <john.omotani@ukaea.uk>
2022-03-11 17:56:25 +0000

    Complete calculus tests of lagrange_uniform

--- test/calculus_tests.jl
+++ test/calculus_tests.jl
@@ -1048,101 +1048,101 @@ function runtests()
             end
         end
 
-        #@testset "Lagrange uniform-grid derivatives (4 argument), Neumann" verbose=false begin
-        #    @testset "$nelement $ngrid" for bc ∈ ("constant", "zero"),
-        #            nelement ∈ (1:5), ngrid ∈ (3:33)
+        @testset "Lagrange uniform-grid derivatives (4 argument), Neumann" verbose=false begin
+            @testset "$nelement $ngrid" for bc ∈ ("constant", "zero"),
+                    nelement ∈ (1:5), ngrid ∈ (3:14)
 
-        #        # define inputs needed for the test
-        #        L = 1.0
-        #        bc = "constant"
-        #        # fd_option and adv_input not actually used so given values unimportant
-        #        fd_option = ""
-        #        adv_input = advection_input("default", 1.0, 0.0, 0.0)
-        #        # create the 'input' struct containing input info needed to create a
-        #        # coordinate
-        #        input = grid_input("coord", ngrid, nelement, L,
-        #            "lagrange_uniform", fd_option, bc, adv_input)
-        #        # create the coordinate struct 'x'
-        #        x, spectral = define_coordinate(input)
+                # define inputs needed for the test
+                L = 1.0
+                bc = "constant"
+                # fd_option and adv_input not actually used so given values unimportant
+                fd_option = ""
+                adv_input = advection_input("default", 1.0, 0.0, 0.0)
+                # create the 'input' struct containing input info needed to create a
+                # coordinate
+                input = grid_input("coord", ngrid, nelement, L,
+                    "lagrange_uniform", fd_option, bc, adv_input)
+                # create the coordinate struct 'x'
+                x, spectral = define_coordinate(input)
 
-        #        # test polynomials up to order ngrid-1
-        #        for n ∈ 0:ngrid-1
-        #            # create array for the function f(x) to be differentiated/integrated
-        #            f = Array{Float64,1}(undef, x.n)
-        #            # create array for the derivative df/dx and the expected result
-        #            df = similar(f)
-        #            expected_df = similar(f)
-        #            # initialize f and expected df
-        #            f[:] .= randn(rng)
-        #            expected_df .= 0.0
-        #            for p ∈ 1:n
-        #                coefficient = randn(rng)
-        #                @. f += coefficient * x.grid ^ p
-        #                @. expected_df += coefficient * p * x.grid ^ (p - 1)
-        #            end
-        #            # differentiate f
-        #            derivative!(df, f, x, spectral)
+                # test polynomials up to order ngrid-1
+                for n ∈ 0:ngrid-1
+                    # create array for the function f(x) to be differentiated/integrated
+                    f = Array{Float64,1}(undef, x.n)
+                    # create array for the derivative df/dx and the expected result
+                    df = similar(f)
+                    expected_df = similar(f)
+                    # initialize f and expected df
+                    f[:] .= randn(rng)
+                    expected_df .= 0.0
+                    for p ∈ 1:n
+                        coefficient = randn(rng)
+                        @. f += coefficient * x.grid ^ p
+                        @. expected_df += coefficient * p * x.grid ^ (p - 1)
+                    end
+                    # differentiate f
+                    derivative!(df, f, x, spectral)
 
-        #            # Note the error we might expect for a p=32 polynomial is probably
-        #            # something like p*(round-off) for x^p (?) so error on expected_df would
-        #            # be p*p*(round-off), or plausibly 1024*(round-off), so tolerance of
-        #            # 3e-11 isn't unreasonable.
-        #            @test isapprox(df, expected_df, rtol=3.0e-11, atol=2.0e-12,
-        #                           norm=maxabs_norm)
-        #        end
-        #    end
-        #end
+                    # Note the error we might expect for a p=32 polynomial is probably
+                    # something like p*(round-off) for x^p (?) so error on expected_df would
+                    # be p*p*(round-off), or plausibly 1024*(round-off), so tolerance of
+                    # 3e-11 isn't unreasonable.
+                    @test isapprox(df, expected_df, rtol=1.0e-10, atol=1.0e-11,
+                                   norm=maxabs_norm)
+                end
+            end
+        end
 
-        #@testset "Lagrange uniform-grid derivatives upwinding (5 argument), Neumann" verbose=false begin
-        #    @testset "$nelement $ngrid" for bc ∈ ("constant", "zero"),
-        #            nelement ∈ (1:5), ngrid ∈ (3:33)
+        @testset "Lagrange uniform-grid derivatives upwinding (5 argument), Neumann" verbose=false begin
+            @testset "$nelement $ngrid" for bc ∈ ("constant", "zero"),
+                    nelement ∈ (1:5), ngrid ∈ (3:15)
 
-        #        # define inputs needed for the test
-        #        L = 1.0
-        #        bc = "constant"
-        #        # fd_option and adv_input not actually used so given values unimportant
-        #        fd_option = ""
-        #        adv_input = advection_input("default", 1.0, 0.0, 0.0)
-        #        # create the 'input' struct containing input info needed to create a
-        #        # coordinate
-        #        input = grid_input("coord", ngrid, nelement, L,
-        #            "lagrange_uniform", fd_option, bc, adv_input)
-        #        # create the coordinate struct 'x'
-        #        x, spectral = define_coordinate(input)
+                # define inputs needed for the test
+                L = 1.0
+                bc = "constant"
+                # fd_option and adv_input not actually used so given values unimportant
+                fd_option = ""
+                adv_input = advection_input("default", 1.0, 0.0, 0.0)
+                # create the 'input' struct containing input info needed to create a
+                # coordinate
+                input = grid_input("coord", ngrid, nelement, L,
+                    "lagrange_uniform", fd_option, bc, adv_input)
+                # create the coordinate struct 'x'
+                x, spectral = define_coordinate(input)
 
-        #        # test polynomials up to order ngrid-1
-        #        for n ∈ 0:ngrid-1
-        #            # create array for the function f(x) to be differentiated/integrated
-        #            f = Array{Float64,1}(undef, x.n)
-        #            # create array for the derivative df/dx and the expected result
-        #            df = similar(f)
-        #            expected_df = similar(f)
-        #            # initialize f and expected df
-        #            f[:] .= randn(rng)
-        #            expected_df .= 0.0
-        #            for p ∈ 1:n
-        #                coefficient = randn(rng)
-        #                @. f += coefficient * x.grid ^ p
-        #                @. expected_df += coefficient * p * x.grid ^ (p - 1)
-        #            end
+                # test polynomials up to order ngrid-1
+                for n ∈ 0:ngrid-1
+                    # create array for the function f(x) to be differentiated/integrated
+                    f = Array{Float64,1}(undef, x.n)
+                    # create array for the derivative df/dx and the expected result
+                    df = similar(f)
+                    expected_df = similar(f)
+                    # initialize f and expected df
+                    f[:] .= randn(rng)
+                    expected_df .= 0.0
+                    for p ∈ 1:n
+                        coefficient = randn(rng)
+                        @. f += coefficient * x.grid ^ p
+                        @. expected_df += coefficient * p * x.grid ^ (p - 1)
+                    end
 
-        #            for advection ∈ (-1.0, 0.0, 1.0)
-        #                adv_fac = similar(f)
-        #                adv_fac .= advection
+                    for advection ∈ (-1.0, 0.0, 1.0)
+                        adv_fac = similar(f)
+                        adv_fac .= advection
 
-        #                # differentiate f
-        #                derivative!(df, f, x, adv_fac, spectral)
+                        # differentiate f
+                        derivative!(df, f, x, adv_fac, spectral)
 
-        #                # Note the error we might expect for a p=32 polynomial is probably
-        #                # something like p*(round-off) for x^p (?) so error on expected_df
-        #                # would be p*p*(round-off), or plausibly 1024*(round-off), so
-        #                # tolerance of 3e-11 isn't unreasonable.
-        #                @test isapprox(df, expected_df, rtol=3.0e-11, atol=3.0e-12,
-        #                               norm=maxabs_norm)
-        #            end
-        #        end
-        #    end
-        #end
+                        # Note the error we might expect for a p=32 polynomial is probably
+                        # something like p*(round-off) for x^p (?) so error on expected_df
+                        # would be p*p*(round-off), or plausibly 1024*(round-off), so
+                        # tolerance of 3e-11 isn't unreasonable.
+                        @test isapprox(df, expected_df, rtol=2.0e-10, atol=1.0e-11,
+                                       norm=maxabs_norm)
+                    end
+                end
+            end
+        end
     end
 end
 
