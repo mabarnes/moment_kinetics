6e080bd32a3135a6dacacd70c3cf2d42cbe11a3d
Michael Barnes <michael.barnes@physics.ox.ac.uk>
2022-03-24 19:28:48 +0000

    tidying up of code in charge_exchange in preparation for adding interpolation between different species vpa grids

--- src/charge_exchange.jl
+++ src/charge_exchange.jl
@@ -18,16 +18,8 @@ function charge_exchange_collisions!(f_out, fvec_in, moments, composition, vpa, 
                 # for each ion species, obtain affect of charge exchange collisions
                 # with all of the neutral species
                 for isp ∈ composition.neutral_species_range
-                    @loop_r_z_vpa ir iz ivpa begin
-                        if moments.evolve_ppar
-                            vth_ratio = moments.vth[iz,ir,is]/moments.vth[iz,ir,isp]
-                        else
-                            vth_ratio = 1.0
-                        end
-                        f_out[ivpa,iz,ir,is] +=
-                        dt*charge_exchange_frequency*fvec_in.density[iz,ir,isp]*
-                        (fvec_in.pdf[ivpa,iz,ir,isp]*vth_ratio - fvec_in.pdf[ivpa,iz,ir,is])
-                    end
+                    @views charge_exchange_collisions_single_species!(f_out[:,:,:,is], fvec_in,
+                        moments, charge_exchange_frequency, dt, is, isp)
                 end
             end
             # apply CX collisions to all neutral species
@@ -36,16 +28,8 @@ function charge_exchange_collisions!(f_out, fvec_in, moments, composition, vpa, 
                 # with all of the ion species
                 for isp ∈ composition.ion_species_range
                     #cxfac = dt*charge_exchange_frequency
-                    @loop_r_z_vpa ir iz ivpa begin
-                        if moments.evolve_ppar
-                            vth_ratio = moments.vth[iz,ir,is]/moments.vth[iz,ir,isp]
-                        else
-                            vth_ratio = 1.0
-                        end
-                        f_out[ivpa,iz,ir,is] +=
-                        dt*charge_exchange_frequency*fvec_in.density[iz,ir,isp]*
-                        (fvec_in.pdf[ivpa,iz,ir,isp]*vth_ratio - fvec_in.pdf[ivpa,iz,ir,is])
-                    end
+                    @views charge_exchange_collisions_single_species!(f_out[:,:,:,is], fvec_in,
+                        moments, charge_exchange_frequency, dt, is, isp)
                 end
             end
         end
@@ -81,6 +65,26 @@ function charge_exchange_collisions!(f_out, fvec_in, moments, composition, vpa, 
                 end
             end
         end
+    end
+end
+
+"""
+"""
+function charge_exchange_collisions_single_species!(f_out, fvec_in, moments, charge_exchange_frequency, dt, is, isp)
+    @loop_r_z_vpa ir iz ivpa begin
+        # if the parallel flow and/or the parallel pressure are separately evolved,
+        # then the parallel velocity coordinate is re-defined so that the jth
+        # vpa grid point for different species corresponds to different physical
+        # values of dz/dt; as charge exchange and ionization collisions require
+        # the evaluation of the pdf for species s' to obtain the update for species s,
+        # will thus have to interpolate between the different vpa grids
+        if moments.evolve_ppar
+            vth_ratio = moments.vth[iz,ir,is]/moments.vth[iz,ir,isp]
+        else
+            vth_ratio = 1.0
+        end
+        f_out[ivpa,iz,ir] += dt * charge_exchange_frequency * fvec_in.density[iz,ir,isp] *
+            (fvec_in.pdf[ivpa,iz,ir,isp] * vth_ratio - fvec_in.pdf[ivpa,iz,ir,is])
     end
 end
 
