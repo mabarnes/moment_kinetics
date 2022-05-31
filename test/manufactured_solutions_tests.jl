6ce18fb77180ca464fe8d47ccd74c2b25e29641b
John Omotani <john.omotani@ukaea.uk>
2022-05-31 10:34:02 +0100

    Move some MMS utility functions to mms_utils.jl so they can be shared

--- test/manufactured_solutions_tests.jl
+++ test/manufactured_solutions_tests.jl
@@ -4,6 +4,7 @@ Test cases using the method of manufactured solutions (MMS)
 module ManufacturedSolutionsTests
 
 include("setup.jl")
+include("mms_utils.jl")
 
 using moment_kinetics.post_processing: L2_error_norm, L_infinity_error_norm
 using moment_kinetics.manufactured_solns
@@ -125,68 +126,6 @@ function runcase(input::Dict)
     end
 
     return n_error_2, n_error_inf, phi_error_2, phi_error_inf, f_error_2, f_error_inf
-end
-
-"""
-    increase_resolution(input::Dict, factor)
-
-Increase resolution of simulation by multiplying the numbers of elements `*_nelement` in
-the `input` settings by `factor`.
-"""
-function increase_resolution(input::Dict, nelement)
-    result = copy(input)
-    result["run_name"] = input["run_name"] * "_$nelement"
-    for key ∈ keys(result)
-        if occursin("_nelement", key)
-            result[key] = nelement
-        end
-    end
-
-    return result
-end
-
-"""
-    get_and_check_ngrid(input::Dict)
-
-Get value of `ngrid` and check that it is the same for all dimensions. `ngrid` needs to
-be the same as it sets the convergence order, and we want this to be the same for all
-operators.
-"""
-function get_and_check_ngrid(input::Dict)::mk_int
-    ngrid = nothing
-
-    for key ∈ keys(input)
-        if occursin("_ngrid", key)
-            if ngrid === nothing
-                ngrid = input[key]
-            else
-                if ngrid != input[key]
-                    error("*_ngrid should all be the same, but $key=$(input[key]) when "
-                          * "we already found ngrid=$ngrid")
-                end
-            end
-        end
-    end
-
-    return ngrid
-end
-
-"""
-    test_error_series(errors::Vector{mk_float}, resolution_factors::Vector,
-                      expected_order, expected_lowest)
-
-Test whether the error norms in `errors` converge as expected with increases in
-resolution by `resolution_factors`. `expected_order` is the order p such that the error
-is expected to be proportional to h^p. `expected_lowest` is the expected value of the
-error at the lowest resolution (used as a regression test).
-
-Note the entries in `errors` and `resolution_factors` should be sorted in increasing
-order of `resolution_factors`.
-"""
-function test_error_series(errors::Vector{mk_float}, resolution_factors::Vector,
-                           expected_order, expected_lowest)
-    error_factors = errors[1:end-1] ./ errors[2:end]
-    expected_factors = resolution_factors[2:end].^expected_order
 end
 
 """
