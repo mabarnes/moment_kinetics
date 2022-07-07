a3c58738d0eedf6fd72814938d2a97e55f88e954
John Omotani <john.omotani@ukaea.uk>
2022-07-07 18:01:37 +0100

    Modified method to enforce constraints, less perturbation at large vpa
    Change the prefactors in terms added to enforce moment constraints
    (dividing by (1+vpa^2)), so they have less effect at large vpa.

--- src/moment_constraints.jl
+++ src/moment_constraints.jl
@@ -161,21 +161,35 @@ function hard_force_moment_constraints!(f, moments, vpa)
         I0 = integrate_over_vspace(f, vpa.wgts)
         I1 = integrate_over_vspace(f, vpa.grid, vpa.wgts)
         I2 = integrate_over_vspace(f, vpa.grid, 2, vpa.wgts)
-        I3 = integrate_over_vspace(f, vpa.grid, 3, vpa.wgts)
-        I4 = integrate_over_vspace(f, vpa.grid, 4, vpa.wgts)
-        A = ((1.0 - 0.5*I2/I4)*(I2 - I3^2/I4) + 0.5*I3/I4*(I1-I2*I3/I4)) /
-            ((I0 - I2^2/I4)*(I2 - I3^2/I4) - (I1 - I2*I3/I4)^2)
-        B = -(0.5*I3/I4 + A*(I1 - I2*I3/I4)) / (I2 - I3^2/I4)
-        C = -(A*I1 + B*I2) / I3
-        @. f = A*f + B*vpa.grid*f + C*vpa.grid*vpa.grid*f
+
+        # Divide by factor so that corrections do not have large effect at large
+        # w_parallel
+        @. vpa.scratch = f / (1.0 + vpa.grid*vpa.grid)
+        J1 = integrate_over_vspace(vpa.scratch, vpa.grid, vpa.wgts)
+        J2 = integrate_over_vspace(vpa.scratch, vpa.grid, 2, vpa.wgts)
+        J3 = integrate_over_vspace(vpa.scratch, vpa.grid, 3, vpa.wgts)
+        J4 = integrate_over_vspace(vpa.scratch, vpa.grid, 4, vpa.wgts)
+
+        A = (J3^2 - J2*J4 + 0.5*(J2^2 - J1*J3)) /
+            (I0*(J3^2 - J2*J4) + I1*(J1*J4 - J2*J3) + I2*(J2^2 - J1*J3))
+        B = (0.5*J3 + A*(I1*J4 - I2*J3)) / (J3^2 - J2*J4)
+        C = -(A*I1 + B*J2) / J3
+
+        @. f = A*f + B*vpa.grid*vpa.scratch + C*vpa.grid*vpa.grid*vpa.scratch
     elseif moments.evolve_upar
         I0 = integrate_over_vspace(f, vpa.wgts)
         I1 = integrate_over_vspace(f, vpa.grid, vpa.wgts)
-        I2 = integrate_over_vspace(f, vpa.grid, 2, vpa.wgts)
-        A = 1.0 / (I0 + I1*I1/I2)
-        B = -I1*A/I2
-        @. f = A*f + B*vpa.grid*f
-    elseif moments.evolve_density
+        # Divide by factor so that corrections do not have large effect at large
+        # w_parallel
+        @. vpa.scratch = f / (1.0 + vpa.grid*vpa.grid)
+        J1 = integrate_over_vspace(vpa.scratch, vpa.grid, vpa.wgts)
+        J2 = integrate_over_vspace(vpa.scratch, vpa.grid, 2, vpa.wgts)
+
+        A = 1.0 / (I0 - I1*J1/J2)
+        B = -A*I1/J2
+
+        @. f = A*f + B*vpa.grid*vpa.scratch
+    elseif moment.evolve_density
         I0 = integrate_over_vspace(f, vpa.wgts)
         @. f = f / I0
     end
