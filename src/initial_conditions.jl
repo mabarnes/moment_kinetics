0669aae12d1b91633ff336e27b6b7f778505db00
John Omotani <john.omotani@ukaea.uk>
2022-07-08 17:53:50 +0100

    Minor fixes for parallelisation in boundary conditions

--- src/initial_conditions.jl
+++ src/initial_conditions.jl
@@ -375,6 +375,7 @@ also enforce boundary conditions in z on all separately evolved velocity space m
 """
 function enforce_boundary_conditions!(f_out, density, upar, ppar, moments, vpa_bc, z_bc, vpa,
         z, r, vpa_adv, z_adv, composition)
+    begin_s_r_z_region()
     @loop_s_r_z is ir iz begin
         #if is ∈ composition.ion_species_range
             # enforce the vpa BC
@@ -542,7 +543,7 @@ function enforce_zero_incoming_bc!(pdf, vpa::coordinate, density, upar, ppar,
     # absolute velocity at right boundary
     @. vpa.scratch2 = vpagrid_to_dzdt(vpa.grid, sqrt(2.0*(ppar[end]/density[end])),
                                       upar[end], evolve_ppar, evolve_upar)
-    for ivpa ∈ 1:nvpa
+    @loop_vpa ivpa begin
         # for left boundary in zed (z = -Lz/2), want
         # f(z=-Lz/2, v_parallel > 0) = 0
         if vpa.scratch[ivpa] > zero
