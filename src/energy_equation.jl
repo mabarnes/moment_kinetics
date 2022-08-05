9995899e345fdfacabc2e20ec422c1d02e791c49
John Omotani <john.omotani@ukaea.uk>
2022-08-05 17:16:22 +0100

    Move comment to better place in energy_equation.jl

--- src/energy_equation.jl
+++ src/energy_equation.jl
@@ -33,9 +33,9 @@ end
 include all contributions to the energy equation aside from collisions
 """
 function energy_equation_no_collisions!(ppar_out, upar, ppar, qpar, dt, z, spectral)
+    # calculate dppar/dz and store in z.scratch
     # Use as 'adv_fac' for upwinding
     @. z.scratch3 = -upar
-    # calculate dppar/dz and store in z.scratch
     derivative!(z.scratch, ppar, z, z.scratch3, spectral)
     # update ppar to account for contribution from parallel pressure gradient
     @. ppar_out -= dt*upar*z.scratch
