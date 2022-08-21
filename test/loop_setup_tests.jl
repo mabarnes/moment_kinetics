270e2353e0e4f142f8a02c36ec327c61225e1223
John Omotani <john.omotani@ukaea.uk>
2022-08-21 22:11:10 +0100

    Fix loop_setup_tests.jl
    Was not being run automatically by test/runtests.jl, and some tests had
    been broken.

--- test/loop_setup_tests.jl
+++ test/loop_setup_tests.jl
@@ -2,33 +2,16 @@ module LoopSetupTests
 
 include("setup.jl")
 
-using moment_kinetics.looping: dims_string, dims_string_underscores,
-      loop_range_names, get_splits, get_load_balance, get_best_ranges,
-      get_best_ranges_from_sizes
+using moment_kinetics.looping: dims_string, get_splits, get_load_balance,
+                               get_best_ranges, get_best_ranges_from_sizes
 
 function runtests()
     @testset "loop setup" verbose=use_verbose begin
         println("loop setup tests")
         @testset "dims_string" begin
-            @test dims_string((:z,:y,:x)) == "zyx"
-            @test dims_string((:a,:b)) == "ab"
-            @test dims_string((:i,:j,:k,:l,:m,:n)) == "ijklmn"
-        end
-        @testset "dims_string_underscores" begin
-            @test dims_string_underscores((:z,:y,:x)) == "z_y_x"
-            @test dims_string_underscores((:a,:b)) == "a_b"
-            @test dims_string_underscores((:i,:j,:k,:l,:m,:n)) == "i_j_k_l_m_n"
-        end
-        @testset "loop_range_names" begin
-            @test loop_range_names((:z,:y,:x)) == Dict(:z=>:z_range_zyx,
-                                                       :y=>:y_range_zyx,
-                                                       :x=>:x_range_zyx)
-            @test loop_range_names((:a,:b)) == Dict(:a=>:a_range_ab,
-                                                    :b=>:b_range_ab)
-            @test loop_range_names((:i,:j,:k,:l,:m,:n)) ==
-                Dict(:i=>:i_range_ijklmn, :j=>:j_range_ijklmn,
-                     :k=>:k_range_ijklmn, :l=>:l_range_ijklmn,
-                     :m=>:m_range_ijklmn, :n=>:n_range_ijklmn)
+            @test dims_string((:z,:y,:x)) == "z_y_x"
+            @test dims_string((:a,:b)) == "a_b"
+            @test dims_string((:i,:j,:k,:l,:m,:n)) == "i_j_k_l_m_n"
         end
         @testset "get_splits" begin
             @test get_splits(1, 4) == [[1, 1, 1, 1]]
@@ -119,25 +102,52 @@ function runtests()
             @test get_best_ranges_from_sizes(9, 10, [3,4,5]) == [1:3, 3:4, 5:5]
         end
         @testset "get_best_ranges" begin
-            @test get_best_ranges(0, 1, (:x,:y,:z), Dict(:x=>3, :y=>4, :z=>5)) ==
-                Dict(:x=>1:3, :y=>1:4, :z=>1:5)
+            @test get_best_ranges(0, 1, (:s,:r,:z),
+                                  Dict(:s=>3, :r=>4, :z=>5, :sn=>1, :vperp=>2, :vpa=>7,
+                                       :vzeta=>11, :vr=>13, :vz=>17)) ==
+                Dict(:s=>1:3, :r=>1:4, :z=>1:5, :sn=>1:1, :vperp=>1:2, :vpa=>1:7,
+                     :vzeta=>1:11, :vr=>1:13, :vz=>1:17)
 
-            @test get_best_ranges(0, 8, (:x,:y,:z), Dict(:x=>3, :y=>4, :z=>5)) ==
-                Dict(:x=>1:3, :y=>1:1, :z=>1:2)
-            @test get_best_ranges(1, 8, (:x,:y,:z), Dict(:x=>3, :y=>4, :z=>5)) ==
-                Dict(:x=>1:3, :y=>2:2, :z=>1:2)
-            @test get_best_ranges(2, 8, (:x,:y,:z), Dict(:x=>3, :y=>4, :z=>5)) ==
-                Dict(:x=>1:3, :y=>3:3, :z=>1:2)
-            @test get_best_ranges(3, 8, (:x,:y,:z), Dict(:x=>3, :y=>4, :z=>5)) ==
-                Dict(:x=>1:3, :y=>4:4, :z=>1:2)
-            @test get_best_ranges(4, 8, (:x,:y,:z), Dict(:x=>3, :y=>4, :z=>5)) ==
-                Dict(:x=>1:3, :y=>1:1, :z=>3:5)
-            @test get_best_ranges(5, 8, (:x,:y,:z), Dict(:x=>3, :y=>4, :z=>5)) ==
-                Dict(:x=>1:3, :y=>2:2, :z=>3:5)
-            @test get_best_ranges(6, 8, (:x,:y,:z), Dict(:x=>3, :y=>4, :z=>5)) ==
-                Dict(:x=>1:3, :y=>3:3, :z=>3:5)
-            @test get_best_ranges(7, 8, (:x,:y,:z), Dict(:x=>3, :y=>4, :z=>5)) ==
-                Dict(:x=>1:3, :y=>4:4, :z=>3:5)
+            @test get_best_ranges(0, 8, (:s,:r,:z),
+                                  Dict(:s=>3, :r=>4, :z=>5, :sn=>1, :vperp=>2, :vpa=>7,
+                                       :vzeta=>11, :vr=>13, :vz=>17)) ==
+                Dict(:s=>1:3, :r=>1:1, :z=>1:2, :sn=>1:1, :vperp=>1:2, :vpa=>1:7,
+                     :vzeta=>1:11, :vr=>1:13, :vz=>1:17)
+            @test get_best_ranges(1, 8, (:s,:r,:z),
+                                  Dict(:s=>3, :r=>4, :z=>5, :sn=>1, :vperp=>2, :vpa=>7,
+                                       :vzeta=>11, :vr=>13, :vz=>17)) ==
+                Dict(:s=>1:3, :r=>2:2, :z=>1:2, :sn=>1:1, :vperp=>1:2, :vpa=>1:7,
+                     :vzeta=>1:11, :vr=>1:13, :vz=>1:17)
+            @test get_best_ranges(2, 8, (:s,:r,:z),
+                                  Dict(:s=>3, :r=>4, :z=>5, :sn=>1, :vperp=>2, :vpa=>7,
+                                       :vzeta=>11, :vr=>13, :vz=>17)) ==
+                Dict(:s=>1:3, :r=>3:3, :z=>1:2, :sn=>1:1, :vperp=>1:2, :vpa=>1:7,
+                     :vzeta=>1:11, :vr=>1:13, :vz=>1:17)
+            @test get_best_ranges(3, 8, (:s,:r,:z),
+                                  Dict(:s=>3, :r=>4, :z=>5, :sn=>1, :vperp=>2, :vpa=>7,
+                                       :vzeta=>11, :vr=>13, :vz=>17)) ==
+                Dict(:s=>1:3, :r=>4:4, :z=>1:2, :sn=>1:1, :vperp=>1:2, :vpa=>1:7,
+                     :vzeta=>1:11, :vr=>1:13, :vz=>1:17)
+            @test get_best_ranges(4, 8, (:s,:r,:z),
+                                  Dict(:s=>3, :r=>4, :z=>5, :sn=>1, :vperp=>2, :vpa=>7,
+                                       :vzeta=>11, :vr=>13, :vz=>17)) ==
+                Dict(:s=>1:3, :r=>1:1, :z=>3:5, :sn=>1:1, :vperp=>1:2, :vpa=>1:7,
+                     :vzeta=>1:11, :vr=>1:13, :vz=>1:17)
+            @test get_best_ranges(5, 8, (:s,:r,:z),
+                                  Dict(:s=>3, :r=>4, :z=>5, :sn=>1, :vperp=>2, :vpa=>7,
+                                       :vzeta=>11, :vr=>13, :vz=>17)) ==
+                Dict(:s=>1:3, :r=>2:2, :z=>3:5, :sn=>1:1, :vperp=>1:2, :vpa=>1:7,
+                     :vzeta=>1:11, :vr=>1:13, :vz=>1:17)
+            @test get_best_ranges(6, 8, (:s,:r,:z),
+                                  Dict(:s=>3, :r=>4, :z=>5, :sn=>1, :vperp=>2, :vpa=>7,
+                                       :vzeta=>11, :vr=>13, :vz=>17)) ==
+                Dict(:s=>1:3, :r=>3:3, :z=>3:5, :sn=>1:1, :vperp=>1:2, :vpa=>1:7,
+                     :vzeta=>1:11, :vr=>1:13, :vz=>1:17)
+            @test get_best_ranges(7, 8, (:s,:r,:z),
+                                  Dict(:s=>3, :r=>4, :z=>5, :sn=>1, :vperp=>2, :vpa=>7,
+                                       :vzeta=>11, :vr=>13, :vz=>17)) ==
+                Dict(:s=>1:3, :r=>4:4, :z=>3:5, :sn=>1:1, :vperp=>1:2, :vpa=>1:7,
+                     :vzeta=>1:11, :vr=>1:13, :vz=>1:17)
         end
     end
 end
