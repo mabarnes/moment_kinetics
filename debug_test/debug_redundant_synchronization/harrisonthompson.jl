3a7b422452103eafd0f720652a7a7a89c3f9148b
John Omotani <john.omotani@ukaea.uk>
2022-08-21 20:36:13 +0100

    Move redundant _block_synchronize() checks into separate subdirectory
    The checks for redundant _block_synchronize() calls require all
    dimensions to be split over processes, so they require a large number of
    processes to run in 2D2V/2D3V mode. Using that many processes, even if
    oversubscribing the physical cpu cores, requires a lot of memory, so is
    not feasible on CI servers or regular desktop/laptop computers.
    Therefore the tests intended for checking for redundant
    _block_synchronize() calls have been separated into a separate
    subdiretory, to allow the standard debug checks to be modified so that
    they can run on only 2 processes.
    
    Also disable the redundant _block_synchronize() checks in CI.

---
+++ debug_test/debug_redundant_synchronization/harrisonthompson.jl
@@ -0,0 +1,23 @@
+module HarrisonThompsonDebug
+# Test case with constant-in-space, delta-function-in-vpa source against
+# analytic solution from [E. R. Harrison and W. B. Thompson. The low pressure
+# plane symmetric discharge. Proc. Phys. Soc., 74:145, 1959]
+
+include("../setup.jl")
+
+# Create a temporary directory for test output
+test_output_directory = tempname()
+mkpath(test_output_directory)
+
+# Input parameters for the test
+include("../harrisonthompson_inputs.jl")
+
+# Defines the test functions, using variables defined in the *_inputs.jl file
+include("runtest_template.jl")
+
+end # HarrisonThompsonDebug
+
+
+using .HarrisonThompsonDebug
+
+HarrisonThompsonDebug.runtests()
