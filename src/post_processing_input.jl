070fd25b97ddc5f881596d8e0ef8afd34d43cf06
John Omotani <john.omotani@ukaea.uk>
2022-09-02 11:52:39 +0100

    Custom post-processing input: make more gifs!

--- src/post_processing_input.jl
+++ src/post_processing_input.jl
@@ -12,13 +12,13 @@ using ..input_structs: pp_input
 # rate of phi, using values at iz = iz0
 const calculate_frequencies = false
 # if plot_phi0_vs_t = true, create plot of phi(z0) vs time
-const plot_phi0_vs_t = true
+const plot_phi0_vs_t = false
 # if plot_phi_vs_z_t = true, create heatmap of phi vs z and time
 const plot_phi_vs_z_t = true
 # if animate_phi_vs_z = true, create animation of phi(z) at different time slices
 const animate_phi_vs_z = true
 # if plot_dens0_vs_t = true, create plots of species density(z0) vs time
-const plot_dens0_vs_t = true
+const plot_dens0_vs_t = false
 # if plot_upar0_vs_t = true, create plots of species upar(z0) vs time
 const plot_upar0_vs_t = false
 # if plot_ppar0_vs_t = true, create plots of species ppar(z0) vs time
@@ -38,18 +38,18 @@ const plot_qpar_vs_z_t = false
 # if animate_dens_vs_z = true, create animation of species density(z) at different time slices
 const animate_dens_vs_z = true
 # if animate_upar_vs_z = true, create animation of species parallel flow(z) at different time slices
-const animate_upar_vs_z = false
+const animate_upar_vs_z = true
 # if animate_ppar_vs_z = true, create animation of species parallel pressure(z) at different time slices
-const animate_ppar_vs_z = false
+const animate_ppar_vs_z = true
 # if animate_vth_vs_z = true, create animation of species thermal_velocity(z) at different time slices
-const animate_vth_vs_z = false
-# if animate_qpar_vs_z = true, create animation of species parallel heat flux(z) at different time slices
-const animate_qpar_vs_z = false
+const animate_vth_vs_z = true
+# if animate_ppar_vs_z = true, create animation of species parallel pressure(z) at different time slices
+const animate_qpar_vs_z = true
 # if animate_f_vs_vpa_z = true, create animation of f(vpa,z) at different time slices
-const animate_f_vs_vpa_z = false
+const animate_f_vs_vpa_z = true
 # if animate_f_unnormalized = true, create animation of f_unnorm(v_parallel_unnorm,z) at
 # different time slices
-const animate_f_unnormalized = false
+const animate_f_unnormalized = true
 # if animate_deltaf_vs_vpa_z = true, create animation of δf(vpa,z) at different time slices
 const animate_deltaf_vs_vpa_z = false
 # if animate_f_vs_vpa_z0 = true, create animation of f(vpa0,z) at different time slices
@@ -57,11 +57,11 @@ const animate_f_vs_vpa_z0 = false
 # if animate_deltaf_vs_vpa_z0 = true, create animation of δf(vpa0,z) at different time slices
 const animate_deltaf_vs_vpa_z0 = false
 # if animate_f_vs_z0_vpa = true, create animation of f(vpa,z0) at different time slices
-const animate_f_vs_z0_vpa = true
+const animate_f_vs_z0_vpa = false
 # if animate_deltaf_vs_z0_vpa = true, create animation of δf(vpa,z0) at different time slices
 const animate_deltaf_vs_vpa_z0 = false
 # animations will use one in every nwrite_movie data slices
-const nwrite_movie = 10
+const nwrite_movie = 1
 # itime_min is the minimum time index at which to start animations
 const itime_min = -1
 # itime_max is the final time index at which to end animations
