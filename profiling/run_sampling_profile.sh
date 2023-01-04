172d327af2a1132c7f56f0d907ed332f43a751ae
John Omotani <john.omotani@ukaea.uk>
2023-01-04 10:46:33 +0000

    Save profile output to profile.txt in run_sampling_profile.sh

--- profiling/run_sampling_profile.sh
+++ profiling/run_sampling_profile.sh
@@ -2,4 +2,4 @@
 
 # Run the memory_profile.jl script with memory profiling activated
 # First argument to the script gives the input file to use
-julia -O3 --check-bounds=no --project sampling_profile.jl $1
+julia -O3 --check-bounds=no --project sampling_profile.jl $1 | tee profile.txt
