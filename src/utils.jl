a81189bc58518e34ab245b4070a0936a8edf16a1
John Omotani <john.omotani@ukaea.uk>
2023-03-20 22:22:30 +0000

    Add e-i and i-e collision frequencies to dimensional parameters

--- src/utils.jl
+++ src/utils.jl
@@ -44,6 +44,7 @@ function get_unnormalized_parameters(input::Dict; Nnorm::Number, Tnorm::Number,
 
     e = 1.602176634e-19*Unitful.C
     epsilon0 = 8.8541878128e-12*Unitful.F/Unitful.m
+    me = 9.1093837015e-31*Unitful.kg
 
     # Assume ions are Deuterium
     mi = 3.3435837724e-27*Unitful.kg
@@ -51,6 +52,8 @@ function get_unnormalized_parameters(input::Dict; Nnorm::Number, Tnorm::Number,
     # Cold-ion sound speed with constant input electron temperature used for
     # normalization
     cs = Unitful.upreferred(sqrt(2.0*Tnorm/mi))
+
+    vth_e0 = Unitful.upreferred(sqrt(2.0*Tnorm/me))
 
     # Time normalization
     timenorm = Unitful.upreferred(Lnorm / cs)
@@ -100,6 +103,40 @@ function get_unnormalized_parameters(input::Dict; Nnorm::Number, Tnorm::Number,
     # Ion-ion collision frequency from Helander's book, for Z=1, at reference parameters
     parameters["nu_ii0"] =
         Unitful.upreferred(Nnorm*e^4*logLambda_ii0 / (4*π*epsilon0^2*mi^2*cs^3))
+
+    # From NRL formulary, Coulomb logarithm for electron-ion or ion-electron collisions is
+    # lnΛ_ei = lnΛ_ie = 23 - ln(sqrt(n)*Z*T_e^(-3/2)) for T_i*m_e/m_i < T_e < 10*Z^2 eV
+    #                 = 24 - ln(sqrt(n)*T_e^(-1)) for T_i*m_e/m_i < 10*Z^2 eV < T_e
+    #                 = 16 - ln(sqrt(n)*T_e^(-3/2)*Z^2*μ) for T_e < T_i*m_e/m_i eV
+    # where the ion mass in units of the proton mass is μ = m_i / m_p, temperatures are in
+    # eV and densities in cm^-3.
+    # So for Z=1
+    # lnΛ_ei = lnΛ_ie = 23 - 0.5*ln(n/T_e^3)) for T_i*m_e/m_i < T_e < 10 eV
+    #                 = 24 - 0.5*ln(n/T_e^2)) for T_i*m_e/m_i < 10 eV < T_e
+    #                 = 16 - 0.5*ln(n*μ^2/T_e^3) for T_e < T_i*m_e/m_i eV
+    # and changing density to units of m^-3
+    # lnΛ_ei = lnΛ_ie = 23 - 0.5*ln(n*1e-6/T_e^3)) for T_i*m_e/m_i < T_e < 10 eV
+    #                 = 24 - 0.5*ln(n*1e-6/T_e^2)) for T_i*m_e/m_i < 10 eV < T_e
+    #                 = 16 - 0.5*ln(n*1e-6*μ^2/T_e^3) for T_e < T_i*m_e/m_i eV
+    # lnΛ_ei = lnΛ_ie = 23 - 0.5*ln(1e-6) - 0.5*ln(n/T_e^3)) for T_i*m_e/m_i < T_e < 10 eV
+    #                 = 24 - 0.5*ln(1e-6) - 0.5*ln(n/T_e^2)) for T_i*m_e/m_i < 10 eV < T_e
+    #                 = 16 - 0.5*ln(1e-6) - 0.5*ln(n*μ^2/T_e^3) for T_e < T_i*m_e/m_i eV
+    # lnΛ_ei = lnΛ_ie = 29.9 - 0.5*ln(n/T_e^3)) for T_i*m_e/m_i < T_e < 10 eV
+    #                 = 30.9 - 0.5*ln(n/T_e^2)) for T_i*m_e/m_i < 10 eV < T_e
+    #                 = 22.9 - 0.5*ln(n*μ^2/T_e^3) for T_e < T_i*m_e/m_i eV
+    # At normalization parameters, T_i0=T_e0=Tnorm so can ignore final case
+    if Tnorm < 10*eV
+        logLambda_ei0 = 29.9 - 0.5*log(Nnorm / Unitful.m^(-3) / (Tnorm / eV)^3)
+    else
+        logLambda_ei0 = 30.9 - 0.5*log(Nnorm / Unitful.m^(-3) / (Tnorm / eV)^2)
+    end
+
+    # Electron-ion collision frequency from Helander's book, for Z=1, at reference parameters
+    parameters["nu_ei0"] =
+        Unitful.upreferred(Nnorm*e^4*logLambda_ei0 / (4*π*epsilon0^2*me^2*vth_e0^3))
+
+    # Electron-ion collision frequency
+    parameters["nu_ie0"] = me/mi * parameters["nu_ei0"]
 
     return parameters
 end
