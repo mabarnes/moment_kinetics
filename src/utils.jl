f20592e08b74863cdcde75b9d0bac6f78d4ee408
John Omotani <john.omotani@ukaea.uk>
2023-03-20 21:28:39 +0000

    Add ion-ion collision parameters to get_unnormalized_parameters()

--- src/utils.jl
+++ src/utils.jl
@@ -42,6 +42,9 @@ function get_unnormalized_parameters(input::Dict; Nnorm::Number, Tnorm::Number,
         Bnorm *= Unitful.T
     end
 
+    e = 1.602176634e-19*Unitful.C
+    epsilon0 = 8.8541878128e-12*Unitful.F/Unitful.m
+
     # Assume ions are Deuterium
     mi = 3.3435837724e-27*Unitful.kg
 
@@ -58,6 +61,7 @@ function get_unnormalized_parameters(input::Dict; Nnorm::Number, Tnorm::Number,
     parameters["Nnorm"] = Nnorm
     parameters["Tnorm"] = Tnorm
     parameters["Lnorm"] = Lnorm
+    parameters["timenorm"] = timenorm
 
     parameters["Lz"] = Lnorm * z_input.L
 
@@ -73,6 +77,28 @@ function get_unnormalized_parameters(input::Dict; Nnorm::Number, Tnorm::Number,
 
     parameters["CX_rate_coefficient"] = collisions.charge_exchange / Nnorm / timenorm
     parameters["ionization_rate_coefficient"] = collisions.ionization / Nnorm / timenorm
+
+    # Collisional parameters
+    ########################
+
+    # From NRL formulary, Coulomb logarithm for ion-ion collisions is
+    # lnΛ_ii' = 23 - ln[ZZ'(μ+μ')/(μT_i'+μ'T_i)*sqrt(n_i Z^2/T_i + n_i' Z'^2/T_i')]
+    # where the ion mass in units of the proton mass is μ = m_i / m_p, temperatures are in
+    # eV and densities in cm^-3.
+    # So for same-species collisions with Z=1
+    # lnΛ_ii' = 23 - ln[1/T_i*sqrt(2n_i/T_i)]
+    # lnΛ_ii' = 23 - ln[sqrt(2n_i/T_i^3)]
+    # lnΛ_ii' = 23 - 0.5*ln(2n_i/T_i^3)
+    # and changing density to units of m^-3
+    # lnΛ_ii' = 23 - 0.5*ln(2n_i*1e-6/T_i^3)
+    # lnΛ_ii' = 23 - 0.5*ln(2e-6) - 0.5*ln(n_i/T_i^3)
+    # lnΛ_ii' = 29.6 - 0.5*ln(n_i/T_i^3)
+    logLambda_ii0 = 29.6 - 0.5*log(Nnorm / Unitful.m^(-3) / (Tnorm / eV)^3)
+    parameters["logLambda_ii0"] = logLambda_ii0
+
+    # Ion-ion collision frequency from Helander's book, for Z=1, at reference parameters
+    parameters["nu_ii0"] =
+        Unitful.upreferred(Nnorm*e^4*logLambda_ii0 / (4*π*epsilon0^2*mi^2*cs^3))
 
     return parameters
 end
