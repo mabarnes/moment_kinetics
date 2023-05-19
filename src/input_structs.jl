162366fb08be16fb44d027a91c01bdd5cd1f6524
John Omotani <john.omotani@ukaea.uk>
2023-05-19 13:11:13 +0100

    (Re-)add numerical dissipation for neutral pdf
    Ensure consistency with previous 1D1V implementation.

--- src/input_structs.jl
+++ src/input_structs.jl
@@ -70,6 +70,7 @@ mutable struct advance_info
     manufactured_solns_test::Bool
     r_diffusion::Bool #flag to control how r bc is imposed when r diffusion terms are present
     vpa_diffusion::Bool #flag to control how vpa bc is imposed when vpa diffusion terms are present
+    vz_diffusion::Bool #flag to control how vz bc is imposed when vz diffusion terms are present
 end
 
 """
