19c5827a0bb1f7fd2321f39e30c633deb92cfc2a
John Omotani <john.omotani@ukaea.uk>
2023-07-11 11:01:18 +0100

    2D instability plots in Makie post processing

--- src/analysis.jl
+++ src/analysis.jl
@@ -8,6 +8,7 @@ export analyze_pdf_data
 
 using ..array_allocation: allocate_float
 using ..calculus: integral
+using ..coordinates: coordinate
 using ..interpolation: interpolate_to_grid_1d
 using ..load_data: open_readonly_output_file, get_nranks, load_pdf_data, load_rank_data
 using ..load_data: load_distributed_charged_pdf_slice
@@ -314,8 +315,134 @@ function field_line_average(fld, wgts, L)
 end
 
 """
+Return (v - mean(v, dims=2))
 """
-function analyze_2D_instability(phi, density, thermal_speed, r, z, r_spectral, z_spectral)
+function get_r_perturbation(v::AbstractArray{T,3}) where T
+    # Get background as r-average of the variable, assuming the background is constant
+    # in r
+    background = mean(v, dims=2)
+    perturbation = v .- background
+    return perturbation
+end
+
+"""
+Get 2D Fourier transform (in r and z) of non_uniform_data
+
+First interpolates to uniform grid, then uses FFT
+"""
+function get_Fourier_modes_2D(non_uniform_data::AbstractArray{T,3}, r::coordinate,
+                              r_spectral, z::coordinate, z_spectral) where T
+    nt = size(non_uniform_data, 3)
+
+    uniform_points_per_element_r = r.ngrid ÷ 4
+    n_uniform_r = r.nelement_global * uniform_points_per_element_r
+    uniform_spacing_r = r.L / n_uniform_r
+    uniform_grid_r = collect(1:n_uniform_r).*uniform_spacing_r .+ 0.5.*uniform_spacing_r .- 0.5.*r.L
+
+    uniform_points_per_element_z = z.ngrid ÷ 4
+    n_uniform_z = z.nelement_global * uniform_points_per_element_z
+    uniform_spacing_z = z.L / n_uniform_z
+    uniform_grid_z = collect(1:n_uniform_z).*uniform_spacing_z .+ 0.5.*uniform_spacing_z .- 0.5.*z.L
+
+    intermediate = allocate_float(n_uniform_z, r.n, nt)
+    for it ∈ 1:nt, ir ∈ 1:r.n
+        @views intermediate[:,ir,it] =
+        interpolate_to_grid_1d(uniform_grid_z, non_uniform_data[:,ir,it], z,
+                               z_spectral)
+    end
+
+    uniform_data = allocate_float(n_uniform_z, n_uniform_r, nt)
+    for it ∈ 1:nt, iz ∈ 1:n_uniform_z
+        @views uniform_data[iz,:,it] =
+        interpolate_to_grid_1d(uniform_grid_r, non_uniform_data[iz,:,it], r,
+                               r_spectral)
+    end
+
+    fourier_data = fft(uniform_data, (1,2))
+
+    return fourier_data
+end
+
+"""
+Get 1D Fourier transform (in r) of non_uniform_data
+
+First interpolates to uniform grid, then uses FFT.
+
+If zind is not given, find the zind where mode seems to be growing most strongly.
+"""
+function get_Fourier_modes_1D(non_uniform_data::AbstractArray{T,3}, r::coordinate,
+                              r_spectral, z; zind=nothing) where T
+
+    nt = size(non_uniform_data, 3)
+
+    if zind === nothing
+        # Find a z-location where the mode seems to be growing most strongly to analyse
+        ###############################################################################
+
+        # Get difference between max and min over the r dimension as a measure of the mode
+        # amplitude
+        Delta_var = maximum(non_uniform_data; dims=2)[:,1,:] - minimum(non_uniform_data; dims=2)[:,1,:]
+        max_Delta_var = maximum(Delta_var; dims=1)[1,:]
+
+        # Start searching for mode position once amplitude has grown to twice initial
+        # perturbation
+        startind = findfirst(x -> x>max_Delta_var[1], max_Delta_var)
+        if startind === nothing
+            startind = 1
+        end
+
+        # Find the z-index of the maximum of Delta_var
+        # Need the iterator thing to convert CartesianIndex structs returned by argmax into
+        # Ints that we can do arithmetic with.
+        zind_maximum = [i[1] for i ∈ argmax(Delta_var; dims=1)]
+        zind_maximum = zind_maximum[startind:end]
+
+        # Want the 'most common' value in zind_maximum, but maybe that is noisy?
+        # First find the most common bin for some reasonable number of bins. The background is
+        # a mode with one wave-period in the box, so 16 bins seems like plenty.
+        if z.n > 16
+            nbins = 16
+            bin_size = (z.n - 1) ÷ 16
+        else
+            nbins = 1
+            bin_size = z.n
+        end
+        binned_zind_maximum = @. (zind_maximum-1) ÷ bin_size
+        most_common_bin = mode(binned_zind_maximum)
+        bin_min = most_common_bin * bin_size + 1
+        bin_max = (most_common_bin+1) * bin_size
+        zinds_in_bin = [zind for zind in zind_maximum if bin_min ≤ zind ≤ bin_max]
+
+        # Find the most common zind in the bin, which might have some noise but will be in
+        # about the right region regardless as it is in the bin
+        zind = mode(zinds_in_bin)
+        println("Estimating average maximum mode amplitude at zind=$zind, z=", z.grid[zind])
+    end
+
+    # Analyse the Fourier modes at zind
+    ###################################
+    non_uniform_data = @view non_uniform_data[zind,:,:]
+    uniform_points_per_element_r = r.ngrid ÷ 4
+    n_uniform_r = r.nelement_global * uniform_points_per_element_r
+    uniform_spacing_r = r.L / n_uniform_r
+    uniform_grid_r = collect(0:(n_uniform_r-1)).*uniform_spacing_r .+ 0.5.*uniform_spacing_r .- 0.5.*r.L
+
+    uniform_data = allocate_float(n_uniform_r, nt)
+    for it ∈ 1:nt
+        @views uniform_data[:,it] =
+        interpolate_to_grid_1d(uniform_grid_r, non_uniform_data[:,it], r,
+                               r_spectral)
+    end
+
+    fourier_data = fft(uniform_data, 1)
+
+    return fourier_data, zind
+end
+
+"""
+"""
+function analyze_2D_instability(phi, density, thermal_speed, r, z, r_spectral, z_spectral;
+                                do_1d=true, do_2d=true, do_perturbation=true)
     # Assume there is only one species for this test
     density = density[:,:,1,:]
     thermal_speed = thermal_speed[:,:,1,:]
@@ -323,117 +450,37 @@ function analyze_2D_instability(phi, density, thermal_speed, r, z, r_spectral, z
     # NB normalisation removes the factor of 1/2
     temperature = thermal_speed.^2
 
-    # Get background as r-average of the variable, as the background is constant in r
-    background_phi = mean(phi, dims=2)
-    background_density = mean(density, dims=2)
-    background_temperature = mean(temperature, dims=2)
-
-    phi_perturbation = phi .- background_phi
-    density_perturbation = density .- background_density
-    temperature_perturbation = temperature .- background_temperature
+    if do_perturbation
+        phi_perturbation = get_r_perturbation(phi)
+        density_perturbation = get_r_perturbation(density)
+        temperature_perturbation = get_r_perturbation(temperature)
+    else
+        phi_perturbation = nothing
+        density_perturbation = nothing
+        temperature_perturbation = nothing
+    end
 
     nt = size(phi, 3)
 
-    function get_Fourier_modes_2D(non_uniform_data, r, r_spectral, z, z_spectral)
-
-        uniform_points_per_element_r = r.ngrid ÷ 4
-        n_uniform_r = r.nelement_global * uniform_points_per_element_r
-        uniform_spacing_r = r.L / n_uniform_r
-        uniform_grid_r = collect(1:n_uniform_r).*uniform_spacing_r .+ 0.5.*uniform_spacing_r .- 0.5.*r.L
-
-        uniform_points_per_element_z = z.ngrid ÷ 4
-        n_uniform_z = z.nelement_global * uniform_points_per_element_z
-        uniform_spacing_z = z.L / n_uniform_z
-        uniform_grid_z = collect(1:n_uniform_z).*uniform_spacing_z .+ 0.5.*uniform_spacing_z .- 0.5.*z.L
-
-        intermediate = allocate_float(n_uniform_z, r.n, nt)
-        for it ∈ 1:nt, ir ∈ 1:r.n
-            @views intermediate[:,ir,it] =
-                interpolate_to_grid_1d(uniform_grid_z, non_uniform_data[:,ir,it], z,
-                                       z_spectral)
-        end
-
-        uniform_data = allocate_float(n_uniform_z, n_uniform_r, nt)
-        for it ∈ 1:nt, iz ∈ 1:n_uniform_z
-            @views uniform_data[iz,:,it] =
-                interpolate_to_grid_1d(uniform_grid_r, non_uniform_data[iz,:,it], r,
-                                       r_spectral)
-        end
-
-        fourier_data = fft(uniform_data, (1,2))
-
-        return fourier_data
-    end
-
-    phi_Fourier_2D = get_Fourier_modes_2D(phi, r, r_spectral, z, z_spectral)
-    density_Fourier_2D = get_Fourier_modes_2D(density, r, r_spectral, z, z_spectral)
-    temperature_Fourier_2D = get_Fourier_modes_2D(temperature, r, r_spectral, z, z_spectral)
-
-    # Find a z-location where the mode seems to be growing most strongly to analyse
-    ###############################################################################
-
-    # Get difference between max and min over the r dimension as a measure of the mode
-    # amplitude
-    Delta_phi = maximum(phi; dims=2)[:,1,:] - minimum(phi; dims=2)[:,1,:]
-    max_Delta_phi = maximum(Delta_phi; dims=1)[1,:]
-
-    # Start searching for mode position once amplitude has grown to twice initial
-    # perturbation
-    startind = findfirst(x -> x>max_Delta_phi[1], max_Delta_phi)
-    if startind === nothing
-        startind = 1
-    end
-
-    # Find the z-index of the maximum of Delta_phi
-    # Need the iterator thing to convert CartesianIndex structs returned by argmax into
-    # Ints that we can do arithmetic with.
-    zind_maximum = [i[1] for i ∈ argmax(Delta_phi; dims=1)]
-    zind_maximum = zind_maximum[startind:end]
-
-    # Want the 'most common' value in zind_maximum, but maybe that is noisy?
-    # First find the most common bin for some reasonable number of bins. The background is
-    # a mode with one wave-period in the box, so 16 bins seems like plenty.
-    if z.n > 16
-        nbins = 16
-        bin_size = (z.n - 1) ÷ 16
+    if do_2d
+        phi_Fourier_2D = get_Fourier_modes_2D(phi, r, r_spectral, z, z_spectral)
+        density_Fourier_2D = get_Fourier_modes_2D(density, r, r_spectral, z, z_spectral)
+        temperature_Fourier_2D = get_Fourier_modes_2D(temperature, r, r_spectral, z, z_spectral)
     else
-        nbins = 1
-        bin_size = z.n
-    end
-    binned_zind_maximum = @. (zind_maximum-1) ÷ bin_size
-    most_common_bin = mode(binned_zind_maximum)
-    bin_min = most_common_bin * bin_size + 1
-    bin_max = (most_common_bin+1) * bin_size
-    zinds_in_bin = [zind for zind in zind_maximum if bin_min ≤ zind ≤ bin_max]
-
-    # Find the most common zind in the bin, which might have some noise but will be in
-    # about the right region regardless as it is in the bin
-    zind = mode(zinds_in_bin)
-    println("Estimating average maximum mode amplitude at zind=$zind, z=", z.grid[zind])
-
-    # Now analyse the Fourier modes at this zind
-    function get_Fourier_modes_1D(non_uniform_data, r, r_spectral)
-
-        uniform_points_per_element_r = r.ngrid ÷ 4
-        n_uniform_r = r.nelement_global * uniform_points_per_element_r
-        uniform_spacing_r = r.L / n_uniform_r
-        uniform_grid_r = collect(0:(n_uniform_r-1)).*uniform_spacing_r .+ 0.5.*uniform_spacing_r .- 0.5.*r.L
-
-        uniform_data = allocate_float(n_uniform_r, nt)
-        for it ∈ 1:nt
-            @views uniform_data[:,it] =
-                interpolate_to_grid_1d(uniform_grid_r, non_uniform_data[:,it], r,
-                                       r_spectral)
-        end
-
-        fourier_data = fft(uniform_data, 1)
-
-        return fourier_data
+        phi_Fourier_2D = nothing
+        density_Fourier_2D = nothing
+        temperature_Fourier_2D = nothing
     end
 
-    @views phi_Fourier_1D = get_Fourier_modes_1D(phi[zind,:,:], r, r_spectral)
-    @views density_Fourier_1D = get_Fourier_modes_1D(density[zind,:,:], r, r_spectral)
-    @views temperature_Fourier_1D = get_Fourier_modes_1D(temperature[zind,:,:], r, r_spectral)
+    if do_1d
+        phi_Fourier_1D, zind = get_Fourier_modes_1D(phi, r, r_spectral, z)
+        density_Fourier_1D, _ = get_Fourier_modes_1D(density, r, r_spectral, z, zind=zind)
+        temperature_Fourier_1D, _ = get_Fourier_modes_1D(temperature, r, r_spectral, z, zind=zind)
+    else
+        phi_Fourier_1D = nothing
+        density_Fourier_1D = nothing
+        temperature_Fourier_1D = nothing
+    end
 
     return phi_perturbation, density_perturbation, temperature_perturbation,
            phi_Fourier_2D, density_Fourier_2D, temperature_Fourier_2D,
