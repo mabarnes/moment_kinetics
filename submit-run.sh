f48a901c0edd5aa24baa2c88eec1ced3b72f6e5c
John Omotani <john.omotani@ukaea.uk>
2023-07-25 15:40:20 +0100

    Flag in submit-run.sh to disable submission of post-processing job

--- submit-run.sh
+++ submit-run.sh
@@ -21,7 +21,8 @@ QOS=${JOBINFO[7]}
 # Parse command line options
 # [See e.g. https://www.stackchief.com/tutorials/Bash%20Tutorial%3A%20getopts
 # for examples of how to use Bash's getopts]
-while getopts "ht:n:u:m:p:q:" opt; do
+POSTPROC=0
+while getopts "ht:n:u:m:p:q:a" opt; do
   case $opt in
     h)
       echo "Submit jobs for a simulation (using INPUT_FILE for input) and post-processing to the queue
@@ -31,7 +32,8 @@ Usage: submit-run.sh [option] INPUT_FILE
 -u     The run time for the post-processing, e.g. 1:00:00
 -m     The requested memory for post-processing
 -p     The 'partition' (passed to 'sbatch --partition')
--q     The 'quality of service' (passed to 'sbatch --qos')"
+-q     The 'quality of service' (passed to 'sbatch --qos')
+-a     Do not submit post-processing job after the run"
       exit 1
       ;;
     t)
@@ -52,6 +54,9 @@ Usage: submit-run.sh [option] INPUT_FILE
     q)
       QOS=$OPTARG
       ;;
+    a)
+      POSTPROC=1
+      ;;
   esac
 done
 
@@ -63,7 +68,11 @@ if [ -z "$INPUTFILE" ]; then
   exit 1
 fi
 
-echo "Submitting $INPUTFILE for run and post-processing..."
+if [[ $POSTPROC -eq 0 ]]; then
+  echo "Submitting $INPUTFILE for run and post-processing..."
+else
+  echo "Submitting $INPUTFILE for run..."
+fi
 
 RUNNAME=$(util/get-run-name.jl $INPUTFILE)
 
@@ -74,15 +83,18 @@ mkdir -p $RUNDIR
 RUNJOBSCRIPT=${RUNDIR}$RUNNAME.job
 sed -e "s|NODES|$NODES|" -e "s|RUNTIME|$RUNTIME|" -e "s|ACCOUNT|$ACCOUNT|" -e "s|PARTITION|$PARTITION|" -e "s|QOS|$QOS|" -e "s|RUNDIR|$RUNDIR|" -e "s|INPUTFILE|$INPUTFILE|" machines/$MACHINE/jobscript-run.template > $RUNJOBSCRIPT
 
-# Create a submission script for post-processing
-POSTPROCJOBSCRIPT=${RUNDIR}$RUNNAME-post.job
-sed -e "s|POSTPROCMEMORY|$POSTPROCMEMORY|" -e "s|POSTPROCTIME|$POSTPROCTIME|" -e "s|ACCOUNT|$ACCOUNT|" -e "s|RUNDIR|$RUNDIR|" machines/$MACHINE/jobscript-postprocess.template > $POSTPROCJOBSCRIPT
-
 JOBID=$(sbatch --parsable $RUNJOBSCRIPT)
 echo "Run: $JOBID"
 echo "In the queue" > ${RUNDIR}slurm-$JOBID.out
-POSTID=$(sbatch -d afterany:$JOBID --parsable $POSTPROCJOBSCRIPT)
-echo "Postprocess: $POSTID"
-echo "In the queue" > ${RUNDIR}slurm-post-$POSTID.out
+
+if [[ $POSTPROC -eq 0 ]]; then
+  # Create a submission script for post-processing
+  POSTPROCJOBSCRIPT=${RUNDIR}$RUNNAME-post.job
+  sed -e "s|POSTPROCMEMORY|$POSTPROCMEMORY|" -e "s|POSTPROCTIME|$POSTPROCTIME|" -e "s|ACCOUNT|$ACCOUNT|" -e "s|RUNDIR|$RUNDIR|" machines/$MACHINE/jobscript-postprocess.template > $POSTPROCJOBSCRIPT
+
+  POSTID=$(sbatch -d afterany:$JOBID --parsable $POSTPROCJOBSCRIPT)
+  echo "Postprocess: $POSTID"
+  echo "In the queue" > ${RUNDIR}slurm-post-$POSTID.out
+fi
 
 echo "Done"
