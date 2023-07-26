f111aed8256654e8bc271fac792584b1bb6767c4
John Omotani <john.omotani@ukaea.uk>
2023-07-26 18:02:35 +0100

    Attempt to use PrecompileTools to get more precompilation
    [skip ci]

--- src/precompilation.jl
+++ src/precompilation.jl
@@ -1,2 +1,6 @@
-precompile(run_moment_kinetics, ())
-precompile(run_moment_kinetics, (String,))
+using PrecompileTools
+
+@compile_workload begin
+    moment_kinetics.communication.__init__()
+    run_moment_kinetics("minimal.toml")
+end
