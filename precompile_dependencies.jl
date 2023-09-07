ff9275c04cfb63461c00b9c8ef9208df901630f6
John Omotani <john.omotani@ukaea.uk>
2023-09-07 08:48:11 +0100

    precompile-fixes: Let PackageCompiler find all packages associated with project
    The default, when no `packages` are passed as an argument to
    `create_sysimage()` is to include 'all packages in the project'. Using
    this avoids us having to create and maintain a list of packages to
    compile.

--- precompile_dependencies.jl
+++ precompile_dependencies.jl
@@ -5,12 +5,9 @@ Pkg.activate(".")
 
 using PackageCompiler
 
-packages = [:PackageCompiler, :ArgParse, :Combinatorics, :DelimitedFiles, :FFTW, :Glob, :IJulia, :LinearAlgebra, :LsqFit, :MPI, :NaturalSort, :NCDatasets, :OrderedCollections, :Plots, :Primes, :Roots, :SHA, :SpecialFunctions, :Statistics, :TOML, :TimerOutputs]
-
 # create the sysimage 'dependencies.so' in the base moment_kinetics source directory
 # with the above pre-compiled packages
-create_sysimage(packages;
-                sysimage_path="dependencies.so",
+create_sysimage(; sysimage_path="dependencies.so",
                 precompile_execution_file="util/precompile_run_short.jl",
                 include_transitive_dependencies=false, # This is needed to make MPI work, see https://github.com/JuliaParallel/MPI.jl/issues/518
                 sysimage_build_args=`-O3 --check-bounds=no`, # Assume if we are precompiling we want an optimized, production build
