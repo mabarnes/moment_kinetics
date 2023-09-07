7de747522de75997765742c7379077ecd76e20b2
John Omotani <john.omotani@ukaea.uk>
2023-09-07 17:31:01 +0100

    precompile-fixes: Fetch all dependencies from Project.toml in precompile_dependencies.jl
    Can't use the default of `create_sysimage()` for
    precompile_dependecies.jl because that would include moment_kinetics,
    and for this script we want to avoid baking moment_kinetics into the
    system image, so that we can change moment_kinetics afterwards.
    
    [skip ci]

--- precompile_dependencies.jl
+++ precompile_dependencies.jl
@@ -5,9 +5,13 @@ Pkg.activate(".")
 
 using PackageCompiler
 
+using TOML
+project_file = TOML.parsefile("Project.toml")
+packages = collect(Symbol(d) for d ∈ keys(project_file["deps"]))
+
 # create the sysimage 'dependencies.so' in the base moment_kinetics source directory
 # with the above pre-compiled packages
-create_sysimage(; sysimage_path="dependencies.so",
+create_sysimage(packages; sysimage_path="dependencies.so",
                 precompile_execution_file="util/precompile_run_short.jl",
                 include_transitive_dependencies=false, # This is needed to make MPI work, see https://github.com/JuliaParallel/MPI.jl/issues/518
                 sysimage_build_args=`-O3 --check-bounds=no`, # Assume if we are precompiling we want an optimized, production build
