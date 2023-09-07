7f66a5579331c4eed3e06288aeff7d910784862f
John Omotani <john.omotani@ukaea.uk>
2023-09-07 08:48:11 +0100

    precompile-fixes: Let PackageCompiler find all packages associated with project
    The default, when no `packages` are passed as an argument to
    `create_sysimage()` is to include 'all packages in the project'. Using
    this avoids us having to create and maintain a list of packages to
    compile.

--- precompile-makie-post-processing.jl
+++ precompile-makie-post-processing.jl
@@ -5,14 +5,11 @@ Pkg.activate(".")
 
 using PackageCompiler
 
-packages = [:moment_kinetics, :PackageCompiler, :ArgParse, :CairoMakie, :Combinatorics, :DelimitedFiles, :FFTW, :Glob, :HDF5, :IJulia, :LinearAlgebra, :LsqFit, :MPI, :NaturalSort, :NCDatasets, :OrderedCollections, :Primes, :Roots, :SHA, :SpecialFunctions, :Statistics, :TOML, :TimerOutputs]
-
 # Create the sysimage 'makie_postproc.so' in the base moment_kinetics source directory
 # with both moment_kinetics and the dependencies listed above precompiled.
 # Warning: editing the code will not affect what runs when using this .so, you
 # need to re-precompile if you change anything.
-create_sysimage(packages;
-                sysimage_path="makie_postproc.so",
+create_sysimage(; sysimage_path="makie_postproc.so",
                 precompile_execution_file="util/precompile_makie_plots.jl",
                 include_transitive_dependencies=false, # This is needed to make MPI work, see https://github.com/JuliaParallel/MPI.jl/issues/518
                )
--- precompile-no-run.jl
+++ precompile-no-run.jl
@@ -5,19 +5,11 @@ Pkg.activate(".")
 
 using PackageCompiler
 
-using TOML
-project_file = TOML.parsefile("Project.toml")
-deps = (Symbol(d) for d ∈ keys(project_file["deps"]))
-
-packages = [:moment_kinetics, :PackageCompiler, deps...]
-println("precompling $packages")
-
 # Create the sysimage 'moment_kinetics.so' in the base moment_kinetics source directory
 # with both moment_kinetics and the dependencies listed above precompiled.
 # Warning: editing the code will not affect what runs when using this .so, you
 # need to re-precompile if you change anything.
-create_sysimage(packages;
-                sysimage_path="moment_kinetics.so",
+create_sysimage(; sysimage_path="moment_kinetics.so",
                 include_transitive_dependencies=false, # This is needed to make MPI work, see https://github.com/JuliaParallel/MPI.jl/issues/518
                 sysimage_build_args=`-O3 --check-bounds=no`, # Assume if we are precompiling we want an optimized, production build
                )
