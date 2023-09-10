c0e232fcdbfcdeef4ac138eeb1c77db5ba37baa4
John Omotani <john.omotani@ukaea.uk>
2023-09-10 12:35:38 +0100

    external-sources: use isn as the species index in the neutral energy equation

--- src/energy_equation.jl
+++ src/energy_equation.jl
@@ -70,44 +70,44 @@ function neutral_energy_equation!(pz, fvec, moments, collisions, dt, spectral,
 
     begin_sn_r_z_region()
 
-    @loop_sn_r_z is ir iz begin
-        pz[iz,ir,is] += dt*(-fvec.uz_neutral[iz,ir,is]*moments.neutral.dpz_dz_upwind[iz,ir,is]
-                            - moments.neutral.dqz_dz[iz,ir,is]
-                            - 3.0*fvec.pz_neutral[iz,ir,is]*moments.neutral.duz_dz[iz,ir,is])
+    @loop_sn_r_z isn ir iz begin
+        pz[iz,ir,isn] += dt*(-fvec.uz_neutral[iz,ir,isn]*moments.neutral.dpz_dz_upwind[iz,ir,isn]
+                             - moments.neutral.dqz_dz[iz,ir,isn]
+                             - 3.0*fvec.pz_neutral[iz,ir,isn]*moments.neutral.duz_dz[iz,ir,isn])
     end
 
     if neutral_source_settings.active
         source_amplitude = moments.neutral.external_source_amplitude
         source_T = neutral_source_settings.source_T
-        @loop_s_r_z is ir iz begin
-            pz[iz,ir,is] += dt * source_amplitude[iz,ir]
-                            (0.5*source_T + fvec.uz_neutral[iz,ir,is]^2)
+        @loop_s_r_z isn ir iz begin
+            pz[iz,ir,isn] += dt * source_amplitude[iz,ir]
+                             (0.5*source_T + fvec.uz_neutral[iz,ir,isn]^2)
         end
     end
 
     diffusion_coefficient = num_diss_params.moment_dissipation_coefficient
     if diffusion_coefficient > 0.0
-        @loop_sn_r_z is ir iz begin
-            pz[iz,ir,is] += dt*diffusion_coefficient*moments.neutral.d2pz_dz2[iz,ir,is]
+        @loop_sn_r_z isn ir iz begin
+            pz[iz,ir,isn] += dt*diffusion_coefficient*moments.neutral.d2pz_dz2[iz,ir,isn]
         end
     end
 
     # add in contributions due to charge exchange/ionization collisions
     if composition.n_neutral_species > 0
         if abs(collisions.charge_exchange) > 0.0
-            @loop_sn_r_z is ir iz begin
-                pz[iz,ir,is] -=
+            @loop_sn_r_z isn ir iz begin
+                pz[iz,ir,isn] -=
                     dt*collisions.charge_exchange*(
-                        fvec.density[iz,ir,is]*fvec.pz_neutral[iz,ir,is] -
-                        fvec.density_neutral[iz,ir,is]*fvec.ppar[iz,ir,is] -
-                        fvec.density_neutral[iz,ir,is]*fvec.density[iz,ir,is] *
-                            (fvec.uz_neutral[iz,ir,is] - fvec.upar[iz,ir,is])^2)
+                        fvec.density[iz,ir,isn]*fvec.pz_neutral[iz,ir,isn] -
+                        fvec.density_neutral[iz,ir,isn]*fvec.ppar[iz,ir,isn] -
+                        fvec.density_neutral[iz,ir,isn]*fvec.density[iz,ir,isn] *
+                            (fvec.uz_neutral[iz,ir,isn] - fvec.upar[iz,ir,isn])^2)
             end
         end
         if abs(collisions.ionization) > 0.0
-            @loop_sn_r_z is ir iz begin
-                pz[iz,ir,is] -=
-                    dt*collisions.ionization*fvec.density[iz,ir,is]*fvec.pz_neutral[iz,ir,is]
+            @loop_sn_r_z isn ir iz begin
+                pz[iz,ir,isn] -=
+                    dt*collisions.ionization*fvec.density[iz,ir,isn]*fvec.pz_neutral[iz,ir,isn]
             end
         end
     end
