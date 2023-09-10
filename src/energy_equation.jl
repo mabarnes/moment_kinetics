7283fdc1a8add469973f23525651e30a7b43d26f
John Omotani <john.omotani@ukaea.uk>
2023-09-10 16:34:06 +0100

    external-sources: fix typo in source term in neutral_energy_equation!()

--- src/energy_equation.jl
+++ src/energy_equation.jl
@@ -80,7 +80,7 @@ function neutral_energy_equation!(pz, fvec, moments, collisions, dt, spectral,
         source_amplitude = moments.neutral.external_source_amplitude
         source_T = neutral_source_settings.source_T
         @loop_s_r_z is ir iz begin
-            pz[iz,ir,is] += dt * source_amplitude[iz,ir]
+            pz[iz,ir,is] += dt * source_amplitude[iz,ir] *
                             (0.5*source_T + fvec.uz_neutral[iz,ir,is]^2)
         end
     end
