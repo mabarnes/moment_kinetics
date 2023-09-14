e74a5e0fe0b6cf8dbfe92efa1e95693589cd1ae0
John Omotani <john.omotani@ukaea.uk>
2023-09-14 10:58:31 +0100

    eftc-plots: update plot script
    [skip ci]

--- publication_inputs/2023_EFTC_jto-poster/wall-bc/wall-bc-plots.jl
+++ publication_inputs/2023_EFTC_jto-poster/wall-bc/wall-bc-plots.jl
@@ -7,11 +7,14 @@ function main()
     output_dir = "publication_inputs/2023_EFTC_jto-poster/wall-bc/"
     ext = ".png"
 
+    CairoMakie.activate!(; px_per_unit=4)
+
     #run_dir = "runs/wall-bc_recyclefraction0.5"
     #run_dir = "runs/wall-bc_recyclefraction0.5_split3"
 
     run_dirs = ("runs/wall-bc_volumerecycle", "runs/wall-bc_volumerecycle_split1",
                 "runs/wall-bc_volumerecycle_split2", "runs/wall-bc_volumerecycle_split3")
+    prefixes = ("full-f", "split1", "split2", "split3")
     short_labels = (L"full-f$$", L"n", L"n,u_\parallel", L"n,u_\parallel,p_\parallel")
     subtitles = (L"full-f$$", L"evolving-$n$", L"evolving-$n,u_\parallel$", L"evolving-$n,u_\parallel,p_\parallel$")
 
@@ -48,14 +51,39 @@ function main()
     #plot_vs_vz_z(run_info_dfns, "f_neutral"; title=L"f_n", outfile=joinpath(output_dir, "f_neutral$ext"), xlabel=L"v_\parallel", ylabel=L"z")
 
     ion_cbar_max = 3
-    neutral_cbar_max = 6
+    neutral_cbar_max = 5
     lims = (-10.0, 10.0, -0.5, 0.5)
     axis_args = Dict(:limits=>lims, :xgridvisible=>false, :ygridvisible=>false)
-    plot_f_unnorm_vs_vpa_z(run_info_dfns; title=L"f_i", outfile=joinpath(output_dir, "f_ion$ext"), xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(0, ion_cbar_max), subtitles=subtitles, axis_args=axis_args)
-    plot_f_unnorm_vs_vpa_z(run_info_dfns; neutral=true, title=L"f_n", outfile=joinpath(output_dir, "f_neutral$ext"), xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(0, neutral_cbar_max), subtitles=subtitles, axis_args=axis_args)
+    #plot_f_unnorm_vs_vpa_z(run_info_dfns; title=L"f_i", outfile=joinpath(output_dir, "f_ion$ext"), xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(0, ion_cbar_max), subtitles=subtitles, axis_args=axis_args)
+    #plot_f_unnorm_vs_vpa_z(run_info_dfns; neutral=true, title=L"f_n", outfile=joinpath(output_dir, "f_neutral$ext"), xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(0, neutral_cbar_max), subtitles=subtitles, axis_args=axis_args)
 
-    plot_f_unnorm_vs_vpa_z(run_info_dfns; title=L"f_i", outfile=joinpath(output_dir, "logf_ion$ext"), transform=positive_or_nan, colorscale=log10, xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(1e-16, ion_cbar_max), subtitles=subtitles, axis_args=axis_args)
-    plot_f_unnorm_vs_vpa_z(run_info_dfns; neutral=true, title=L"f_n", outfile=joinpath(output_dir, "logf_neutral$ext"), transform=positive_or_nan, colorscale=log10, xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(1e-16, neutral_cbar_max), subtitles=subtitles, axis_args=axis_args)
+    #plot_f_unnorm_vs_vpa_z(run_info_dfns; title=L"f_i", outfile=joinpath(output_dir, "logf_ion$ext"), transform=positive_or_nan, colorscale=log10, xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(1e-16, ion_cbar_max), subtitles=subtitles, axis_args=axis_args)
+    #plot_f_unnorm_vs_vpa_z(run_info_dfns; neutral=true, title=L"f_n", outfile=joinpath(output_dir, "logf_neutral$ext"), transform=positive_or_nan, colorscale=log10, xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(1e-16, neutral_cbar_max), subtitles=subtitles, axis_args=axis_args)
+    for (ri, p, st) ∈ zip(run_info_dfns, prefixes, subtitles)
+        plot_f_unnorm_vs_vpa_z(ri;
+                               outfile=joinpath(output_dir, "$(p)_f_ion$ext"),
+                               xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0,
+                               colorrange=(0, ion_cbar_max), title=st,
+                               axis_args=axis_args)
+        plot_f_unnorm_vs_vpa_z(ri; neutral=true,
+                               outfile=joinpath(output_dir, "$(p)_f_neutral$ext"),
+                               xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0,
+                               colorrange=(0, neutral_cbar_max), title=st,
+                               axis_args=axis_args)
+
+        plot_f_unnorm_vs_vpa_z(ri;
+                               outfile=joinpath(output_dir, "$(p)_logf_ion$ext"),
+                               transform=positive_or_nan, colorscale=log10,
+                               xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0,
+                               colorrange=(1e-16, ion_cbar_max), title=st,
+                               axis_args=axis_args)
+        plot_f_unnorm_vs_vpa_z(ri; neutral=true,
+                               outfile=joinpath(output_dir, "$(p)_logf_neutral$ext"),
+                               transform=positive_or_nan, colorscale=log10,
+                               xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0,
+                               colorrange=(1e-16, neutral_cbar_max), title=st,
+                               axis_args=axis_args)
+    end
 end
 
 main()
