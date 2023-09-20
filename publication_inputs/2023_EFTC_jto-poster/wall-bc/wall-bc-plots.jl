ba8fbb6201abad14b727e3a26256e25a557c9ba4
John Omotani <john.omotani@ukaea.uk>
2023-09-20 15:29:44 +0100

    eftc-plots: Better layout, legends, etc. for wall-bc-plots.jl
    [skip ci]

--- publication_inputs/2023_EFTC_jto-poster/wall-bc/wall-bc-plots.jl
+++ publication_inputs/2023_EFTC_jto-poster/wall-bc/wall-bc-plots.jl
@@ -25,8 +25,10 @@ function main()
                )
 
     prefixes = ("full-f", "split1", "split2", "split3")
-    short_labels = (L"full-f$$", L"n", L"n,u_\parallel", L"n,u_\parallel,p_\parallel")
-    subtitles = (L"full-f$$", L"evolving-$n$", L"evolving-$n,u_\parallel$", L"evolving-$n,u_\parallel,p_\parallel$")
+    short_labels = (L"full-f$$", L"evolving-$n$", L"evolving-$n,u_\parallel$",
+                    L"evolving-$n,u_\parallel,p_\parallel$")
+    #subtitles = (L"full-f$$", L"evolving-$n$", L"evolving-$n,u_\parallel$",
+    #             L"evolving-$n,u_\parallel,p_\parallel$")
 
     run_info = Tuple(get_run_info(d; itime_min=-1) for d ∈ run_dirs)
     run_info_dfns = Tuple(get_run_info(d; itime_min=-1, dfns=true) for d ∈ run_dirs)
@@ -34,12 +36,14 @@ function main()
         joinpath(output_dir, "post_processing_input_eftc2023.toml"),
         run_info_moments=run_info, run_info_dfns=run_info_dfns)
 
-    for (var_names, ylabel) ∈ ((("density", "density_neutral"), L"n"),
-                               (("parallel_flow", "uz_neutral"), L"u_\parallel"),
-                               (("temperature", "temperature_neutral"), L"T"))
-        local fig, ax = get_1d_ax(xlabel=L"z", ylabel=ylabel)
-        for (ri, label1, linestyle) ∈ zip(run_info,
-                                          short_labels,
+    fig, axes = get_1d_ax(3; xlabel=L"z",
+                          subtitles=(L"density$$", L"parallel flow$$", L"temperature$$"),
+                          resolution=(1200, 400))
+    for ((var_names, ylabel), ax) ∈ zip(((("density", "density_neutral"), L"n"),
+                                         (("parallel_flow", "uz_neutral"), L"u_\parallel"),
+                                         (("temperature", "temperature_neutral"), L"T")),
+                                        axes)
+        for (ri, label1, linestyle) ∈ zip(run_info, short_labels,
                                           (nothing, :dash, :dashdot, :dot))
             for (var_name, label2) ∈ zip(var_names, ("ion", "neutral"))
                 if var_name == "temperature"
@@ -49,13 +53,13 @@ function main()
                 else
                     data = nothing
                 end
-                plot_vs_z(ri, var_name; ax=ax, data=data,
+                plot_vs_z(ri, var_name; ax=ax, data=data, ylabel=ylabel,
                           label=LaTeXString(label1*" "*label2), linestyle=linestyle)
             end
         end
-        put_legend_below(fig, ax; orientation=:horizontal, nbanks=2)
-        save(joinpath(output_dir, var_names[1] * ext), fig)
     end
+    Legend(fig[2, 1], axes[1]; tellheight=true, tellwidth=false, orientation=:horizontal, nbanks=2)
+    save(joinpath(output_dir, "moments" * ext), fig)
 
     #plot_vs_vpa_z(run_info_dfns, "f"; title=L"f_i", outfile=joinpath(output_dir, "f_ion$ext"), xlabel=L"v_\parallel", ylabel=L"z")
     #plot_vs_vz_z(run_info_dfns, "f_neutral"; title=L"f_n", outfile=joinpath(output_dir, "f_neutral$ext"), xlabel=L"v_\parallel", ylabel=L"z")
@@ -63,37 +67,64 @@ function main()
     ion_cbar_max = 3
     neutral_cbar_max = 8
     lims = (-12.0, 12.0, -0.5, 0.5)
-    axis_args = Dict(:limits=>lims, :xgridvisible=>false, :ygridvisible=>false)
+    axis_args = Dict(:limits=>lims, :xgridvisible=>false, :ygridvisible=>false, :xticks=>-10:5:10)
     #plot_f_unnorm_vs_vpa_z(run_info_dfns; title=L"f_i", outfile=joinpath(output_dir, "f_ion$ext"), xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(0, ion_cbar_max), subtitles=subtitles, axis_args=axis_args)
     #plot_f_unnorm_vs_vpa_z(run_info_dfns; neutral=true, title=L"f_n", outfile=joinpath(output_dir, "f_neutral$ext"), xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(0, neutral_cbar_max), subtitles=subtitles, axis_args=axis_args)
 
     #plot_f_unnorm_vs_vpa_z(run_info_dfns; title=L"f_i", outfile=joinpath(output_dir, "logf_ion$ext"), transform=positive_or_nan, colorscale=log10, xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(1e-16, ion_cbar_max), subtitles=subtitles, axis_args=axis_args)
     #plot_f_unnorm_vs_vpa_z(run_info_dfns; neutral=true, title=L"f_n", outfile=joinpath(output_dir, "logf_neutral$ext"), transform=positive_or_nan, colorscale=log10, xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0, colorrange=(1e-16, neutral_cbar_max), subtitles=subtitles, axis_args=axis_args)
-    for (ri, p, st) ∈ zip(run_info_dfns, prefixes, subtitles)
-        plot_f_unnorm_vs_vpa_z(ri;
-                               outfile=joinpath(output_dir, "$(p)_f_ion$ext"),
-                               xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0,
-                               colorrange=(0, ion_cbar_max), title=st,
-                               axis_args=axis_args)
-        plot_f_unnorm_vs_vpa_z(ri; neutral=true,
-                               outfile=joinpath(output_dir, "$(p)_f_neutral$ext"),
-                               xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0,
-                               colorrange=(0, neutral_cbar_max), title=st,
-                               axis_args=axis_args)
 
-        plot_f_unnorm_vs_vpa_z(ri;
-                               outfile=joinpath(output_dir, "$(p)_logf_ion$ext"),
-                               transform=positive_or_nan, colorscale=log10,
-                               xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0,
-                               colorrange=(1e-16, ion_cbar_max), title=st,
-                               axis_args=axis_args)
-        plot_f_unnorm_vs_vpa_z(ri; neutral=true,
-                               outfile=joinpath(output_dir, "$(p)_logf_neutral$ext"),
-                               transform=positive_or_nan, colorscale=log10,
-                               xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0,
-                               colorrange=(1e-16, neutral_cbar_max), title=st,
-                               axis_args=axis_args)
+    fig = Figure(resolution=(1200, 500))
+
+    # Make column headings
+    for (i, st) ∈ enumerate(short_labels)
+        Label(fig[1, i], st, tellwidth=false)
     end
+
+    axes_ion = [Axis(fig[2,i]; axis_args...) for i ∈ 1:4]
+    axes_neutral = [Axis(fig[3,i]; axis_args...) for i ∈ 1:4]
+    hm_ion = nothing
+    hm_neutral = nothing
+    for (ri, p, ax_ion, ax_neutral) ∈ zip(run_info_dfns, prefixes, axes_ion, axes_neutral)
+        hm_ion = plot_f_unnorm_vs_vpa_z(ri; xlabel=L"v_\parallel", ylabel=L"z",
+                                        rasterize=8.0, colorrange=(0, ion_cbar_max),
+                                        ax=ax_ion, title="")
+        hm_neutral = plot_f_unnorm_vs_vpa_z(ri; neutral=true, xlabel=L"v_\parallel",
+                                            ylabel=L"z", rasterize=8.0,
+                                            colorrange=(0, neutral_cbar_max),
+                                            ax=ax_neutral, title="")
+
+        #plot_f_unnorm_vs_vpa_z(ri;
+        #                       outfile=joinpath(output_dir, "$(p)_logf_ion$ext"),
+        #                       transform=positive_or_nan, colorscale=log10,
+        #                       xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0,
+        #                       colorrange=(1e-16, ion_cbar_max), title=st,
+        #                       axis_args=axis_args)
+        #plot_f_unnorm_vs_vpa_z(ri; neutral=true,
+        #                       outfile=joinpath(output_dir, "$(p)_logf_neutral$ext"),
+        #                       transform=positive_or_nan, colorscale=log10,
+        #                       xlabel=L"v_\parallel", ylabel=L"z", rasterize=4.0,
+        #                       colorrange=(1e-16, neutral_cbar_max), title=st,
+        #                       axis_args=axis_args)
+    end
+
+    for ax ∈ axes_ion
+        hidexdecorations!(ax)
+    end
+    for ax ∈ axes_ion[2:end]
+        hideydecorations!(ax)
+    end
+    for ax ∈ axes_neutral[2:end]
+        hideydecorations!(ax)
+    end
+
+    Label(fig[2,0], L"ion$$", rotation=π/2, tellheight=false)
+    Label(fig[3,0], L"neutral$$", rotation=π/2, tellheight=false)
+
+    Colorbar(fig[2, end+1], hm_ion)
+    Colorbar(fig[3, end], hm_neutral)
+
+    save(joinpath(output_dir, "pdfs$ext"), fig)
 end
 
 main()
