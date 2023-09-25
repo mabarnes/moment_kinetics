cf8a956293a23708d00e4589aafde9a55dabf844
John Omotani <john.omotani@ukaea.uk>
2023-09-25 12:51:31 +0100

    Restrict check for 1V in Krook collisions to moment-kinetic cases
    2V Krook collisions are now implemented for full-f simulations.

--- src/krook_collisions.jl
+++ src/krook_collisions.jl
@@ -42,8 +42,8 @@ Currently Krook collisions
 function krook_collisions!(pdf_out, fvec_in, moments, composition, collisions, vperp, vpa, dt)
     begin_s_r_z_region()
 
-    if vperp.n > 1
-        error("Krook collisions not implemented for 2V case yet")
+    if vperp.n > 1 && (moments.evolve_density || moments.evolve_upar || moments.evolve_ppar)
+        error("Krook collisions not implemented for 2V moment-kinetic cases yet")
     end
 
     # Note: do not need 1/sqrt(pi) for the 'Maxwellian' term because the pdf is already
