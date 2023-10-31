326efbb2cbc28ebe41a306548575570b0ea3e3c5
John Omotani <john.omotani@ukaea.uk>
2023-10-31 10:11:46 +0000

    Fix coordinate creation in makie_post_processing
    Need to add an `element_spacing_option` argument for the dummy
    coordinate input.

--- src/makie_post_processing.jl
+++ src/makie_post_processing.jl
@@ -817,7 +817,7 @@ function get_run_info(run_dir, restart_index=nothing; itime_min=1, itime_max=-1,
             dummy_comm = MPI.COMM_NULL
             dummy_input = grid_input("dummy", 1, 1, 1, 1, 0, 1.0,
                                      "chebyshev_pseudospectral", "", "periodic",
-                                     dummy_adv_input, dummy_comm)
+                                     dummy_adv_input, dummy_comm, "uniform")
             vzeta, vzeta_spectral = define_coordinate(dummy_input)
             vzeta_chunk_size = 1
             vr, vr_spectral = define_coordinate(dummy_input)
