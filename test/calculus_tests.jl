b5bc673a76b9c79241e15993fa6ca20358ee6edf
Michael Hardman <michael.hardman@physics.ox.ac.uk>
2023-11-22 09:47:06 +0000

    Inclusion of the "gausslegendre_pseudospectral" coordinate option into the check in tests for first derivatives.

--- test/calculus_tests.jl
+++ test/calculus_tests.jl
@@ -15,7 +15,7 @@ function runtests()
         println("calculus tests")
         @testset "fundamental theorem of calculus" begin
             @testset "$discretization $ngrid $nelement" for
-                    (discretization, element_spacing_option, etol, cheb_option) ∈ (("finite_difference", "uniform", 1.0e-15, ""), ("chebyshev_pseudospectral", "uniform", 1.0e-15, "FFT"), ("chebyshev_pseudospectral", "uniform", 1.0e-15, "matrix"), ("chebyshev_pseudospectral", "sqrt", 1.0e-2, "FFT")),
+                    (discretization, element_spacing_option, etol, cheb_option) ∈ (("finite_difference", "uniform", 1.0e-15, ""), ("chebyshev_pseudospectral", "uniform", 1.0e-15, "FFT"), ("chebyshev_pseudospectral", "uniform", 1.0e-15, "matrix"), ("chebyshev_pseudospectral", "sqrt", 1.0e-2, "FFT"), ("gausslegendre_pseudospectral", "uniform", 1.0e-14, "")),
                     ngrid ∈ (5,6,7,8,9,10), nelement ∈ (1, 2, 3, 4, 5)
 
                 if discretization == "finite_difference" && (ngrid - 1) * nelement % 2 == 1
@@ -755,6 +755,354 @@ function runtests()
                     element_spacing_option)
                 # create the coordinate struct 'x' and info for derivatives, etc.
                 x, spectral = define_coordinate(input)
+                # test polynomials up to order ngrid-1
+                for n ∈ 0:ngrid-1
+                    # create array for the function f(x) to be differentiated/integrated
+                    f = Array{Float64,1}(undef, x.n)
+                    # create array for the derivative df/dx and the expected result
+                    df = similar(f)
+                    expected_df = similar(f)
+                    # initialize f and expected df
+                    f[:] .= randn(rng)
+                    expected_df .= 0.0
+                    for p ∈ 1:n
+                        coefficient = randn(rng)
+                        @. f += coefficient * x.grid ^ p
+                        @. expected_df += coefficient * p * x.grid ^ (p - 1)
+                    end
+
+                    for advection ∈ (-1.0, 0.0, 1.0)
+                        adv_fac = similar(f)
+                        adv_fac .= advection
+
+                        # differentiate f
+                        derivative!(df, f, x, adv_fac, spectral)
+
+                        # Note the error we might expect for a p=32 polynomial is probably
+                        # something like p*(round-off) for x^p (?) so error on expected_df
+                        # would be p*p*(round-off), or plausibly 1024*(round-off), so
+                        # tolerance of 3e-11 isn't unreasonable.
+                        @test isapprox(df, expected_df, rtol=3.0e-11, atol=3.0e-11,
+                                       norm=maxabs_norm)
+                    end
+                end
+            end
+        end
+        
+        @testset "GaussLegendre pseudospectral derivatives (4 argument), testing periodic functions" verbose=false begin
+            @testset "$nelement $ngrid" for (nelement, ngrid, rtol) ∈
+                    (
+                     (1, 5, 8.e-1),
+                     (1, 6, 2.e-1),
+                     (1, 7, 1.e-1),
+                     (1, 8, 1.e-2),
+                     (1, 9, 5.e-3),
+                     (1, 10, 3.e-3),
+                     (1, 11, 5.e-4),
+                     (1, 12, 5.e-6),
+                     (1, 13, 3.e-6),
+                     (1, 14, 8.e-8),
+                     (1, 15, 4.e-8),
+                     (1, 16, 8.e-10),
+                     (1, 17, 8.e-10),
+                     
+
+                     (2, 4, 2.e-1),
+                     (2, 5, 4.e-2),
+                     (2, 6, 2.e-2),
+                     (2, 7, 4.e-4),
+                     (2, 8, 2.e-4),
+                     (2, 9, 4.e-6),
+                     (2, 10, 2.e-6),
+                     (2, 11, 2.e-8),
+                     (2, 12, 1.e-8),
+                     (2, 13, 1.e-10),
+                     (2, 14, 5.e-11),
+                     (2, 15, 4.e-13),
+                     (2, 16, 2.e-13),
+                     (2, 17, 2.e-13),
+                     
+                     (3, 3, 4.e-1),
+                     (3, 4, 1.e-1),
+                     (3, 5, 1.e-2),
+                     (3, 6, 2.e-3),
+                     (3, 7, 1.e-4),
+                     (3, 8, 1.e-5),
+                     (3, 9, 6.e-7),
+                     (3, 10, 5.e-8),
+                     (3, 11, 2.e-9),
+                     (3, 12, 1.e-10),
+                     (3, 13, 5.e-12),
+                     (3, 14, 3.e-13),
+                     (3, 15, 2.e-13),
+                     (3, 16, 2.e-13),
+                     (3, 17, 2.e-13),
+                     
+                     (4, 3, 3.e-1),
+                     (4, 4, 4.e-2),
+                     (4, 5, 4.e-3),
+                     (4, 6, 4.e-4),
+                     (4, 7, 4.e-5),
+                     (4, 8, 1.e-6),
+                     (4, 9, 8.e-8),
+                     (4, 10, 4.e-9),
+                     (4, 11, 4.e-10),
+                     (4, 12, 4.e-12),
+                     (4, 13, 2.e-13),
+                     (4, 14, 2.e-13),
+                     (4, 15, 2.e-13),
+                     (4, 16, 2.e-13),
+                     (4, 17, 2.e-13),
+                     
+                     (5, 3, 2.e-1),
+                     (5, 4, 2.e-2),
+                     (5, 5, 2.e-3),
+                     (5, 6, 1.e-4),
+                     (5, 7, 1.e-5),
+                     (5, 8, 2.e-7),
+                     (5, 9, 2.e-8),
+                     (5, 10, 3.e-10),
+                     (5, 11, 2.e-11),
+                     (5, 12, 3.e-13),
+                     (5, 13, 2.e-13),
+                     (5, 14, 2.e-13),
+                     (5, 15, 2.e-13),
+                     (5, 16, 2.e-13),
+                     (5, 17, 4.e-13),
+                    )
+
+                # define inputs needed for the test
+                L = 6.0
+                bc = "periodic"
+                # fd_option and adv_input not actually used so given values unimportant
+                fd_option = ""
+                adv_input = advection_input("default", 1.0, 0.0, 0.0)
+                cheb_option = ""
+                # create the 'input' struct containing input info needed to create a
+                # coordinate
+                nelement_local = nelement
+				nrank_per_block = 0 # dummy value
+				irank = 0 # dummy value
+				comm = MPI.COMM_NULL # dummy value
+                element_spacing_option = "uniform"
+				input = grid_input("coord", ngrid, nelement,
+                    nelement_local, nrank_per_block, irank, L,
+                    "gausslegendre_pseudospectral", fd_option, cheb_option, bc, adv_input, comm,
+                    element_spacing_option)
+                # create the coordinate struct 'x' and info for derivatives, etc.
+                x, spectral = define_coordinate(input,init_YY=false)
+
+                offset = randn(rng)
+                f = @. sinpi(2.0 * x.grid / L) + offset
+                expected_df = @. 2.0 * π / L * cospi(2.0 * x.grid / L)
+
+                # create array for the derivative df/dx
+                df = similar(f)
+
+                # differentiate f
+                derivative!(df, f, x, spectral)
+
+                @test isapprox(df, expected_df, rtol=rtol, atol=1.e-14,
+                               norm=maxabs_norm)
+            end
+        end
+
+        @testset "GaussLegendre pseudospectral derivatives upwinding (5 argument), testing periodic functions" verbose=false begin
+            @testset "$nelement $ngrid" for (nelement, ngrid, rtol) ∈
+                    (
+                     (1, 5, 8.e-1),
+                     (1, 6, 2.e-1),
+                     (1, 7, 1.e-1),
+                     (1, 8, 1.e-2),
+                     (1, 9, 5.e-3),
+                     (1, 10, 3.e-3),
+                     (1, 11, 8.e-4),
+                     (1, 12, 5.e-6),
+                     (1, 13, 3.e-6),
+                     (1, 14, 8.e-8),
+                     (1, 15, 4.e-8),
+                     (1, 16, 8.e-10),
+                     (1, 17, 8.e-10),
+                     
+                     (2, 4, 2.e-1),
+                     (2, 5, 4.e-2),
+                     (2, 6, 2.e-2),
+                     (2, 7, 4.e-4),
+                     (2, 8, 2.e-4),
+                     (2, 9, 4.e-6),
+                     (2, 10, 2.e-6),
+                     (2, 11, 2.e-8),
+                     (2, 12, 1.e-8),
+                     (2, 13, 1.e-10),
+                     (2, 14, 5.e-11),
+                     (2, 15, 4.e-13),
+                     (2, 16, 2.e-13),
+                     (2, 17, 2.e-13),
+                     
+                     (3, 3, 4.e-1),
+                     (3, 4, 1.e-1),
+                     (3, 5, 3.e-2),
+                     (3, 6, 2.e-3),
+                     (3, 7, 5.e-4),
+                     (3, 8, 1.e-5),
+                     (3, 9, 1.e-6),
+                     (3, 10, 5.e-8),
+                     (3, 11, 2.e-8),
+                     (3, 12, 1.e-9),
+                     (3, 13, 5.e-11),
+                     (3, 14, 3.e-13),
+                     (3, 15, 2.e-13),
+                     (3, 16, 2.e-13),
+                     (3, 17, 2.e-13),
+                     
+                     (4, 3, 3.e-1),
+                     (4, 4, 4.e-2),
+                     (4, 5, 4.e-3),
+                     (4, 6, 4.e-4),
+                     (4, 7, 4.e-5),
+                     (4, 8, 4.e-6),
+                     (4, 9, 8.e-8),
+                     (4, 10, 4.e-9),
+                     (4, 11, 5.e-10),
+                     (4, 12, 4.e-12),
+                     (4, 13, 2.e-13),
+                     (4, 14, 2.e-13),
+                     (4, 15, 2.e-13),
+                     (4, 16, 2.e-13),
+                     (4, 17, 2.e-13),
+                     
+                     (5, 3, 2.e-1),
+                     (5, 4, 2.e-2),
+                     (5, 5, 2.e-3),
+                     (5, 6, 1.e-4),
+                     (5, 7, 1.e-5),
+                     (5, 8, 4.e-7),
+                     (5, 9, 2.e-8),
+                     (5, 10, 8.e-10),
+                     (5, 11, 2.e-11),
+                     (5, 12, 3.e-13),
+                     (5, 13, 2.e-13),
+                     (5, 14, 2.e-13),
+                     (5, 15, 2.e-13),
+                     (5, 16, 2.e-13),
+                     (5, 17, 4.e-13),
+                    )
+
+                # define inputs needed for the test
+                L = 6.0
+                bc = "periodic"
+                # fd_option and adv_input not actually used so given values unimportant
+                fd_option = ""
+                adv_input = advection_input("default", 1.0, 0.0, 0.0)
+                cheb_option = ""
+                # create the 'input' struct containing input info needed to create a
+                # coordinate
+                nelement_local = nelement
+				nrank_per_block = 0 # dummy value
+				irank = 0 # dummy value
+				comm = MPI.COMM_NULL # dummy value
+                element_spacing_option = "uniform"
+				input = grid_input("coord", ngrid, nelement,
+                    nelement_local, nrank_per_block, irank, L,
+                    "gausslegendre_pseudospectral", fd_option, cheb_option, bc, adv_input, comm,
+                    element_spacing_option)
+                # create the coordinate struct 'x' and info for derivatives, etc.
+                x, spectral = define_coordinate(input,init_YY=false)
+
+                offset = randn(rng)
+                f = @. sinpi(2.0 * x.grid / L) + offset
+                expected_df = @. 2.0 * π / L * cospi(2.0 * x.grid / L)
+
+                # create array for the derivative df/dx
+                df = similar(f)
+
+                for advection ∈ (-1.0, 0.0, 1.0)
+                    adv_fac = similar(f)
+                    adv_fac .= advection
+
+                    # differentiate f
+                    derivative!(df, f, x, adv_fac, spectral)
+
+                    @test isapprox(df, expected_df, rtol=rtol, atol=1.e-12,
+                                   norm=maxabs_norm)
+                end
+            end
+        end
+        
+        @testset "GaussLegendre pseudospectral derivatives (4 argument), testing exact polynomials" verbose=false begin
+            @testset "$nelement $ngrid" for bc ∈ ("constant", "zero"), element_spacing_option ∈ ("uniform", "sqrt"),
+                    nelement ∈ (1:5), ngrid ∈ (3:17)
+                    
+                # define inputs needed for the test
+                L = 1.0
+                bc = "constant"
+                # fd_option and adv_input not actually used so given values unimportant
+                fd_option = ""
+                adv_input = advection_input("default", 1.0, 0.0, 0.0)
+                cheb_option = "" #not used
+                # create the 'input' struct containing input info needed to create a
+                # coordinate
+                nelement_local = nelement
+				nrank_per_block = 0 # dummy value
+				irank = 0 # dummy value
+				comm = MPI.COMM_NULL # dummy value
+                input = grid_input("coord", ngrid, nelement,
+                    nelement_local, nrank_per_block, irank, L,
+                    "gausslegendre_pseudospectral", fd_option, cheb_option, bc, adv_input, comm,
+                    element_spacing_option)
+                # create the coordinate struct 'x' and info for derivatives, etc.
+                x, spectral = define_coordinate(input,init_YY=false)
+                # test polynomials up to order ngrid-1
+                for n ∈ 0:ngrid-1
+                    # create array for the function f(x) to be differentiated/integrated
+                    f = Array{Float64,1}(undef, x.n)
+                    # create array for the derivative df/dx and the expected result
+                    df = similar(f)
+                    expected_df = similar(f)
+                    # initialize f and expected df
+                    f[:] .= randn(rng)
+                    expected_df .= 0.0
+                    for p ∈ 1:n
+                        coefficient = randn(rng)
+                        @. f += coefficient * x.grid ^ p
+                        @. expected_df += coefficient * p * x.grid ^ (p - 1)
+                    end
+                    # differentiate f
+                    derivative!(df, f, x, spectral)
+
+                    # Note the error we might expect for a p=32 polynomial is probably
+                    # something like p*(round-off) for x^p (?) so error on expected_df would
+                    # be p*p*(round-off), or plausibly 1024*(round-off), so tolerance of
+                    # 2e-11 isn't unreasonable.
+                    @test isapprox(df, expected_df, rtol=2.0e-11, atol=6.0e-12,
+                                   norm=maxabs_norm)
+                end
+            end
+        end
+        
+        @testset "GaussLegendre pseudospectral derivatives upwinding (5 argument), testing exact polynomials" verbose=false begin
+            @testset "$nelement $ngrid" for bc ∈ ("constant", "zero"), element_spacing_option ∈ ("uniform", "sqrt"),
+                    nelement ∈ (1:5), ngrid ∈ (3:17)
+
+                # define inputs needed for the test
+                L = 1.0
+                bc = "constant"
+                # fd_option and adv_input not actually used so given values unimportant
+                fd_option = ""
+                adv_input = advection_input("default", 1.0, 0.0, 0.0)
+                cheb_option = "" # not used
+                # create the 'input' struct containing input info needed to create a
+                # coordinate
+                nelement_local = nelement
+				nrank_per_block = 0 # dummy value
+				irank = 0 # dummy value
+				comm = MPI.COMM_NULL # dummy value
+                input = grid_input("coord", ngrid, nelement,
+                    nelement_local, nrank_per_block, irank, L,
+                    "gausslegendre_pseudospectral", fd_option, cheb_option, bc, adv_input, comm,
+                    element_spacing_option)
+                # create the coordinate struct 'x' and info for derivatives, etc.
+                x, spectral = define_coordinate(input,init_YY=false)
                 # test polynomials up to order ngrid-1
                 for n ∈ 0:ngrid-1
                     # create array for the function f(x) to be differentiated/integrated
