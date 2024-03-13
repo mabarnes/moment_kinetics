9214b5ece6992c8dc2c9566d3d27ad85def5ef2e
John Omotani <john.omotani@ukaea.uk>
2024-03-13 15:44:57 +0000

    Use Documenter.jl-provided Docker image to build pdf docs on CI
    Avoids needing to install LaTeX, etc. on the CI server.

--- docs/make-pdf.jl
+++ docs/make-pdf.jl
@@ -26,9 +26,15 @@ doc_files = glob("src/*.md")
 # Remove the src/ prefix
 doc_files = [basename(s) for s ∈ doc_files]
 
+if get(ENV, "CI", nothing) == "true"
+    latex_kwargs = (platform = "docker",)
+else
+    latex_kwargs = ()
+end
+
 makedocs(
     sitename = "momentkinetics",
-    format = Documenter.LaTeX(),
+    format = Documenter.LaTeX(; latex_kwargs...),
     modules = [moment_kinetics, makie_post_processing, plots_post_processing],
     authors = "M. Barnes, J.T. Omotani, M. Hardman",
     pages = doc_files
