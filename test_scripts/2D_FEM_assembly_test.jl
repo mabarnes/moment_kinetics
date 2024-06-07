0d767c6cb2195f1a4a267c6c095a02df13d94a40
Michael Hardman <michael.hardman@physics.ox.ac.uk>
2024-06-07 07:47:19 +0100

    Reorder definition of vpa and vperp structs and the initialisation of the distributed memory MPI. This prevents an MPI error described in issue #221. However, the script is producing NaN and Inf values for errors in situations where it should not, suggesting other errors have been introduced elsewhere in the script.

--- test_scripts/2D_FEM_assembly_test.jl
+++ test_scripts/2D_FEM_assembly_test.jl
@@ -112,14 +112,16 @@ end
         println("made inputs")
         println("vpa: ngrid: ",ngrid," nelement: ",nelement_local_vpa, " Lvpa: ",Lvpa)
         println("vperp: ngrid: ",ngrid," nelement: ",nelement_local_vperp, " Lvperp: ",Lvperp)
-        vpa, vpa_spectral = define_coordinate(vpa_input)
-        vperp, vperp_spectral = define_coordinate(vperp_input)
+        #vpa, vpa_spectral = define_coordinate(vpa_input,ignore_MPI=false)
+        #vperp, vperp_spectral = define_coordinate(vperp_input,ignore_MPI=false)
         
         # Set up MPI
         if standalone
             initialize_comms!()
         end
         setup_distributed_memory_MPI(1,1,1,1)
+        vpa, vpa_spectral = define_coordinate(vpa_input,ignore_MPI=false)
+        vperp, vperp_spectral = define_coordinate(vperp_input,ignore_MPI=false)
         looping.setup_loop_ranges!(block_rank[], block_size[];
                                        s=1, sn=1,
                                        r=1, z=1, vperp=vperp.n, vpa=vpa.n,
