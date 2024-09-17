eab874259cf9bbd8a64beb998dca9d31ac3c0581
Michael Hardman <29800382+mrhardman@users.noreply.github.com>
2024-09-17 16:02:25 +0100

    Initial Poisson solver for (polar,radial) problem using 2D Gauss-Legendre tensor product elements (not using Fourier transform).

--- moment_kinetics/src/spatial_poisson.jl
+++ moment_kinetics/src/spatial_poisson.jl
@@ -8,6 +8,8 @@ module spatial_poisson
 
 export init_spatial_poisson
 export spatial_poisson_solve!
+export init_spatial_poisson2D
+export spatial_poisson2D_solve!
 
 using ..type_definitions: mk_float, mk_int
 using ..gauss_legendre: get_QQ_local!
@@ -18,7 +20,12 @@ using LinearAlgebra: ldiv!, mul!, LU, lu
 using SuiteSparse
 using ..fourier: fourier_info, fourier_forward_transform!, fourier_backward_transform! 
 using ..moment_kinetics_structs: null_spatial_dimension_info
-
+using ..sparse_matrix_functions: icsc_func, ic_func, allocate_sparse_matrix_constructor,
+                                 assemble_constructor_data!, assign_constructor_data!,
+                                 sparse_matrix_constructor, create_sparse_matrix,
+                                 get_global_compound_index
+using ..moment_kinetics_structs: weak_discretization_info
+                                 
 struct poisson_arrays 
    # an array of lu objects for solving the mth polar harmonic of Poisson's equation
    laplacian_lu_objs::Array{SuiteSparse.UMFPACK.UmfpackLU{mk_float,mk_int},1}
@@ -187,4 +194,228 @@ function spatial_poisson_solve!(phi,rho,poisson_arrays,radial,polar,polar_spectr
    return nothing
 end
 
+# functions associated with a 2D (polar,radial) solver
+# using a 2D sparse matrix
+struct poisson2D_arrays{M <: AbstractSparseArray{mk_float,mk_int,N} where N}
+   # an array of lu objects for solving the mth polar harmonic of Poisson's equation
+   laplacian_lu_obj::SuiteSparse.UMFPACK.UmfpackLU{mk_float,mk_int}
+   # an Array of 2D arrays (one 1D operator on r for
+   # each Fourier component in the polar coordinate
+   laplacian2D::M
+   # the matrix that needs to multiply the nodal values of the source function
+   massmatrix2D::M
+   # dummy array for forming RHS in LU solve
+   rhspr::Array{mk_float,2}
 end
+
+"""
+function to initialise the arrays needed for the 2D weak-form
+Poisson's equation problem
+ radial = a coordinate struct of a cylindrical=true coordinate
+ polar = a coordinate struct of a periodic polar coordinate with length = 2pi
+"""
+function init_spatial_poisson2D(radial::coordinate, polar::coordinate, 
+                                radial_spectral::weak_discretization_info,
+                                polar_spectral::weak_discretization_info)
+   npolar = polar.n
+   nradial = radial.n
+   LP2D_sparse, MR2D_sparse = init_sparse_laplacian2D(radial,polar,radial_spectral,polar_spectral)
+   laplacian_lu = lu(LP2D_sparse)
+   rhspr = allocate_float(npolar,nradial)
+   return poisson2D_arrays(laplacian_lu,LP2D_sparse,MR2D_sparse,rhspr)
+end
+
+"""
+This function makes sparse matrices representing the 2D Laplacian operator.
+The compound index runs over the radial and polar coordinates. 
+We use 
+"""
+function init_sparse_laplacian2D(radial::coordinate, polar::coordinate, 
+                                radial_spectral::weak_discretization_info,
+                                polar_spectral::weak_discretization_info)
+    nradial = radial.n
+    npolar = polar.n
+    
+    ngrid_radial = radial.ngrid
+    ngrid_polar = polar.ngrid
+    nelement_radial = radial.nelement_local
+    nelement_polar = polar.nelement_local
+    
+    ntot_polar = (polar.nelement_local - 1)*(polar.ngrid^2 - 1) + polar.ngrid^2
+    ntot_radial = (radial.nelement_local - 1)*(radial.ngrid^2 - 1) + radial.ngrid^2
+    nsparse = ntot_polar*ntot_radial
+    
+    MR2D = allocate_sparse_matrix_constructor(nsparse)
+    # Laplacian matrix
+    LP2D = allocate_sparse_matrix_constructor(nsparse)
+    # local dummy arrays
+    MMpolar = Array{mk_float,2}(undef,ngrid_polar,ngrid_polar)
+    KKpolar = Array{mk_float,2}(undef,ngrid_polar,ngrid_polar)
+    MNradial = Array{mk_float,2}(undef,ngrid_radial,ngrid_radial)
+    MRradial = Array{mk_float,2}(undef,ngrid_radial,ngrid_radial)
+    KJradial = Array{mk_float,2}(undef,ngrid_radial,ngrid_radial)
+    PPradial = Array{mk_float,2}(undef,ngrid_radial,ngrid_radial)
+    # loop over elements to carry out assembly
+    for ielement_radial in 1:nelement_radial
+        # get local radial 1D matrices
+        get_QQ_local!(MNradial,ielement_radial,radial_spectral.lobatto,radial_spectral.radau,radial,"N")
+        get_QQ_local!(MRradial,ielement_radial,radial_spectral.lobatto,radial_spectral.radau,radial,"R")
+        get_QQ_local!(KJradial,ielement_radial,radial_spectral.lobatto,radial_spectral.radau,radial,"J")
+        get_QQ_local!(PPradial,ielement_radial,radial_spectral.lobatto,radial_spectral.radau,radial,"P")
+        for ielement_polar in 1:nelement_polar
+            # get local polar 1D matrices
+            get_QQ_local!(MMpolar,ielement_polar,polar_spectral.lobatto,polar_spectral.radau,polar,"M")
+            get_QQ_local!(KKpolar,ielement_polar,polar_spectral.lobatto,polar_spectral.radau,polar,"K")
+            # loop over grid points
+            for iradialp_local in 1:ngrid_radial
+                for iradial_local in 1:ngrid_radial
+                    for ipolarp_local in 1:ngrid_polar
+                        for ipolar_local in 1:ngrid_polar
+                            ic_global = get_global_compound_index(polar,radial,ielement_polar,ielement_radial,ipolar_local,iradial_local)
+                            icp_global = get_global_compound_index(polar,radial,ielement_polar,ielement_radial,ipolarp_local,iradialp_local)
+                            icsc = icsc_func(ipolar_local,ipolarp_local,ielement_polar,
+                                           ngrid_polar,nelement_polar,
+                                           iradial_local,iradialp_local,
+                                           ielement_radial,
+                                           ngrid_radial,nelement_radial)
+                            # boundary condition possibilities
+                            lower_boundary_row_polar = (ielement_polar == 1 && ipolar_local == 1)
+                            upper_boundary_row_polar = (ielement_polar == polar.nelement_local && ipolar_local == polar.ngrid)
+                            lower_boundary_row_radial = (ielement_radial == 1 && iradial_local == 1)
+                            upper_boundary_row_radial = (ielement_radial == radial.nelement_local && iradial_local == radial.ngrid)
+                            
+                            # Laplacian assembly
+                            if upper_boundary_row_radial
+                                # Dirichlet boundary condition on outer boundary
+                                if iradialp_local == radial.ngrid && ipolar_local == ipolarp_local
+                                    assign_constructor_data!(LP2D,icsc,ic_global,icp_global,1.0)
+                                else
+                                    assign_constructor_data!(LP2D,icsc,ic_global,icp_global,0.0)
+                                end
+                            elseif upper_boundary_row_polar
+                                # ensure periodicity by setting this row to 0 apart from appropriately placed 1, -1.
+                                if ipolarp_local == ngrid_polar && iradial_local == iradialp_local
+                                    # assign -1 to this point using loop icsc, ic, icp
+                                    assign_constructor_data!(LP2D,icsc,ic_global,icp_global,-1.0)
+                                    # find other end of polar domain for this ipolar_local
+                                    # and set ipolarp_local = ielement_polar = 1.
+                                    icp_global_bc = get_global_compound_index(polar,radial,1,ielement_radial,1,iradialp_local)
+                                    icsc_bc = icsc_func(ipolar_local,1,1,
+                                           ngrid_polar,nelement_polar,
+                                           iradial_local,iradialp_local,
+                                           ielement_radial,
+                                           ngrid_radial,nelement_radial)
+                                    # assign 1 to this point using ic from loop and updated icsc, icp
+                                    assign_constructor_data!(LP2D,icsc_bc,ic_global,icp_global_bc,1.0)
+                                else 
+                                    assign_constructor_data!(LP2D,icsc,ic_global,icp_global,0.0)
+                                end
+                            else 
+                                # carry out assembly
+                                # of Laplacian matrix data
+                                assemble_constructor_data!(LP2D,icsc,ic_global,icp_global,
+                                            (MMpolar[ipolar_local,ipolarp_local]*
+                                             (KJradial[iradial_local,iradialp_local] - 
+                                              PPradial[iradial_local,iradialp_local]) +
+                                             KKpolar[ipolar_local,ipolarp_local]*
+                                             MNradial[iradial_local,iradialp_local]))
+                                if lower_boundary_row_polar
+                                    # assemble the contributions from the upper_boundary_row_polar location
+                                    icp_global_bc = get_global_compound_index(polar,radial,nelement_polar,ielement_radial,ipolarp_local,iradialp_local)
+                                    icsc_bc = icsc_func(ipolar_local,ipolarp_local,nelement_polar,
+                                           ngrid_polar,nelement_polar,
+                                           iradial_local,iradialp_local,
+                                           ielement_radial,
+                                           ngrid_radial,nelement_radial)
+                                    assemble_constructor_data!(LP2D,icsc_bc,ic_global,icp_global_bc,
+                                            (MMpolar[ipolar_local,ipolarp_local]*
+                                             (KJradial[iradial_local,iradialp_local] - 
+                                              PPradial[iradial_local,iradialp_local]) +
+                                             KKpolar[ipolar_local,ipolarp_local]*
+                                             MNradial[iradial_local,iradialp_local]))    
+                                end
+                            end # Laplcian assembly
+                            
+                            # mass matrices for RHS of matrix equation (enforce periodicity only)
+                            if upper_boundary_row_polar
+                                # ensure periodicity by setting this row to 0 apart from appropriately placed 1, -1.
+                                if ipolarp_local == ngrid_polar && iradial_local == iradialp_local
+                                    # assign -1 to this point using loop icsc, ic, icp
+                                    assign_constructor_data!(MR2D,icsc,ic_global,icp_global,-1.0)
+                                    # find other end of polar domain for this ipolar_local
+                                    # and set ipolarp_local = ielement_polar = 1.
+                                    icp_global_bc = get_global_compound_index(polar,radial,1,ielement_radial,1,iradialp_local)
+                                    icsc_bc = icsc_func(ipolar_local,1,1,
+                                           ngrid_polar,nelement_polar,
+                                           iradial_local,iradialp_local,
+                                           ielement_radial,
+                                           ngrid_radial,nelement_radial)
+                                    # assign 1 to this point using ic from loop and updated icsc, icp
+                                    assign_constructor_data!(MR2D,icsc_bc,ic_global,icp_global_bc,1.0)
+                                else 
+                                    assign_constructor_data!(MR2D,icsc,ic_global,icp_global,0.0)
+                                end
+                            else 
+                                # carry out assembly
+                                # of mass matrix data
+                                assemble_constructor_data!(MR2D,icsc,ic_global,icp_global,
+                                            (MMpolar[ipolar_local,ipolarp_local]*
+                                             MRradial[iradial_local,iradialp_local]))
+                                if lower_boundary_row_polar
+                                    # assemble the contributions from the upper_boundary_row_polar location
+                                    icp_global_bc = get_global_compound_index(polar,radial,nelement_polar,ielement_radial,ipolarp_local,iradialp_local)
+                                    icsc_bc = icsc_func(ipolar_local,ipolarp_local,nelement_polar,
+                                           ngrid_polar,nelement_polar,
+                                           iradial_local,iradialp_local,
+                                           ielement_radial,
+                                           ngrid_radial,nelement_radial)
+                                    assemble_constructor_data!(MR2D,icsc_bc,ic_global,icp_global_bc,
+                                            (MMpolar[ipolar_local,ipolarp_local]*
+                                             MRradial[iradial_local,iradialp_local]))    
+                                end # mass matrix assembly
+                            end
+                        end                         
+                    end                         
+                end                         
+            end                         
+        end                         
+    end                         
+    LP2D_sparse = create_sparse_matrix(LP2D)
+    MR2D_sparse = create_sparse_matrix(MR2D)
+                            
+    return LP2D_sparse, MR2D_sparse
+end
+
+"""
+function for carrying out a 2D Poisson solve, solving the equation
+(1/r) d( r d phi / d r) d r + (1/r^2)d^2 phi/ d polar^2 = rho
+with 
+ phi = an Array[polar,radial] representing phi(polar,r)
+ rho = an Array[polar,radial] representing rho(polar,r)
+ poisson = a struct of type poisson2D_arrays
+
+The vec() function is used to get 1D views of the 2D arrays of shape Array{mk_float,2}(npolar,nradial)
+phi has assumed zero boundary conditions at r = L, and we assume r spans (0,L].
+"""
+function spatial_poisson2D_solve!(phi::Array{mk_float,2},rho::Array{mk_float,2},
+                                  poisson::poisson2D_arrays)
+   laplacian_lu_obj = poisson.laplacian_lu_obj
+   massmatrix2D = poisson.massmatrix2D
+   rhspr = poisson.rhspr
+   # get data into the compound index format
+   # by making 1D views of the array with vec()
+   rhoc = vec(rho)
+   rhsc = vec(rhspr)
+   phic = vec(phi)
+   # form RHS of LP2D . phic = MR2D . rhoc
+   mul!(rhsc,massmatrix2D,rhoc)
+   # enforce the boundary conditions using the original 2D reference to the array
+   # phi(r=L) = 0.0
+   @. rhspr[:,end] = 0.0
+   # solve system with LU decomposition
+   ldiv!(phic,laplacian_lu_obj,rhsc)
+   return nothing
+end
+
+
+end 
--- test_scripts/PoissonSolve.jl
+++ test_scripts/PoissonSolve.jl
@@ -10,6 +10,7 @@ using moment_kinetics.input_structs: grid_input, advection_input
 using moment_kinetics.coordinates: define_coordinate
 using moment_kinetics.type_definitions: mk_float, mk_int
 using moment_kinetics.spatial_poisson: init_spatial_poisson, spatial_poisson_solve!
+using moment_kinetics.spatial_poisson: init_spatial_poisson2D, spatial_poisson2D_solve!
 using moment_kinetics.communication
 using moment_kinetics.looping
 using moment_kinetics.calculus: derivative!
@@ -127,9 +128,93 @@ function run_poisson_test(; nelement_radial=5,ngrid_radial=5,Lradial=1.0,nelemen
    return nothing
 end
 
+function run_poisson2D_test(; nelement_radial=5,ngrid_radial=5,Lradial=1.0,nelement_polar=5,ngrid_polar=5,Lpolar=2.0*pi,kk::mk_int=1,plot_results=false)
+
+   nelement_local_polar = nelement_polar # number of elements per rank
+   nelement_global_polar = nelement_local_polar # total number of elements 
+   nelement_local_radial = nelement_radial # number of elements per rank
+   nelement_global_radial = nelement_local_radial # total number of elements 
+   bc = "none" #not required to take a particular value, not used, set to "none" to avoid extra BC impositions 
+   # fd_option and adv_input not actually used so given values unimportant
+   fd_option = "fourth_order_centered"
+   cheb_option = "matrix"
+   adv_input = advection_input("default", 1.0, 0.0, 0.0)
+   nrank = 1
+   irank = 0
+   comm = MPI.COMM_NULL
+   # create the 'input' struct containing input info needed to create a
+   # coordinate
+   element_spacing_option = "uniform"
+   polar_input = grid_input("polar", ngrid_polar, nelement_global_polar, nelement_local_polar, 
+      nrank, irank, Lpolar, "gausslegendre_pseudospectral", fd_option, cheb_option, bc, adv_input,comm,element_spacing_option)
+   radial_input = grid_input("r", ngrid_radial, nelement_global_radial, nelement_local_radial, 
+      nrank, irank, Lradial, "gausslegendre_pseudospectral", fd_option, cheb_option, bc, adv_input,comm,element_spacing_option)
+   println("made inputs")
+  
+   # Set up MPI
+   initialize_comms!()
+   setup_distributed_memory_MPI(1,1,1,1)
+   polar, polar_spectral = define_coordinate(polar_input)
+   radial, radial_spectral = define_coordinate(radial_input)
+   looping.setup_loop_ranges!(block_rank[], block_size[];
+                                 s=1, sn=1,
+                                 r=radial.n, z=polar.n, vperp=1, vpa=1,
+                                 vzeta=1, vr=1, vz=1)
+   
+   begin_serial_region()
+  
+   println("Test Poisson")
+   poisson = init_spatial_poisson2D(radial,polar,radial_spectral,polar_spectral)
+   phi = allocate_float(polar.n,radial.n)
+   exact_phi = allocate_float(polar.n,radial.n)
+   err_phi = allocate_float(polar.n,radial.n)
+   rho = allocate_float(polar.n,radial.n)
+   
+   for irad in 1:radial.n
+      for ipol in 1:polar.n
+         exact_phi[ipol,irad] = 0.25*(radial.grid[irad]^2 -1)
+         rho[ipol,irad] = 1.0
+      end
+   end
+   spatial_poisson2D_solve!(phi,rho,poisson)
+   println("Maximum error value Test rho=1 : ",maximum(abs.(phi- exact_phi)))
+   if plot_results
+      plot_test_data(exact_phi,phi,err_phi,"phi_1",radial,polar)
+      println("phi(theta=-pi): ",phi[1,:])
+      println("phi(theta=pi): ",phi[end,:])
+   end
+   
+   if kk < 1
+      error("ERROR: kk >=1 required for test")
+   end
+   if !(mod(kk,1) == 0)
+      error("ERROR: kk integer required for test")
+   end
+   if abs(polar.L - 2.0*pi) >1.0e-14
+      error("ERROR: polar coordinate assumed angle for definition of the following test - set Lpolar = 2.0*pi")
+   end
+   
+   for irad in 1:radial.n
+      for ipol in 1:polar.n
+         exact_phi[ipol,irad] = (1.0 - radial.grid[irad])*(radial.grid[irad]^kk)*cos(2.0*pi*kk*polar.grid[ipol]/polar.L)
+         rho[ipol,irad] = (kk^2 - (kk+1)^2)*(radial.grid[irad]^(kk-1))*cos(2.0*kk*pi*polar.grid[ipol]/polar.L)
+      end
+   end
+   
+   spatial_poisson2D_solve!(phi,rho,poisson)
+   @. err_phi = abs(phi - exact_phi)
+   println("Maximum error value Test rho = (kk^2 - (kk+1)^2) * cos(2 pi kk P/L) * r^(kk-1): ",maximum(err_phi))
+   if plot_results
+      plot_test_data(exact_phi,phi,err_phi,"phi_2",radial,polar)
+   end
+   finalize_comms!()
+   return nothing
+end
+
 if abspath(PROGRAM_FILE) == @__FILE__
     using Pkg
     Pkg.activate(".")
     
     run_poisson_test()
+    run_poisson2D_test()
 end
