31c7ec702c47cd871bdc527769f39b0b573f0bec
John Omotani <john.omotani@ukaea.uk>
2024-10-11 17:21:52 +0100

    Get moment_kinetics version number for online docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Pkg
 
 Pkg.instantiate()
 
-using Documenter
+using Documenter, UUIDs
 using moment_kinetics, makie_post_processing, plots_post_processing
 
 makedocs(
@@ -10,6 +10,13 @@ makedocs(
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true",
                              size_threshold = 1000000,
                              size_threshold_warn = 500000,
+                             # Use the following horrible incantation to get the version
+                             # of moment_kinetics. moment_kinetics is the package with the
+                             # UUID being used here. We need to do this because the
+                             # Project.toml in the top-level directory is user-generated
+                             # and does not have a version, but this is the Project.toml
+                             # that would be used by default by Documenter.jl.
+                             inventory_version = Pkg.dependencies()[UUID("b5ff72cc-06fc-4161-ad14-dba1c22ed34e")].version,
                             ),
     modules = [moment_kinetics, makie_post_processing, plots_post_processing],
 )
