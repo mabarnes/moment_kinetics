534196728176727d0fcc541c471e60fb0416195f
John Omotani <john.omotani@ukaea.uk>
2024-12-04 10:37:51 +0000

    more fix wall adi

--- moment_kinetics/src/electron_kinetic_equation.jl
+++ moment_kinetics/src/electron_kinetic_equation.jl
@@ -3777,7 +3777,7 @@ boundary condition on those entries of δg (when the right-hand-side is set to z
             dc3_dp = get_part3_for_one_moment_upper(vpa.scratch)
             @. vpa.scratch *= vpa_unnorm / vthe_upper
             dd3_dp = get_part3_for_one_moment_upper(vpa.scratch)
-            @. vpa.scratch = -jacobian_zend_ppar * vpa.wgts / sqrt(π) * integral_correction_sharpness * vpa_unnorm^2 / vthe_upper / (1.0 + integral_correction_sharpness * vpa_unnorm^2 / vthe_upper^2)
+            @. vpa.scratch = -jacobian_zend_ppar * vpa.wgts / sqrt(π) * integral_correction_sharpness * vpa_unnorm^2 / vthe_upper^2 / (1.0 + integral_correction_sharpness * vpa_unnorm^2 / vthe_upper^2)
             vpa.scratch[sigma_ind:sigma_ind+1] .= 0.0
             dalpha_dp_interp = get_part3_for_one_moment_upper(vpa.scratch)
             @. vpa.scratch *= vpa_unnorm / vthe_upper
