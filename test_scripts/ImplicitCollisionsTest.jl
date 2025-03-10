21f022ffbc14ff3426dcaf4b7929b9f416ba9eeb
Michael Hardman <29800382+mrhardman@users.noreply.github.com>
2025-03-10 10:48:30 +0000

    Add implicit z advection to be used in conjunction with ImplicitCollisionsTest.jl and test_implicit_standard_dke_collisions()

--- test_scripts/ImplicitCollisionsTest.jl
+++ test_scripts/ImplicitCollisionsTest.jl
@@ -24,6 +24,7 @@ using moment_kinetics.communication: MPISharedArray
 using moment_kinetics.looping
 using moment_kinetics.input_structs: direct_integration, multipole_expansion
 using moment_kinetics.nonlinear_solvers
+using moment_kinetics.z_advection: init_z_advection_implicit, z_advection_implicit_advance!
 
 function plot_test_data(func_exact,func_num,func_err,func_name,vpa,vperp)
     @views heatmap(vperp.grid, vpa.grid, func_num[:,:], ylabel=L"v_{\|\|}", xlabel=L"v_{\perp}", c = :deep, interpolation = :cubic,
@@ -88,6 +89,25 @@ function diagnose_F_gif(pdf,vpa,vperp,ntime)
             @views heatmap(vperp.grid, vpa.grid, pdf[:,:,it], xlabel="vperp", ylabel="vpa", c = :deep, interpolation = :cubic)
         end
         outfile = string("implicit_collisions_pdf_vs_vpa_vperp.gif")
+        gif(anim, outfile, fps=5)
+    end
+end
+
+function diagnose_F_gif(pdf,vpa,vperp,z,ntime)
+    begin_serial_region()
+    @serial_region begin
+        iz = 1
+        anim = @animate for it in 1:ntime
+            @views heatmap(vperp.grid, vpa.grid, pdf[:,:,iz,it], xlabel="vperp", ylabel="vpa", c = :deep, interpolation = :cubic)
+        end
+        outfile = string("implicit_collisions_pdf_vs_vpa_vperp_iz_1.gif")
+        gif(anim, outfile, fps=5)
+        
+        ivperp = 1
+        anim = @animate for it in 1:ntime
+            @views heatmap(z.grid, vpa.grid, pdf[:,ivperp,:,it], xlabel="z", ylabel="vpa", c = :deep, interpolation = :cubic)
+        end
+        outfile = string("implicit_collisions_pdf_vs_vpa_z_ivperp_1.gif")
         gif(anim, outfile, fps=5)
     end
 end
@@ -221,7 +241,7 @@ function test_implicit_collisions(; vth0=0.5,vperp0=1.0,vpa0=0.0, ngrid=3,neleme
 
     #println(nl_solver_params.preconditioners)
     for it in 1:ntime
-        backward_euler_step!(Fnew, Fold, delta_t, ms, msp, nussp, fkpl_arrays, dummy_vpavperp,
+        fokker_planck_backward_euler_step!(Fnew, Fold, delta_t, ms, msp, nussp, fkpl_arrays, dummy_vpavperp,
             vperp, vpa, vperp_spectral, vpa_spectral, coords,
             Fdummy1, Fdummy2, Fdummy3, Fdummy4, Fdummy5, nl_solver_params,
             test_numerical_conserving_terms=test_numerical_conserving_terms,
@@ -259,7 +279,7 @@ function test_implicit_collisions(; vth0=0.5,vperp0=1.0,vpa0=0.0, ngrid=3,neleme
     end    
 end
 
-function backward_euler_step!(Fnew, Fold, delta_t, ms, msp, nussp, fkpl_arrays, dummy_vpavperp,
+function fokker_planck_backward_euler_step!(Fnew, Fold, delta_t, ms, msp, nussp, fkpl_arrays, dummy_vpavperp,
     vperp, vpa, vperp_spectral, vpa_spectral, coords,
     Fresidual, F_delta_x, F_rhs_delta, Fv, Fw, nl_solver_params;
     test_numerical_conserving_terms=false,
@@ -272,7 +292,8 @@ function backward_euler_step!(Fnew, Fold, delta_t, ms, msp, nussp, fkpl_arrays, 
     use_Maxwellian_field_particle_distribution=false,
     algebraic_solve_for_d2Gdvperp2=false,
     calculate_GG = false, calculate_dGdvperp=false,
-    boundary_data_option=multipole_expansion)
+    boundary_data_option=multipole_expansion,
+    standalone=true)
     
     # residual function to be used for Newton-Krylov
     function residual_func!(Fresidual, Fnew; krylov=false)
@@ -299,8 +320,9 @@ function backward_euler_step!(Fnew, Fold, delta_t, ms, msp, nussp, fkpl_arrays, 
         return nothing
     end
     
-    begin_s_r_z_anyv_region()
-    
+    if standalone
+        begin_s_r_z_anyv_region()
+    end
     if test_particle_preconditioner
       calculate_test_particle_preconditioner!(Fold,delta_t,ms,msp,nussp,
         vpa,vperp,vpa_spectral,vperp_spectral,
@@ -336,9 +358,225 @@ function backward_euler_step!(Fnew, Fold, delta_t, ms, msp, nussp, fkpl_arrays, 
                       coords, right_preconditioner=right_preconditioner)
     end
     _anyv_subblock_synchronize()
-    begin_serial_region()
+    #begin_serial_region()
 end    
 
+function test_implicit_standard_dke_collisions(; vth0=0.5,vperp0=1.0,vpa0=0.0, ngrid=3,nelement_vpa=8,nelement_vperp=4,ngrid_z=3,nelement_z=2,
+    Lvpa=6.0,Lvperp=3.0,Lz=1.0,ntime=1,delta_t=1.0, nu_source = 1.0,
+    z_element_spacing_option="uniform",
+    restart = 8,
+    max_restarts = 1,
+    atol = 1.0e-10,
+    serial_solve = false,
+    anyv_region = true,
+    test_particle_preconditioner=false,
+    test_linearised_advance=false,
+    use_Maxwellian_Rosenbluth_coefficients_in_preconditioner=false,
+    plot_test_output=false,
+    test_parallelism=false,test_self_operator=true,
+    test_dense_construction=false,standalone=false,
+    use_Maxwellian_Rosenbluth_coefficients=false,
+    use_Maxwellian_field_particle_distribution=false,
+    test_numerical_conserving_terms=false,
+    algebraic_solve_for_d2Gdvperp2=false,
+    boundary_data_option=multipole_expansion)
+    
+    nelement_local_vpa = nelement_vpa # number of elements per rank
+    nelement_global_vpa = nelement_local_vpa # total number of elements 
+    nelement_local_vperp = nelement_vperp # number of elements per rank
+    nelement_global_vperp = nelement_local_vperp # total number of elements 
+    bc = "" #not required to take a particular value, not used 
+    # fd_option and adv_input not actually used so given values unimportant
+    #discretization = "chebyshev_pseudospectral"
+    discretization = "gausslegendre_pseudospectral"
+    element_spacing_option = "uniform"
+    
+    # Set up MPI
+    if standalone
+        initialize_comms!()
+    end
+    setup_distributed_memory_MPI(1,1,1,1)
+    coords_input = OptionsDict(
+        "z"=>OptionsDict("ngrid"=>ngrid_z, "nelement"=>nelement_z,
+                                "nelement_local"=>nelement_z, "L"=>Lz,
+                                "discretization"=>discretization,
+                                "element_spacing_option"=>z_element_spacing_option,
+                                "bc"=>"none"),
+        "vperp"=>OptionsDict("ngrid"=>ngrid, "nelement"=>nelement_global_vperp,
+                                "nelement_local"=>nelement_local_vperp, "L"=>Lvperp,
+                                "discretization"=>discretization,
+                                "element_spacing_option"=>element_spacing_option,
+                                "bc"=>"none"),
+        "vpa"=>OptionsDict("ngrid"=>ngrid, "nelement"=>nelement_global_vpa,
+                            "nelement_local"=>nelement_local_vpa, "L"=>Lvpa,
+                            "discretization"=>discretization,
+                            "element_spacing_option"=>element_spacing_option,
+                            "bc"=>"none"),
+    )
+    #println("made inputs")
+    #println("vpa: ngrid: ",ngrid," nelement: ",nelement_local_vpa, " Lvpa: ",Lvpa)
+    #println("vperp: ngrid: ",ngrid," nelement: ",nelement_local_vperp, " Lvperp: ",Lvperp)
+    # create the coordinate structs
+    z, z_spectral = define_coordinate(coords_input, "z")
+    vperp, vperp_spectral = define_coordinate(coords_input, "vperp")
+    vpa, vpa_spectral = define_coordinate(coords_input, "vpa")
+    looping.setup_loop_ranges!(block_rank[], block_size[];
+                                    s=1, sn=1,
+                                    r=1, z=z.n, vperp=vperp.n, vpa=vpa.n,
+                                    vzeta=1, vr=1, vz=1)
+    begin_serial_region()
+    start_init_time = now()
+    if boundary_data_option == direct_integration
+        precompute_weights = true
+    else
+        precompute_weights = false
+    end
+    fkpl_arrays = init_fokker_planck_collisions_weak_form(vpa,vperp,vpa_spectral,vperp_spectral; 
+                        precompute_weights=precompute_weights, test_dense_matrix_construction=test_dense_construction)
+    streaming_arrays = init_z_advection_implicit(z,z_spectral,vperp,vpa,delta_t)
+    finish_init_time = now()
+    
+    # initial condition
+    fvpavperpz = allocate_shared_float(vpa.n,vperp.n,z.n,ntime+1)
+    Hminus = allocate_float(vpa.n)
+    Hplus = allocate_float(vpa.n)
+    zerovpa = 1.e-10
+    Hminus .= 0.0
+    Hplus .= 0.0
+    @loop_vpa ivpa begin
+        if vpa.grid[ivpa] < -zerovpa  
+            Hminus[ivpa] = 1.0
+        end
+        if vpa.grid[ivpa] > zerovpa
+            Hplus[ivpa] = 1.0
+        end
+    end
+    @serial_region begin
+        @loop_z_vperp_vpa iz ivperp ivpa begin
+            zfac = ((0.5 - z.grid[iz]/z.L)*(vpa.grid[ivpa]^2)*Hminus[ivpa] +
+                        (0.5 + z.grid[iz]/z.L)*(vpa.grid[ivpa]^2)*Hplus[ivpa])
+            fvpavperpz[ivpa,ivperp,iz,1] = zfac*exp(-((vpa.grid[ivpa])^2 + (vperp.grid[ivperp])^2)/(vth0^2))/(vth0^3)
+        end
+    end
+    # arrays needed for advance
+    dummy_vpavperp = Array{mk_float,2}(undef,vpa.n,vperp.n)
+    Fold = allocate_shared_float(vpa.n,vperp.n,z.n)
+    Fnew = allocate_shared_float(vpa.n,vperp.n,z.n)
+    density = allocate_shared_float(z.n)
+    phi = allocate_shared_float(z.n)
+    Ez = allocate_shared_float(z.n)
+    FSource = allocate_shared_float(vpa.n,vperp.n,z.n)
+
+    # dummy arrays for collision newton_solve!()
+    Fdummy1 = allocate_shared_float(vpa.n,vperp.n)
+    Fdummy2 = allocate_shared_float(vpa.n,vperp.n)
+    Fdummy3 = allocate_shared_float(vpa.n,vperp.n)
+    Fdummy4 = allocate_shared_float(vpa.n,vperp.n)
+    Fdummy5 = allocate_shared_float(vpa.n,vperp.n)
+    # zero dummy arrays
+    @serial_region begin
+        @loop_vperp_vpa ivperp ivpa begin
+        Fdummy1[ivpa,ivperp] = 0.0
+        Fdummy2[ivpa,ivperp] = 0.0
+        Fdummy3[ivpa,ivperp] = 0.0
+        Fdummy4[ivpa,ivperp] = 0.0
+        Fdummy5[ivpa,ivperp] = 0.0
+        end
+    end
+    # physics parameters
+    ms = 1.0
+    msp = 1.0
+    nussp = 1.0
+    Te = 1.0
+    Ne = 1.0
+
+    # initial condition 
+    time = 0.0
+    @serial_region begin
+        @loop_z_vperp_vpa iz ivperp ivpa begin
+            Fold[ivpa,ivperp,iz] = fvpavperpz[ivpa,ivperp,iz,1]
+            Fnew[ivpa,ivperp,iz] = Fold[ivpa,ivperp,iz]
+            FSource[ivpa,ivperp,iz] = nu_source * exp(-((vpa.grid[ivpa]-vpa0)^2 + (vperp.grid[ivperp]-vperp0)^2)/(vth0^2))/(vth0^3)
+        end
+    end
+    #diagnose_F_Maxwellian(Fold,Fdummy1,Fdummy2,Fdummy3,vpa,vperp,time,ms,0)
+    
+    # coords and params for newton_solve!() for collisions only
+    coords = (vperp=vperp,vpa=vpa)
+    nl_solver_params = setup_nonlinear_solve(
+        true,
+        OptionsDict("nonlinear_solver" =>
+                    OptionsDict("rtol" => 0.0,
+                                "atol" => atol,
+                                "linear_restart" => restart,
+                                "linear_max_restarts" => max_restarts,
+                                "nonlinear_max_iterations" => 100)),
+        coords; serial_solve=serial_solve, anyv_region=anyv_region,
+        preconditioner_type=Val(:lu))
+
+    for it in 1:ntime
+        # use operator splitting
+        # advance due to source and parallel streaming
+        begin_z_vperp_vpa_region()
+        @loop_z_vperp_vpa iz ivperp ivpa begin
+            Fold[ivpa,ivperp,iz] += delta_t * FSource[ivpa,ivperp,iz]
+        end
+        z_advection_implicit_advance!(Fold,z,vpa,vperp,streaming_arrays)
+        # compute fields
+        begin_z_region()
+        @loop_z iz begin
+            @views density[iz] = get_density(Fold[:,:,iz],vpa,vperp)
+            phi[iz] = Te * log(density[iz]/Ne)
+        end
+        begin_serial_region()
+        @serial_region begin
+            derivative!(Ez, -phi, z, z_spectral)
+        end
+        # advance collisions and velocity advection
+        if true
+            begin_s_r_z_anyv_region()
+            @loop_z iz begin
+                @views fokker_planck_backward_euler_step!(Fnew[:,:,iz], Fold[:,:,iz], delta_t, ms, msp, nussp, fkpl_arrays, dummy_vpavperp,
+                    vperp, vpa, vperp_spectral, vpa_spectral, coords,
+                    Fdummy1, Fdummy2, Fdummy3, Fdummy4, Fdummy5, nl_solver_params,
+                    test_numerical_conserving_terms=test_numerical_conserving_terms,
+                    test_particle_preconditioner=test_particle_preconditioner,
+                    test_linearised_advance=test_linearised_advance,
+                    use_Maxwellian_Rosenbluth_coefficients_in_preconditioner=use_Maxwellian_Rosenbluth_coefficients_in_preconditioner,
+                    test_self_operator=test_self_operator,
+                    test_assembly_serial=test_parallelism,
+                    use_Maxwellian_Rosenbluth_coefficients=use_Maxwellian_Rosenbluth_coefficients,
+                    use_Maxwellian_field_particle_distribution=use_Maxwellian_field_particle_distribution,
+                    algebraic_solve_for_d2Gdvperp2=algebraic_solve_for_d2Gdvperp2,
+                    calculate_GG = false, calculate_dGdvperp=false,
+                    boundary_data_option=boundary_data_option,
+                    standalone=false)
+            end
+            begin_serial_region()
+            # update the pdf
+            @serial_region begin
+                @loop_z_vperp_vpa iz ivperp ivpa begin
+                    Fold[ivpa,ivperp,iz] = Fnew[ivpa,ivperp,iz]
+                end
+            end
+        end
+        # diagnose Fold
+        time += delta_t
+        println("it ",it," time ",time)
+        #diagnose_F_Maxwellian(Fold,Fdummy1,Fdummy2,Fdummy3,vpa,vperp,time,ms,it)
+        # update outputs
+        @serial_region begin
+            @loop_z_vperp_vpa iz ivperp ivpa begin
+                fvpavperpz[ivpa,ivperp,iz,it+1] = Fold[ivpa,ivperp,iz] 
+            end
+        end
+    end
+    
+    #diagnose_F_Maxwellian(Fold,Fdummy1,Fdummy2,Fdummy3,vpa,vperp,ntime,ms)
+    if plot_test_output
+        diagnose_F_gif(fvpavperpz,vpa,vperp,z,ntime)
+    end    
+end
     
 if abspath(PROGRAM_FILE) == @__FILE__
     using Pkg
