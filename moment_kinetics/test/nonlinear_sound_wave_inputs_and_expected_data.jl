dd11e0bc36db6ae4868c2fc7040a72a99f203bdd
John Omotani <john.omotani@ukaea.uk>
2025-04-07 22:01:20 +0100

    Fix nonlinear_sound_wave_inputs_and_expected_data.jl
    [skip ci]

--- moment_kinetics/test/nonlinear_sound_wave_inputs_and_expected_data.jl
+++ moment_kinetics/test/nonlinear_sound_wave_inputs_and_expected_data.jl
@@ -135,8 +135,8 @@ test_input_finite_difference_split_3_moments =
     recursive_merge(test_input_finite_difference_split_2_moments,
                     OptionsDict("output" => OptionsDict("run_name" => "finite_difference_split_3_moments"),
                                 "evolve_moments" => OptionsDict("pressure" => true),
-                                "vpa" => OptionsDict("L" => 16.970562748477143),
-                                "vz" => OptionsDict("L" => 16.970562748477143),
+                                "vpa" => OptionsDict("L" => 20.784609690826528),
+                                "vz" => OptionsDict("L" => 20.784609690826528),
                                ))
 
 test_input_chebyshev = recursive_merge(test_input_finite_difference,
@@ -171,6 +171,6 @@ test_input_chebyshev_split_3_moments =
     recursive_merge(test_input_chebyshev_split_2_moments,
                     OptionsDict("output" => OptionsDict("run_name" => "chebyshev_pseudospectral_split_3_moments"),
                                 "evolve_moments" => OptionsDict("pressure" => true),
-                                "vpa" => OptionsDict("L" => 16.970562748477143),
-                                "vz" => OptionsDict("L" => 16.970562748477143),
+                                "vpa" => OptionsDict("L" => 20.784609690826528),
+                                "vz" => OptionsDict("L" => 20.784609690826528),
                                ))
