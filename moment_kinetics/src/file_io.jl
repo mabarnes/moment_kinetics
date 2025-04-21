5c1897764e52f151d1b9d6f125ceb817c12990fb
John Omotani <john.omotani@ukaea.uk>
2025-04-21 17:20:35 +0100

    Fix I/O for electron shape function when using `explicit_time_evolving`
    Need to add `explicit_time_evolving` to the special case, along with
    `implicit_time_evolving`, to write `f_electron` from `scratch` instead
    of `scratch_electron`.

--- moment_kinetics/src/file_io.jl
+++ moment_kinetics/src/file_io.jl
@@ -3323,7 +3323,7 @@ binary output file
         # add the distribution function data at this time slice to the output file
         write_ion_dfns_data_to_binary(scratch, t_params, n_ion_species, io_dfns, t_idx, r,
                                       z, vperp, vpa)
-        if t_params.kinetic_electron_solver == implicit_time_evolving || scratch_electron !== nothing
+        if t_params.kinetic_electron_solver ∈ (implicit_time_evolving, explicit_time_evolving) || scratch_electron !== nothing
             write_electron_dfns_data_to_binary(scratch, scratch_electron, t_params,
                                                io_dfns, t_idx, r, z, vperp, vpa)
         end
@@ -3406,7 +3406,7 @@ function write_electron_dfns_data_to_binary(scratch, scratch_electron, t_params,
         parallel_io = io_dfns.io_input.parallel_io
 
         if io_dfns.f_electron !== nothing
-            if t_params.kinetic_electron_solver == implicit_time_evolving || scratch_electron === nothing
+            if t_params.kinetic_electron_solver ∈ (implicit_time_evolving, explicit_time_evolving) || scratch_electron === nothing
                 n_rk_stages = t_params.n_rk_stages
                 this_scratch = scratch
             elseif t_params.electron === nothing
