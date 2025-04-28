364587d651a14e294a416fd07d9ed8b5cdbbda38
Michael Hardman <29800382+mrhardman@users.noreply.github.com>
2025-04-28 14:32:50 +0100

    Use preconditioner for FP implicit collisions by default.

--- moment_kinetics/src/fokker_planck.jl
+++ moment_kinetics/src/fokker_planck.jl
@@ -921,7 +921,7 @@ function implicit_ion_fokker_planck_self_collisions!(pdf_out, pdf_in, dSdt,
                     vpa, vperp, z, r, delta_t, spectral_objects,
                     nl_solver_params; diagnose_entropy_production=false,
                     test_linearised_advance=false,
-                    test_particle_preconditioner=false,
+                    test_particle_preconditioner=true,
                     use_Maxwellian_Rosenbluth_coefficients_in_preconditioner=false,
                     use_end_of_step_corrections=true)
     # bounds checking
@@ -1006,7 +1006,7 @@ function fokker_planck_self_collisions_backward_euler_step!(Fold, delta_t, ms, n
                         nl_solver_params;
                         test_numerical_conserving_terms=false,
                         test_linearised_advance=false,
-                        test_particle_preconditioner=false,
+                        test_particle_preconditioner=true,
                         use_Maxwellian_Rosenbluth_coefficients_in_preconditioner=false,
                         boundary_data_option=multipole_expansion)
 
