cb109d6f1dd55d19596ffbf9a3fdd2a85839af57
mrhardman <29800382+mrhardman@users.noreply.github.com>
2025-05-06 10:27:08 +0100

    Update moment_kinetics/test/fokker_planck_tests.jl
    Remove commented code.
    
    Co-authored-by: John Omotani <john.omotani@ukaea.uk>

--- moment_kinetics/test/fokker_planck_tests.jl
+++ moment_kinetics/test/fokker_planck_tests.jl
@@ -366,7 +366,6 @@ function runtests()
                 # i.e., we check the numerical Maxwellian is close to the analytical one.
                 # This is faster and more stable than doing a relaxation from vperp0 /= 0.
                 backward_Euler_fokker_planck_self_collisions_test(bc_vperp=bc, bc_vpa=bc,
-                   #ngrid = 5, nelement_vpa = 16, nelement_vperp = 8, Lvpa = 10.0, Lvperp = 5.0,
                    ntime = 10, delta_t = 0.1,
                    vth0 = 1.0, vpa0 = 1.0, vperp0 = 0.0, 
                    print_to_screen=print_to_screen)
