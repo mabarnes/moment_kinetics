a41c65d7961be5d708a526fbe4a3967aa1c6f9a6
mrhardman <29800382+mrhardman@users.noreply.github.com>
2025-05-06 10:18:21 +0100

    Update moment_kinetics/src/nonlinear_solvers.jl
    Reinsert timer.
    
    Co-authored-by: John Omotani <john.omotani@ukaea.uk>

--- moment_kinetics/src/nonlinear_solvers.jl
+++ moment_kinetics/src/nonlinear_solvers.jl
@@ -1095,10 +1095,9 @@ function parallel_map(
     @_anyv_subblock_synchronize()
     return nothing
 end
-#@timeit_debug global_timer parallel_map(
-function parallel_map(
+@timeit_debug global_timer parallel_map(
                   ::Val{:vperpvpa}, func, result::AbstractArray{mk_float, 2},
-                  x1, x2)# = begin
+                  x1, x2) = begin
 
     result_pdf = result
     x1_pdf = x1
