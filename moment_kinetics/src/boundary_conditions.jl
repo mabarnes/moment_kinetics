37b7891f79477386a4d5937ad403c0e22993e518
Lucas McConnell <sjoh5887@ox.ac.uk>
2025-06-04 13:28:25 +0100

    printouts for more debugging

--- moment_kinetics/src/boundary_conditions.jl
+++ moment_kinetics/src/boundary_conditions.jl
@@ -619,7 +619,7 @@ function enforce_zero_incoming_bc!(pdf, z::coordinate, vperp::coordinate, vpa::c
                     (I0*(J3^2 - J2*J4) + I1*(J1*J4 - J2*J3) + I2*(J2^2 - J1*J3))
                 B = (1.5*J3 + A*(I1*J4 - I2*J3)) / (J3^2 - J2*J4)
                 C = (1.5 - A*I2 - B*J3) / J4
-
+                println("in boundary_conditions.jl: A = $A, B = $B, C = $C")
                 @. f = A*f + B*vpa.grid*vpa.scratch2 + C*vpa.grid*vpa.grid*vpa.scratch2
             elseif evolve_upar
                 I0 = integral(f, vpa.wgts)
@@ -695,6 +695,7 @@ function enforce_zero_incoming_bc!(pdf, z::coordinate, vperp::coordinate, vpa::c
                 J2 = integral((vperp,vpa)->(vpa^2*abs(vpa+upar[iz]/vth)/(one_over_scale_factor+abs(vpa+upar[iz]/vth))/(1.0+(4.0*(vpa+upar[iz]/vth)/vpa_L)^4)), f, vperp, vpa)
                 J3 = integral((vperp,vpa)->(vpa^3*abs(vpa+upar[iz]/vth)/(one_over_scale_factor+abs(vpa+upar[iz]/vth))/(1.0+(4.0*(vpa+upar[iz]/vth)/vpa_L)^4)), f, vperp, vpa)
                 J4 = integral((vperp,vpa)->(vpa^4*abs(vpa+upar[iz]/vth)/(one_over_scale_factor+abs(vpa+upar[iz]/vth))/(1.0+(4.0*(vpa+upar[iz]/vth)/vpa_L)^4)), f, vperp, vpa)
+                println("J1 = $J1, J2 = $J2, J3 = $J3, J4 = $J4")
                 # Given a corrected distribution function
                 #   F = A * Fhat + (B*wpa + C*wpa*2) * s*vpa/vth / (1 + s*|vpa/vth|) / (1 +(4*vpa/vth/Lvpa)^4) * Fhat
                 # the constraints (assuming 1D1V)
@@ -728,9 +729,15 @@ function enforce_zero_incoming_bc!(pdf, z::coordinate, vperp::coordinate, vpa::c
                     (I0*(J3^2 - J2*J4) + I1*(J1*J4 - J2*J3) + I2*(J2^2 - J1*J3))
                 B = (1.5*J3 + A*(I1*J4 - I2*J3)) / (J3^2 - J2*J4)
                 C = (1.5 - A*I2 - B*J3) / J4
+
+                
                 #println("I0 = ", I0, " I1 = ", I1, " I2 = ", I2)
                 #println("J1 = ", J1, " J2 = ", J2, " J3 = ", J3, " J4 = ", J4)
-
+                println("in boundary_conditions.jl: A = $A, B = $B, C = $C")
+                println("vperp.wgts = ")
+                display(vperp.wgts[3])
+                println("vpa.wgts = ")
+                display(vpa.wgts[3])
                 @. f = A*f + B*vpa.grid*vpa.scratch2*f + C*vpa.grid*vpa.grid*vpa.scratch2*f
             elseif evolve_upar
                 I0 = integral((vperp,vpa)->(1), f, vperp, vpa)
@@ -750,7 +757,7 @@ function enforce_zero_incoming_bc!(pdf, z::coordinate, vperp::coordinate, vpa::c
 
                 J1 = integral((vperp,vpa)->(vpa*abs((vpa+upar[iz])/vth)/(sqrt(2.0)+abs((vpa+upar[iz])/vth))/(1.0+(4.0*((vpa+upar[iz])/vth)/vpa_L)^4)), f, vperp, vpa)
                 J2 = integral((vperp,vpa)->(vpa^2*abs((vpa+upar[iz])/vth)/(sqrt(2.0)+abs((vpa+upar[iz])/vth))/(1.0+(4.0*((vpa+upar[iz])/vth)/vpa_L)^4)), f, vperp, vpa)
-
+                println("J1 = $J1, J2 = $J2")
 
                 # Given a corrected distribution function
                 #   F = A * Fhat + B*wpa * s*vpa/vth / (1 + s*|vpa/vth|) / (1 +(4*vpa/vth/Lvpa)^4) * Fhat
@@ -773,8 +780,12 @@ function enforce_zero_incoming_bc!(pdf, z::coordinate, vperp::coordinate, vpa::c
                 A = 1.0 / (I0 - I1*J1/J2)
                 B = -A*I1/J2
                 @. f = A*f + B*vpa.grid*vpa.scratch2*f
-
-
+                println("J1 = $J1, J2 = $J2")
+                println("in boundary_conditions.jl: A = $A, B = $B")
+                println("vperp.wgts = ")
+                display(vperp.wgts[3])
+                println("vpa.wgts = ")
+                display(vpa.wgts[3])
             elseif evolve_density
                 I0 = integral((vperp,vpa)->(1), f, vperp, vpa)
                 @. f = f / I0
