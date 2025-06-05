1d199775c143e59a469824a5d9f70fe0c154ece5
John Omotani <john.omotani@ukaea.uk>
2025-06-05 15:54:00 +0100

    Add more debug timings of slightly lower level functions

--- moment_kinetics/src/moment_constraints.jl
+++ moment_kinetics/src/moment_constraints.jl
@@ -27,7 +27,9 @@ initial state, and when applying boundary conditions.
 
 Note this function assumes the input is given at a single spatial position.
 """
-function hard_force_moment_constraints!(f, moments, vpa, vperp)
+@timeit global_timer hard_force_moment_constraints!(f, moments, vpa, vperp) = begin
+
+    f1d = @view f[:,1]
     if moments.evolve_p
         # fnew = (A + B*wpa + C*(wpa^2 + wperp^2))*f
         # Constraints:
@@ -149,7 +151,7 @@ Notes:
 * this function assumes the input is given at a single spatial position.
 * currently only works with '1V' runs, where vz is the only velocity-space dimension
 """
-function hard_force_moment_constraints_neutral!(f, moments, vz)
+@timeit global_timer hard_force_moment_constraints_neutral!(f, moments, vz) = begin
     f1d = @view f[:,1,1]
     if moments.evolve_p
         # fnew = (A + B*wz + C*wz^2)*f
