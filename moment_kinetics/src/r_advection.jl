2986e5a4f8680d1ab3c1f18b10dd2c3585d6cd52
John Omotani <john.omotani@ukaea.uk>
2025-06-05 10:15:00 +0100

    Update and optimise r_advection
    Remove functions that would 'unnormalise' the shape function for
    moment_kinetic runs (which are not yet supported in 2D anyway), in line
    with the update in #330 that removed use of the 'unnormalised'
    distribution function in z-advection.
    
    Tidy up a bit, e.g. remove an empty loop that probably resulted from a
    merge mistake.
    
    Optimise: remove the unnecessary copy into `r.scratch`, which was
    previously actually allocating and copying into a temporary array before
    copying into `r.scratch`. Instead, just access the view of the buffer
    array directly. Also use `r.scratch` for `geofac` and update only in the
    z-loop, not inside vperp and vpa loops.

--- moment_kinetics/src/r_advection.jl
+++ moment_kinetics/src/r_advection.jl
@@ -25,61 +25,24 @@ do a single stage time advance (potentially as part of a multi-stage RK scheme)
         @views update_speed_r!(advect[is], fvec_in.upar[:,:,is],
                                moments.ion.vth[:,:,is], fields, moments.evolve_upar,
                                moments.evolve_p, vpa, vperp, z, r, geometry, is)
-        # advance r-advection equation
-        @loop_z_vpa iz ivpa begin
-        end
         # update adv_fac
         @loop_z_vperp_vpa iz ivperp ivpa begin
-            @views adjust_advection_speed!(advect[is].speed[:,ivpa,ivperp,iz],
-                                           fvec_in.density[iz,:,is],
-                                           moments.ion.vth[iz,:,is],
-                                           moments.evolve_density, moments.evolve_p)
-            # take the normalized pdf contained in fvec_in.pdf and remove the normalization,
-            # returning the true (un-normalized) particle distribution function in r.scratch
-            @views unnormalize_pdf!(
-                       scratch_dummy.buffer_vpavperpzrs_2[ivpa,ivperp,iz,:,is],
-                       fvec_in.pdf[ivpa,ivperp,iz,:,is], fvec_in.density[iz,:,is],
-                       moments.ion.vth[iz,:,is], moments.evolve_density,
-                       moments.evolve_p)
-            advect[is].adv_fac[:,ivpa,ivperp,iz] .= -dt.*advect[is].speed[:,ivpa,ivperp,iz]
+            @views @. advect[is].adv_fac[:,ivpa,ivperp,iz] = -dt * advect[is].speed[:,ivpa,ivperp,iz]
         end
     end
     # calculate the upwind derivative along r
-    derivative_r!(scratch_dummy.buffer_vpavperpzrs_1, scratch_dummy.buffer_vpavperpzrs_2,
-        advect, scratch_dummy.buffer_vpavperpzs_1, scratch_dummy.buffer_vpavperpzs_2,
-        scratch_dummy.buffer_vpavperpzs_3,scratch_dummy.buffer_vpavperpzs_4,
-        scratch_dummy.buffer_vpavperpzs_5,scratch_dummy.buffer_vpavperpzs_6, r_spectral,r)
+    derivative_r!(scratch_dummy.buffer_vpavperpzrs_1, fvec_in.pdf, advect,
+                  scratch_dummy.buffer_vpavperpzs_1, scratch_dummy.buffer_vpavperpzs_2,
+                  scratch_dummy.buffer_vpavperpzs_3,scratch_dummy.buffer_vpavperpzs_4,
+                  scratch_dummy.buffer_vpavperpzs_5,scratch_dummy.buffer_vpavperpzs_6,
+                  r_spectral,r)
 
     # advance r-advection equation
     @loop_s_z_vperp_vpa is iz ivperp ivpa begin
-        @. r.scratch = scratch_dummy.buffer_vpavperpzrs_1[ivpa,ivperp,iz,:,is]
         @views advance_f_df_precomputed!(f_out[ivpa,ivperp,iz,:,is],
-          r.scratch, advect[is], ivpa, ivperp, iz, r, dt)
+          scratch_dummy.buffer_vpavperpzrs_1[ivpa,ivperp,iz,:,is], advect[is], ivpa,
+          ivperp, iz, r, dt)
     end
-end
-
-"""
-"""
-function adjust_advection_speed!(speed, dens, vth, evolve_density, evolve_p)
-    if evolve_p
-        @. speed *= vth/dens
-    elseif evolve_density
-        @. speed /= dens
-    end
-    return nothing
-end
-
-"""
-"""
-function unnormalize_pdf!(unnorm, norm, dens, vth, evolve_density, evolve_p)
-    if evolve_p
-        @. unnorm = norm * dens/vth
-    elseif evolve_density
-        @. unnorm = norm * dens
-    else
-        @. unnorm = norm
-    end
-    return nothing
 end
 
 """
@@ -102,15 +65,18 @@ function update_speed_r!(advect, upar, vth, fields, evolve_upar, evolve_p, vpa, 
         geofac = r.scratch
         cvdriftr = geometry.cvdriftr
         gbdriftr = geometry.gbdriftr
+        geofac = r.scratch
         @inbounds begin
-            @loop_z_vperp_vpa iz ivperp ivpa begin
-                # ExB drift
-                @. geofac = bzeta[iz,:]*jacobian[iz,:]/Bmag[iz,:]
-                @views @. advect.speed[:,ivpa,ivperp,iz] = rhostar*geofac*fields.gEz[ivperp,iz,:,is]
-                # magnetic curvature drift
-                @. @views advect.speed[:,ivpa,ivperp,iz] += rhostar*(vpa.grid[ivpa]^2)*cvdriftr[iz,:]
-                # magnetic grad B drift
-                @. @views advect.speed[:,ivpa,ivperp,iz] += 0.5*rhostar*(vperp.grid[ivperp]^2)*gbdriftr[iz,:]
+            @loop_z iz begin
+                @views @. geofac = bzeta[iz,:]*jacobian[iz,:]/Bmag[iz,:]
+                @loop_vperp_vpa ivperp ivpa begin
+                    # ExB drift
+                    @views @. advect.speed[:,ivpa,ivperp,iz] = rhostar*geofac*fields.gEz[ivperp,iz,:,is]
+                    # magnetic curvature drift
+                    @. @views advect.speed[:,ivpa,ivperp,iz] += rhostar*(vpa.grid[ivpa]^2)*cvdriftr[iz,:]
+                    # magnetic grad B drift
+                    @. @views advect.speed[:,ivpa,ivperp,iz] += 0.5*rhostar*(vperp.grid[ivperp]^2)*gbdriftr[iz,:]
+                end
             end
         end
     elseif r.advection.option == "default" && r.n == 1
