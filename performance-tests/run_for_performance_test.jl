0788f468f40316be09ce261d8f3d6fa96ab9a773
John Omotani <john.omotani@ukaea.uk>
2025-06-06 16:19:16 +0100

    Script to run a 'performance test' run
    ...where first a single-timestep run is done to make sure everything is
    compiled, then the full-length run is done.
    
    So far there is no feature in the `machines` scripts to use this script
    for runs, so it is necessary to edit the submission scripts manually.
    Could add a flag to `submit-run.sh` and `submit-restart.sh` in the
    future to make this more convenient...

---
+++ performance-tests/run_for_performance_test.jl
@@ -0,0 +1,38 @@
+# Similar to run_moment_kinetics.jl script, but first runs a single timestep to ensure all
+# compilation is complete, then starts a new run.
+using Pkg
+Pkg.activate(".")
+
+using MPI
+MPI.Init()
+is_rank0 = (MPI.Comm_rank(MPI.COMM_WORLD) == 0)
+
+# Use MPI Barriers to ensure `using moment_kinetics` is completed on rank-0 (in order to
+# complete precompilation of moment_kinetics and dependencies) before being run on all the
+# other MPI ranks.
+if is_rank0
+    using moment_kinetics
+    MPI.Barrier(MPI.COMM_WORLD)
+else
+    MPI.Barrier(MPI.COMM_WORLD)
+    using moment_kinetics
+end
+
+options = moment_kinetics.get_options()
+inputfile = options["inputfile"]
+restart = options["restart"]
+if options["restartfile"] !== nothing
+    restart = options["restartfile"]
+end
+restart_time_index = options["restart-time-index"]
+
+input = moment_kinetics.read_input_file(inputfile)
+
+compilation_run_input = deepcopy(input)
+compilation_run_input["timestepping"]["nstep"] = 1
+
+# Run a single timstep.
+run_moment_kinetics(compilation_run_input; restart, restart_time_index)
+
+# Overwrite any output from the single-timestep run with a full run.
+run_moment_kinetics(input; restart, restart_time_index)
