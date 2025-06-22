ed10b4dfdb5ebf3868e07e8ff0a132dedf65b328
John Omotani <john.omotani@ukaea.uk>
2025-06-22 15:22:59 +0100

    remove extra (working) tests for debug

--- moment_kinetics/debug_test/runtests.jl
+++ moment_kinetics/debug_test/runtests.jl
@@ -4,13 +4,13 @@ include("setup.jl")
 
 function runtests()
     @testset "moment_kinetics tests" begin
-        include(joinpath(@__DIR__, "kinetic_electron_tests.jl"))
-        include(joinpath(@__DIR__, "sound_wave_tests.jl"))
-        include(joinpath(@__DIR__, "fokker_planck_collisions_tests.jl"))
-        include(joinpath(@__DIR__, "wall_bc_tests.jl"))
-        include(joinpath(@__DIR__, "restart_interpolation_tests.jl"))
-        include(joinpath(@__DIR__, "recycling_fraction_tests.jl"))
-        include(joinpath(@__DIR__, "gyroaverage_tests.jl"))
+#        include(joinpath(@__DIR__, "kinetic_electron_tests.jl"))
+#        include(joinpath(@__DIR__, "sound_wave_tests.jl"))
+#        include(joinpath(@__DIR__, "fokker_planck_collisions_tests.jl"))
+#        include(joinpath(@__DIR__, "wall_bc_tests.jl"))
+#        include(joinpath(@__DIR__, "restart_interpolation_tests.jl"))
+#        include(joinpath(@__DIR__, "recycling_fraction_tests.jl"))
+#        include(joinpath(@__DIR__, "gyroaverage_tests.jl"))
 
         manufactured_solutions_ext = Base.get_extension(moment_kinetics, :manufactured_solns_ext)
         if manufactured_solutions_ext !== nothing
