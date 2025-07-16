5bc7a256fad8dab3dd83d4decca4cf571d95c252
John Omotani <john.omotani@ukaea.uk>
2025-07-16 19:04:37 +0100

    Add option for 'sinusoid' source profiles
    Useful to have a source that has zero radial gradient at the boundaries,
    to be compatible with a Neumann boundary condition.

--- moment_kinetics/src/external_sources.jl
+++ moment_kinetics/src/external_sources.jl
@@ -569,6 +569,10 @@ function get_source_profile(profile_type, width, relative_minimum, coord)
     elseif profile_type == "super_gaussian_4"
         x = coord.grid
         return @. (1.0 - relative_minimum) * exp(-(x / width)^4) + relative_minimum
+    elseif profile_type == "sinusoid"
+        # Set so that profile can be 1 on the inner/lower boundary
+        x = coord.grid
+        return @. (1.0 - relative_minimum) * 0.5 * (1.0 - sinpi(x / width)) + relative_minimum
     else
         error("Unrecognised source profile type '$profile_type'.")
     end
