182d97f8c25b65ec13d01fb0358ad576886e1b55
John Omotani <john.omotani@ukaea.uk>
2025-08-18 10:18:15 +0100

    more fix tols

--- moment_kinetics/test/fokker_planck_time_evolution_tests.jl
+++ moment_kinetics/test/fokker_planck_time_evolution_tests.jl
@@ -479,9 +479,9 @@ function runtests(; highres=false)
 
         @testset "evolve_density=$evolve_density, evolve_upar=$evolve_upar, evolve_p=$evolve_p" for
                 (evolve_density, evolve_upar, evolve_p, tol1, tol2, tol3, tol4) ∈
-                    ((false, false, false, 2.0e-14, 1.0e-14, highres ? 4.0e-3 : 1.0e-5, highres ? 1.0e-4 : 5.0e-12),
+                    ((false, false, false, 2.0e-14, 2.0e-14, highres ? 4.0e-3 : 1.0e-5, highres ? 1.0e-4 : 5.0e-12),
                      (true, false, false, highres ? 2.0e-8 : 2.0e-14, highres ? 2.0e-8 : 1.0e-14, highres ? 4.0e-3 : 1.0e-5, highres ? 1.0e-4 : 5.0e-12),
-                     (true, true, false, highres ? 2.0e-3 : 2.0e-14, highres ? 3.0e-4 : 1.0e-14, highres ? 4.0e-3 : 1.0e-5, highres ? 3.0e-4 : 5.0e-12),
+                     (true, true, false, highres ? 2.0e-3 : 2.0e-14, highres ? 3.0e-4 : 2.0e-14, highres ? 4.0e-3 : 1.0e-5, highres ? 3.0e-4 : 5.0e-12),
                      (true, true, true, highres ? 2.0e-3 : 2.0e-14, highres ? 2.0e-3 : 1.0e-14, highres ? 4.0e-3 : 1.0e-5, highres ? 2.0e-3 : 5.0e-12),
                     )
             println("  evolve_density=$evolve_density, evolve_upar=$evolve_upar, evolve_p=$evolve_p:")
