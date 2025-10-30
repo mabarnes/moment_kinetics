a64a403dfed3a27c0bfa3d8b24af9650928e631a
John Omotani <john.omotani@ukaea.uk>
2025-10-30 16:22:46 +0000

    Don't evaluate moment ddt in postproc if corresponding evolve_* is false
    When a moment was not evolved in the simulation, calling the
    corresponding moment time derivative function in post processing may
    error, so instead throw a `KeyError` to indicate that that variable is
    'missing'.

--- moment_kinetics/src/load_data.jl
+++ moment_kinetics/src/load_data.jl
@@ -4785,6 +4785,9 @@ function _get_variable_internal(run_info, variable_name::Symbol;
     elseif variable_name == :neutral_dqz_dz
         variable = get_z_derivative(run_info, "qz_neutral"; kwargs...)
     elseif variable_name == :ddens_dt
+        if !run_info.evolve_density
+            throw(KeyError("evolve_density=false, so do not calculate ddens_dt"))
+        end
         all_moments = _get_all_moment_variables(run_info)
         variable = similar(all_moments.density)
         # Define function here to minimise effect type instability due to
@@ -4808,6 +4811,9 @@ function _get_variable_internal(run_info, variable_name::Symbol;
         get_ddens_dt!(variable, all_moments)
         variable = select_slice_of_variable(variable; kwargs...)
     elseif variable_name == :dnupar_dt
+        if !run_info.evolve_upar
+            throw(KeyError("evolve_upar=false, so do not calculate dnupar_dt"))
+        end
         all_moments = _get_all_moment_variables(run_info)
         variable = similar(all_moments.parallel_flow)
         # Define function here to minimise effect type instability due to
@@ -4831,12 +4837,18 @@ function _get_variable_internal(run_info, variable_name::Symbol;
         get_dnupar_dt!(variable, all_moments)
         variable = select_slice_of_variable(variable; kwargs...)
     elseif variable_name == :dupar_dt
+        if !run_info.evolve_upar
+            throw(KeyError("evolve_upar=false, so do not calculate dupar_dt"))
+        end
         dn_dt = get_variable(run_info, "ddens_dt"; kwargs...)
         dnupar_dt = get_variable(run_info, "dnupar_dt"; kwargs...)
         n = get_variable(run_info, "density"; kwargs...)
         upar = get_variable(run_info, "parallel_flow"; kwargs...)
         variable = @. dnupar_dt / n - upar / n * dn_dt
     elseif variable_name == :dp_dt
+        if !run_info.evolve_p
+            throw(KeyError("evolve_p=false, so do not calculate dp_dt"))
+        end
         all_moments = _get_all_moment_variables(run_info)
         variable = similar(all_moments.pressure)
         # Define function here to minimise effect type instability due to
@@ -4859,6 +4871,9 @@ function _get_variable_internal(run_info, variable_name::Symbol;
         get_dp_dt!(variable, all_moments)
         variable = select_slice_of_variable(variable; kwargs...)
     elseif variable_name == :dvth_dt
+        if !run_info.evolve_p
+            throw(KeyError("evolve_p=false, so do not calculate dvth_dt"))
+        end
         dn_dt = get_variable(run_info, "ddens_dt"; kwargs...)
         dp_dt = get_variable(run_info, "dp_dt"; kwargs...)
         n = get_variable(run_info, "density"; kwargs...)
@@ -4866,6 +4881,9 @@ function _get_variable_internal(run_info, variable_name::Symbol;
         vth = get_variable(run_info, "thermal_speed"; kwargs...)
         variable = @. 0.5 * vth * (dp_dt / p - dn_dt / n)
     elseif variable_name == :electron_dp_dt
+        if !run_info.evolve_p
+            throw(KeyError("evolve_p=false, so do not calculate electron_dp_dt"))
+        end
         # Try to load electron pressure to check that electrons are present in the output.
         _ = get_variable(run_info, "electron_pressure"; kwargs...)
 
@@ -4898,6 +4916,9 @@ function _get_variable_internal(run_info, variable_name::Symbol;
         get_electron_dp_dt!(variable, all_moments)
         variable = select_slice_of_variable(variable; kwargs...)
     elseif variable_name == :electron_dvth_dt
+        if !run_info.evolve_p
+            throw(KeyError("evolve_p=false, so do not calculate electron_dvth_dt"))
+        end
         # Try to load electron pressure to check that electrons are present in the output.
         _ = get_variable(run_info, "electron_pressure"; kwargs...)
 
@@ -4914,6 +4935,9 @@ function _get_variable_internal(run_info, variable_name::Symbol;
         vth = get_variable(run_info, "electron_thermal_speed"; kwargs...)
         variable = @. 0.5 * vth * dp_dt / p
     elseif variable_name == :neutral_ddens_dt
+        if !run_info.evolve_density
+            throw(KeyError("evolve_density=false, so do not calculate neutral_ddens_dt"))
+        end
         all_moments = _get_all_moment_variables(run_info)
         if :density_neutral ∉ keys(all_moments)
             throw(KeyError("density_neutral not present"))
@@ -4940,6 +4964,9 @@ function _get_variable_internal(run_info, variable_name::Symbol;
         get_neutral_ddens_dt!(variable, all_moments)
         variable = select_slice_of_variable(variable; kwargs...)
     elseif variable_name == :neutral_dnuz_dt
+        if !run_info.evolve_upar
+            throw(KeyError("evolve_upar=false, so do not calculate neutral_dnuz_dt"))
+        end
         all_moments = _get_all_moment_variables(run_info)
         if :uz_neutral ∉ keys(all_moments)
             throw(KeyError("uz_neutral not present"))
@@ -4966,12 +4993,18 @@ function _get_variable_internal(run_info, variable_name::Symbol;
         get_neutral_dnuz_dt!(variable, all_moments)
         variable = select_slice_of_variable(variable; kwargs...)
     elseif variable_name == :neutral_duz_dt
+        if !run_info.evolve_upar
+            throw(KeyError("evolve_upar=false, so do not calculate neutral_duz_dt"))
+        end
         dn_dt = get_variable(run_info, "neutral_ddens_dt"; kwargs...)
         dnuz_dt = get_variable(run_info, "neutral_dnuz_dt"; kwargs...)
         n = get_variable(run_info, "density_neutral"; kwargs...)
         uz = get_variable(run_info, "uz_neutral"; kwargs...)
         variable = @. dnuz_dt / n - uz / n * dn_dt
     elseif variable_name == :neutral_dp_dt
+        if !run_info.evolve_p
+            throw(KeyError("evolve_p=false, so do not calculate neutral_dp_dt"))
+        end
         all_moments = _get_all_moment_variables(run_info)
         if :p_neutral ∉ keys(all_moments)
             throw(KeyError("p_neutral not present"))
@@ -4997,6 +5030,9 @@ function _get_variable_internal(run_info, variable_name::Symbol;
         get_neutral_duz_dt!(variable, all_moments)
         variable = select_slice_of_variable(variable; kwargs...)
     elseif variable_name == :neutral_dvth_dt
+        if !run_info.evolve_p
+            throw(KeyError("evolve_p=false, so do not calculate neutral_dvth_dt"))
+        end
         dn_dt = get_variable(run_info, "neutral_ddens_dt"; kwargs...)
         dp_dt = get_variable(run_info, "neutral_dp_dt"; kwargs...)
         n = get_variable(run_info, "density_neutral"; kwargs...)
