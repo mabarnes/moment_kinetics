322cde2ff41aafa1058c02ca11e74e5abd98ace8
John Omotani <john.omotani@ukaea.uk>
2026-02-18 15:28:31 +0000

    Relax fokker_planck_time_evolution_tests.jl tolerance slightly
    Update to Julia-1.12 has increased rounding errors slightly in CI.

--- moment_kinetics/test/fokker_planck_time_evolution_tests.jl
+++ moment_kinetics/test/fokker_planck_time_evolution_tests.jl
@@ -455,7 +455,7 @@ function run_test(test_input, expected, atol, final_atol; interp_to_expected=tru
                 f_err = @. abs(expected.f_ion[:,:,tind] - f_ion[:,:,tind])
                 max_f_err = maximum(f_err)
                 @test isapprox(max_f_err, 0.0, atol=this_atol)
-                @test elementwise_isapprox(expected.f_ion[:,:,tind], f_ion[:,:,tind], rtol=this_atol, atol=this_atol*1.0e-3)
+                @test elementwise_isapprox(expected.f_ion[:,:,tind], f_ion[:,:,tind], rtol=this_atol, atol=this_atol*2.0e-3)
             end
         end
 
